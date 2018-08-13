4e59de3265c3462aad3e1ee9b9aa195e25ce2d3a
weekface <weekface@gmail.com>
2018-08-13 20:29:04 +0800

    *: extract scale down as scaler interface

--- pkg/controller/tidbcluster/tidb_cluster_controller.go
+++ pkg/controller/tidbcluster/tidb_cluster_controller.go
@@ -91,6 +91,7 @@ func NewController(
 	pvControl := controller.NewRealPVControl(kubeCli, pvcInformer.Lister(), recorder)
 	pvcControl := controller.NewRealPVCControl(kubeCli, recorder)
 	podControl := controller.NewRealPodControl(kubeCli, pdControl, recorder)
+	pdScaleDowner := mm.NewPDScaler(pdControl)
 
 	tcc := &Controller{
 		kubeClient: kubeCli,
@@ -103,6 +104,7 @@ func NewController(
 				svcControl,
 				setInformer.Lister(),
 				svcInformer.Lister(),
+				pdScaleDowner,
 			),
 			mm.NewTiKVMemberManager(
 				pdControl,
