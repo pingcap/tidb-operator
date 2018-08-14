4272a909f296cac6638bbc2b6cbea67ea58c130e
weekface <weekface@gmail.com>
2018-08-14 11:40:19 +0800

    address comment

--- pkg/manager/member/pd_scaler.go
+++ pkg/manager/member/pd_scaler.go
@@ -121,6 +121,8 @@ func (psd *pdScaler) ScaleIn(tc *v1alpha1.TidbCluster, oldSet *apps.StatefulSet,
 	return nil
 }
 
+var _ Scaler = &pdScaler{}
+
 type fakePDScaler struct{}
 
 // NewFakePDScaler returns a fake Scaler
@@ -135,3 +137,5 @@ func (fsd *fakePDScaler) ScaleOut(tc *v1alpha1.TidbCluster, oldSet *apps.Statefu
 func (fsd *fakePDScaler) ScaleIn(tc *v1alpha1.TidbCluster, oldSet *apps.StatefulSet, newSet *apps.StatefulSet) error {
 	return nil
 }
+
+var _ Scaler = &fakePDScaler{}
