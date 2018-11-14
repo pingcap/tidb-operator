0ff00b988db3246bd1bdeda23eb080fd20a99f2f
Tennix <tennix@users.noreply.github.com>
2018-11-14 16:21:38 +0800

    Migrate to Go module (#178)
    * migrate from dep to go module

--- hack/retool-install.sh
+++ hack/retool-install.sh
@@ -13,7 +13,6 @@ retool add github.com/client9/misspell/cmd/misspell v0.3.4
 # checks correctness
 retool add github.com/gordonklaus/ineffassign 7bae11eba15a3285c75e388f77eb6357a2d73ee2
 retool add honnef.co/go/tools/cmd/megacheck master
-retool add github.com/dnephin/govet 4a96d43e39d340b63daa8bc5576985aa599885f6
 # slow checks
 retool add github.com/kisielk/errcheck v1.1.0
 # linter
