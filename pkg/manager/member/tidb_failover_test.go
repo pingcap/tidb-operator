a284c268caf08cda6e7d55b03483bedd209f86b5
xiaojingchen <chenxiaojing141@gmail.com>
2018-12-04 18:00:26 +0800

    Update pkg/manager/member/tidb_failover_test.go
    Co-Authored-By: weekface <weekface@gmail.com>

--- pkg/manager/member/tidb_failover_test.go
+++ pkg/manager/member/tidb_failover_test.go
@@ -318,7 +318,7 @@ func TestFakeTiDBFailoverRecover(t *testing.T) {
 						PodName: "failover-tidb-0",
 					},
 					"failover-tidb-1": {
-						PodName: "failover-tidb-0",
+						PodName: "failover-tidb-1",
 					},
 				}
 			},
