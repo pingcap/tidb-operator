4e9e00b88967d0391fa561905be98b447017eaf6
shuijing198799 <yinliang@pingcap.com>
2019-03-08 13:17:14 +0800

    add ignore file and add bakcup test to the stability branch

--- tests/actions.go
+++ tests/actions.go
@@ -20,6 +20,10 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"bytes"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
 
 	"github.com/golang/glog"
 	"github.com/pingcap/kvproto/pkg/metapb"
@@ -33,6 +37,7 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 func NewOperatorActions(cli versioned.Interface, kubeCli kubernetes.Interface) OperatorActions {
@@ -56,14 +61,16 @@ type OperatorActions interface {
 	ScaleTidbCluster(info *TidbClusterInfo) error
 	UpgradeTidbCluster(info *TidbClusterInfo) error
 	DeployAdHocBackup(info *TidbClusterInfo) error
-	CleanAdHocBackup(info *TidbClusterInfo) error
+	CheckAdHocBackup(info *TidbClusterInfo) error
 	DeployScheduledBackup(info *TidbClusterInfo) error
-	CleanScheduledBackup(info *TidbClusterInfo) error
-	DeployIncrementalBackup(info *TidbClusterInfo) error
-	CleanIncrementalBackup(info *TidbClusterInfo) error
-	Restore(from *TidbClusterInfo, jobName string, to *TidbClusterInfo) error
+	CheckScheduledBackup(info *TidbClusterInfo) error
+	DeployIncrementalBackup(from *TidbClusterInfo,to *TidbClusterInfo) error
+	CheckIncrementalBackup(info *TidbClusterInfo) error
+	Restore(from *TidbClusterInfo, to *TidbClusterInfo) error
+	CheckRestore(from *TidbClusterInfo, to *TidbClusterInfo) error
 	DeployMonitor(info *TidbClusterInfo) error
 	CleanMonitor(info *TidbClusterInfo) error
+	ForceDeploy(info *TidbClusterInfo) error
 }
 
 type FaultTriggerActions interface {
@@ -321,15 +328,6 @@ func (oa *operatorActions) StopInsertDataTo(info *TidbClusterInfo) error {
 
 func (oa *operatorActions) ScaleTidbCluster(info *TidbClusterInfo) error        { return nil }
 func (oa *operatorActions) UpgradeTidbCluster(info *TidbClusterInfo) error      { return nil }
-func (oa *operatorActions) DeployAdHocBackup(info *TidbClusterInfo) error       { return nil }
-func (oa *operatorActions) CleanAdHocBackup(info *TidbClusterInfo) error        { return nil }
-func (oa *operatorActions) DeployScheduledBackup(info *TidbClusterInfo) error   { return nil }
-func (oa *operatorActions) CleanScheduledBackup(info *TidbClusterInfo) error    { return nil }
-func (oa *operatorActions) DeployIncrementalBackup(info *TidbClusterInfo) error { return nil }
-func (oa *operatorActions) CleanIncrementalBackup(info *TidbClusterInfo) error  { return nil }
-func (oa *operatorActions) Restore(from *TidbClusterInfo, jobName string, to *TidbClusterInfo) error {
-	return nil
-}
 func (oa *operatorActions) DeployMonitor(info *TidbClusterInfo) error { return nil }
 func (oa *operatorActions) CleanMonitor(info *TidbClusterInfo) error  { return nil }
 
@@ -811,3 +809,470 @@ func checkoutTag(tagName string) error {
 
 	return nil
 }
+
+//scheduler backup, make sure there are serveral backup record is generated, and 
+//store the datas of each record
+func (oa *operatorActions) DeployScheduledBackup(info *TidbClusterInfo) error {
+	glog.Infof("begin to deploy scheduled backup")
+	defer func() {
+		glog.Infof("deploy shceduled backup end")
+	}()
+	minute := time.Now().Minute()
+	cron := fmt.Sprintf("'%d * * * *'",(minute + 5) % 60)
+	sets := map[string]string {
+		"clusterName":		info.ClusterName,
+		"scheduledBackup.create":	"true",
+		"scheduledBackup.user":		"root",
+		"scheduledBackup.password":	info.Password,
+		"scheduledBackup.schedule":	cron,
+	}
+	var buffer bytes.Buffer
+	for k,v := range sets {
+		set := fmt.Sprintf("--set %s=%s",k , v)
+		_,err := buffer.WriteString(set)
+		if err != nil  {
+			return err
+		}
+	}
+
+	setStr := buffer.String()
+	cmd := fmt.Sprintf("helm update %s /charts/%s/tidb-cluster %s",
+		info.ClusterName, info.OperatorTag, setStr)
+	res, err := exec.Command("/bin/sh", "-c", cmd).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("failed to launch scheduler backup job: %v, %s", err, string(res))
+	}
+	return nil
+}
+
+//restore genereted record by scheduledbackup, and make sure the data is correct
+func (oa *operatorActions) CheckScheduledBackup(info *TidbClusterInfo) error {
+	glog.Infof("begin to check scheduler backup")
+	defer func() {
+		glog.Infof("deploy check scheduler end")
+	}()
+
+	jobName := fmt.Sprintf("%s--scheduled-backup",info.ClusterName)
+	fn := func() (bool, error) {
+		job, err :=  oa.kubeCli.BatchV1beta1().CronJobs(info.Namespace).Get(jobName,metav1.GetOptions{})
+		if err != nil {
+			glog.Errorf("failed to get cronjobs %s ,%v",jobName,err)
+			return false, nil
+		}
+
+		jobs, err := oa.kubeCli.BatchV1().Jobs(info.Namespace).List(metav1.ListOptions{})
+		if err != nil {
+			glog.Errorf("failed to list jobs %s ,%v",info.Namespace,err)
+			return false, nil
+		}
+
+		backupJobs := []batchv1.Job{}
+		for _,j := range jobs.Items {
+			if pid, found := getParentUIDFromJob(j); found && pid == job.UID {
+				backupJobs = append(backupJobs,j)
+			}
+		}
+
+		if len(backupJobs) == 0 {
+			glog.Errorf("cluster [%s] scheduler jobs is creating, please wait!",info.ClusterName)
+			return false, nil
+		}
+
+		for _,j := range backupJobs {
+			if j.Status.Succeeded == 0 {
+				glog.Error("cluster [%s] back up job is not completed, please wait! ",info.ClusterName);
+				return false, nil
+			}
+		}
+
+		return true, nil
+	}
+
+	err := wait.Poll(5 * time.Second, 10 * time.Minute, fn)
+	if err != nil {
+		return fmt.Errorf("failed to launch scheduler backup job: %v", err)
+	}
+
+	return nil
+}
+
+//full data backup
+func (oa *operatorActions) DeployAdHocBackup(info *TidbClusterInfo) error {
+	glog.Infof("begin to deploy adhoc backup")
+	defer func() {
+		glog.Infof("deploy adhoc backup end")
+	}()
+	sets := map[string]string {
+		"clusterName":		info.ClusterName,
+		"backup.create":	"true",
+		"backup.user":		"root",
+		"backup.password":	info.Password,
+	}
+	var buffer bytes.Buffer
+	for k,v := range sets {
+		set := fmt.Sprintf("--set %s=%s",k , v)
+		_,err := buffer.WriteString(set)
+		if err != nil  {
+			return err
+		}
+	}
+
+	setStr := buffer.String()
+	fullbackupName := fmt.Sprintf("%s-backup",info.ClusterName)
+	cmd := fmt.Sprintf("helm install -n %s --namespace %s /charts/%s/tidb-backup %s",
+		fullbackupName, info.Namespace, info.OperatorTag, setStr)
+	res, err := exec.Command("/bin/sh", "-c", cmd).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("failed to launch adhoc backup job: %v, %s", err, string(res))
+	}
+	return nil
+}
+
+//restore full data backup is correct and check full data backup data is correct
+func (oa *operatorActions) CheckAdHocBackup(info *TidbClusterInfo) error {
+	glog.Infof("begin to clean adhoc backup")
+	defer func() {
+		glog.Infof("deploy clean backup end")
+	}()
+
+	jobName := fmt.Sprintf("%s-backup",info.ClusterName)
+	fn := func() (bool, error) {
+		job, err :=  oa.kubeCli.BatchV1().Jobs(info.Namespace).Get(jobName,metav1.GetOptions{})
+		if err != nil {
+			glog.Errorf("failed to get jobs %s ,%v",jobName,err)
+			return false, nil
+		}
+		if job.Status.Succeeded == 0 {
+			glog.Error("cluster [%s] back up job is not completed, please wait! ",info.ClusterName);
+			return false, nil
+		}
+
+		return true, nil
+	}
+
+	err := wait.Poll(5 * time.Second, 10 * time.Minute, fn)
+	if err != nil {
+		return fmt.Errorf("failed to launch scheduler backup job: %v", err)
+	}
+	return nil
+}
+
+//return fmt.Errorf("failed to launch scheduler backup job: %v, %s", err, string(res))incretment data backup
+func (oa *operatorActions) DeployIncrementalBackup(from *TidbClusterInfo, to *TidbClusterInfo) error {
+	glog.Infof("begin to deploy incremental backup")
+	defer func() {
+		glog.Infof("deploy incremental backup end")
+	}()
+	sets := map[string]string {
+		"binlog.pump.create":				"true",
+		"binlog.drainer.destDBType":		"mysql",
+		"binlog.drainer.create":			"true",
+		"binlog.drainer.mysql.host":		fmt.Sprintf("%s-tidb.%s", to.ClusterName, to.Namespace),
+		"binlog.drainer.mysql.user":		"root",
+		"binlog.drainer.mysql.password":	to.Password,
+		"binlog.drainer.mysql.port":		"4000",
+	}
+
+	var buffer bytes.Buffer
+	for k,v := range sets {
+		set := fmt.Sprintf("--set %s=%s",k , v)
+		_,err := buffer.WriteString(set)
+		if err != nil  {
+			return err
+		}
+	}
+
+	setStr := buffer.String()
+	cmd := fmt.Sprintf("helm update %s /charts/%s/tidb-cluster %s",
+	from.ClusterName, from.OperatorTag, setStr)
+	res, err := exec.Command("/bin/sh", "-c", cmd).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("failed to launch scheduler backup job: %v, %s", err, string(res))
+	}
+	return nil
+}
+
+//restore increment data backup and make sure data is correct.
+func (oa *operatorActions) CheckIncrementalBackup(info *TidbClusterInfo) error {
+	glog.Infof("begin to check incremental backup")
+	defer func() {
+		glog.Infof("check incremental backup end")
+	}()
+
+	pumpStatefulSetName := fmt.Sprintf("%s-pump",info.ClusterName)
+	fn := func() (bool, error) {
+		pumpStatefulSet, err :=  oa.kubeCli.AppsV1().StatefulSets(info.Namespace).Get(pumpStatefulSetName,metav1.GetOptions{})
+		if err != nil {
+			glog.Errorf("failed to get jobs %s ,%v",pumpStatefulSetName,err)
+			return false, nil
+		}
+
+		if pumpStatefulSet.Status.Replicas != pumpStatefulSet.Status.ReadyReplicas {
+			glog.Errorf("pump replicas is not ready, please wait ! %s ",pumpStatefulSetName)
+			return false, nil
+		}
+
+		listOps := metav1.ListOptions {
+			LabelSelector : labels.SelectorFromSet (
+				pumpStatefulSet.Labels,
+			).String(),
+		}
+
+		pods, err := oa.kubeCli.CoreV1().Pods(info.Namespace).List(listOps)
+		if err!= nil {
+			glog.Errorf("failed to get pods via pump labels %s ,%v",pumpStatefulSetName,err)
+			return false, nil
+		}
+
+		for _,pod := range pods.Items {
+			if (!oa.pumpHealth(info,pod.Spec.Hostname)) {
+				glog.Errorf("some pods is not health %s ,%v",pumpStatefulSetName,err)
+				return false,nil
+			}
+		}
+
+		drainerStatefulSetName := fmt.Sprintf("%s-drainer", info.ClusterName)
+		drainerStatefulSet, err := oa.kubeCli.AppsV1().StatefulSets(info.Namespace).Get(drainerStatefulSetName, metav1.GetOptions{})
+		if err != nil {
+			glog.Errorf("failed to get jobs %s ,%v",pumpStatefulSetName,err)
+			return false, nil
+		}
+		if drainerStatefulSet.Status.Replicas != drainerStatefulSet.Status.ReadyReplicas {
+			glog.Errorf("drainer replicas is not ready, please wait ! %s ",pumpStatefulSetName)
+			return false, nil
+		}
+
+		listOps = metav1.ListOptions{
+			LabelSelector: labels.SelectorFromSet(
+				drainerStatefulSet.Labels,
+			).String(),
+		}
+
+		pods, err = oa.kubeCli.CoreV1().Pods(info.Namespace).List(listOps)
+		if err != nil {
+			return false, nil
+		}
+		for _, pod := range pods.Items {
+			if !oa.drainerHealth(info, pod.Spec.Hostname) {
+				return false, nil
+			}
+		}
+
+		return true, nil
+	}
+
+	err := wait.Poll(5 * time.Second, 10 * time.Minute, fn)
+	if err != nil {
+		return fmt.Errorf("failed to launch scheduler backup job: %v", err)
+	}
+	return nil
+
+}
+
+//full restore data from record to database.
+func (oa *operatorActions) Restore(from *TidbClusterInfo, to *TidbClusterInfo) error {
+	glog.Infof("begin to deploy restore")
+	defer func() {
+		glog.Infof("deploy restore end")
+	}()
+	sets := map[string]string {
+		"clusterName":                 to.ClusterName,
+		"restore.create":              "true",
+		"restore.user":                "root",
+		"restore.password":            to.Password,
+	}
+	var buffer bytes.Buffer
+	for k,v := range sets {
+		set := fmt.Sprintf("--set %s=%s",k , v)
+		_,err := buffer.WriteString(set)
+		if err != nil  {
+			return err
+		}
+	}
+
+	setStr := buffer.String()
+	restoreName := fmt.Sprintf("%s-restore",to.ClusterName)
+	cmd := fmt.Sprintf("helm update -n %s -namespace %s /charts/%s/tidb-backup %s",
+		restoreName,to.ClusterName, to.OperatorTag, setStr)
+	res, err := exec.Command("/bin/sh", "-c", cmd).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("failed to launch restore job: %v, %s", err, string(res))
+	}
+
+	return nil
+}
+
+//check wether restore data is correct.
+func (oa *operatorActions) CheckRestore(from *TidbClusterInfo, to *TidbClusterInfo) error {
+	glog.Infof("begin to check restore backup")
+	defer func() {
+		glog.Infof("check restore end")
+	}()
+
+	jobName := fmt.Sprintf("%s-restore",to.ClusterName)
+	fn := func() (bool, error) {
+		job, err :=  oa.kubeCli.BatchV1().Jobs(to.Namespace).Get(jobName,metav1.GetOptions{})
+		if err != nil {
+			glog.Errorf("failed to get jobs %s ,%v",jobName,err)
+			return false, nil
+		}
+		if job.Status.Succeeded == 0 {
+			glog.Error("cluster [%s] back up job is not completed, please wait! ",to.ClusterName);
+			return false, nil
+		}
+
+		fromCount, err := from.QueryCount()
+		if err != nil {
+			glog.Error("cluster [%s] count err ",from.ClusterName);
+			return false, nil
+		}
+
+		toCount, err := to.QueryCount()
+		if err != nil {
+			glog.Error("cluster [%s] count err ",to.ClusterName);
+			return false, nil
+		}
+
+		if fromCount != toCount {
+			glog.Error("cluster [%s] count %d cluster [%s] count %d is not equal ",
+				from.ClusterName,fromCount,to.ClusterName,toCount);
+			return false, nil
+		}
+		return true, nil
+	}
+
+	err := wait.Poll(5 * time.Second, 10 * time.Minute, fn)
+	if err != nil {
+		return fmt.Errorf("failed to launch scheduler backup job: %v", err)
+	}
+	return nil
+}
+
+type pumpStatus struct {
+	StatusMap map[string]*nodeStatus
+}
+
+type nodeStatus struct {
+	State string
+}
+
+func (oa *operatorActions) pumpHealth(info *TidbClusterInfo , hostName string) bool {
+	pumpHealthUrl := fmt.Sprintf("%s.%s-pump.%s", hostName, info.ClusterName, info.Namespace)
+	res, err := http.Get(pumpHealthUrl)
+	if err != nil {
+		glog.Errorf("cluster:[%s] call %s failed,error:%v", info.ClusterName, pumpHealthUrl, err)
+		return false
+	}
+	if res.StatusCode >= 400 {
+		glog.Errorf("Error response %v", res.StatusCode)
+		return false
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		glog.Errorf("cluster:[%s] read response body failed,error:%v", info.ClusterName, err)
+		return false
+	}
+	healths := pumpStatus{}
+	err = json.Unmarshal(body, &healths)
+	if err != nil {
+		glog.Errorf("cluster:[%s] unmarshal failed,error:%v", info.ClusterName, err)
+		return false
+	}
+	for _, status := range healths.StatusMap {
+		if status.State != "online" {
+			glog.Errorf("cluster:[%s] pump's state is not online", info.ClusterName)
+			return false
+		}
+	}
+	return true
+}
+
+type drainerStatus struct {
+	PumpPos map[string]int64 `json:"PumpPos"`
+	Synced  bool             `json:"Synced"`
+	LastTS  int64            `json:"LastTS"`
+	TsMap   string           `json:"TsMap"`
+}
+
+func (oa *operatorActions) drainerHealth(info *TidbClusterInfo, hostName string) bool {
+	drainerHealthUrl := fmt.Sprintf("%s.%s-drainer.%s", hostName, info.ClusterName, info.Namespace)
+	res, err := http.Get(drainerHealthUrl)
+	if err != nil {
+		glog.Errorf("cluster:[%s] call %s failed,error:%v", info.ClusterName, drainerHealthUrl, err)
+		return false
+	}
+	if res.StatusCode >= 400 {
+		glog.Errorf("Error response %v", res.StatusCode)
+		return false
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		glog.Errorf("cluster:[%s] read response body failed,error:%v", info.ClusterName, err)
+		return false
+	}
+	healths := drainerStatus{}
+	err = json.Unmarshal(body, &healths)
+	if err != nil {
+		glog.Errorf("cluster:[%s] unmarshal failed,error:%v", info.ClusterName, err)
+		return false
+	}
+	return len(healths.PumpPos) > 0 && healths.Synced
+}
+
+func (oa *operatorActions) ForceDeploy(info *TidbClusterInfo) error {
+	if err := oa.CleanTidbCluster(info); err != nil {
+		return err
+	}
+
+	if err := oa.DeployTidbCluster(info); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (info *TidbClusterInfo) FullName() string {
+	return fmt.Sprintf("%s/%s",info.Namespace,info.ClusterName)
+}
+
+func (info *TidbClusterInfo) QueryCount() (int, error) {
+	tableName := "record"
+	db, err := sql.Open("mysql", getDSN(info.Namespace, info.ClusterName, "test", info.Password))
+	if err != nil {
+		return 0, err
+	}
+
+	rows, err := db.Query(fmt.Sprintf("SELECT count(*) FROM %s", tableName))
+	if err != nil {
+		glog.Infof("cluster:[%s], error: %v", info.ClusterName, err)
+		return 0, err
+	}
+
+	for rows.Next() {
+		var count int
+		err := rows.Scan(&count)
+		if err != nil {
+			glog.Infof("cluster:[%s], error :%v", info.ClusterName, err)
+		}
+		return count, nil
+	}
+	return 0, fmt.Errorf("can not find count of ")
+}
+
+func getParentUIDFromJob(j batchv1.Job) (types.UID, bool) {
+	controllerRef := metav1.GetControllerOf(&j)
+
+	if controllerRef == nil {
+		return types.UID(""), false
+	}
+
+	if controllerRef.Kind != "CronJob" {
+		glog.Infof("Job with non-CronJob parent, name %s namespace %s", j.Name, j.Namespace)
+		return types.UID(""), false
+	}
+
+	return controllerRef.UID, true
+}
+
+
+
