7c302393cd0b4cb01ec4330713566b0123007dfc
xiaojingchen <chenxiaojing141@gmail.com>
2019-03-13 17:38:35 +0800

    add volume mount

--- tests/log_dump.go
+++ tests/log_dump.go
@@ -12,7 +12,7 @@ import (
 )
 
 func (oa *operatorActions) DumpAllLogs(operatorInfo *OperatorInfo, testClusters []*TidbClusterInfo) error {
-	logPath := fmt.Sprintf("/%s", oa.logDir)
+	logPath := fmt.Sprintf("/%s/%s", oa.logDir, "operator-stability")
 	if _, err := os.Stat(logPath); os.IsNotExist(err) {
 		err = os.MkdirAll(logPath, os.ModePerm)
 		if err != nil {
