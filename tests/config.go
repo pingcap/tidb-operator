d88bf717b102a50568acc8b7e36cafd4e68c9e78
shuijing198799 <30903849+shuijing198799@users.noreply.github.com>
2019-04-17 19:45:53 +0800

    using validation admission webhooks to test tidb resign ddl owner before stop   (#389)

--- tests/config.go
+++ tests/config.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"github.com/pingcap/tidb-operator/tests/pkg/blockwriter"
@@ -30,6 +31,8 @@ type Config struct {
 	Nodes            []Nodes `yaml:"nodes" json:"nodes"`
 	ETCDs            []Nodes `yaml:"etcds" json:"etcds"`
 	APIServers       []Nodes `yaml:"apiservers" json:"apiservers"`
+	CertFile         string
+	KeyFile          string
 
 	// Block writer
 	BlockWriter blockwriter.Config `yaml:"block_writer,omitempty"`
@@ -60,6 +63,11 @@ func NewConfig() *Config {
 	flag.StringVar(&cfg.TidbVersions, "tidb-versions", "v2.1.3,v2.1.4", "tidb versions")
 	flag.StringVar(&cfg.OperatorTag, "operator-tag", "master", "operator tag used to choose charts")
 	flag.StringVar(&cfg.OperatorImage, "operator-image", "pingcap/tidb-operator:latest", "operator image")
+	flag.StringVar(&cfg.CertFile, "tls-cert-file", cfg.CertFile, ""+
+		"File containing the default x509 Certificate for HTTPS. (CA cert, if any, concatenated "+
+		"after server cert).")
+	flag.StringVar(&cfg.KeyFile, "tls-private-key-file", cfg.KeyFile, ""+
+		"File containing the default x509 private key matching --tls-cert-file.")
 	flag.StringVar(&cfg.OperatorRepoDir, "operator-repo-dir", "/tidb-operator", "local directory to which tidb-operator cloned")
 	flag.Parse()
 
@@ -122,6 +130,16 @@ func (c *Config) GetTiDBVersionOrDie() string {
 	}
 
 	return v
+}
+
+func (c *Config) ConfigTLS() *tls.Config {
+	sCert, err := tls.LoadX509KeyPair(c.CertFile, c.KeyFile)
+	if err != nil {
+		glog.Fatal(err)
+	}
+	return &tls.Config{
+		Certificates: []tls.Certificate{sCert},
+	}
 }
 
 func (c *Config) GetUpgradeTidbVersions() []string {
