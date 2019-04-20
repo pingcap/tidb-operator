d6f9478e87e03e8e36e7fadb67bdb5997680061d
shuijing198799 <yinliang@pingcap.com>
2019-04-21 00:35:44 +0800

    save tikv leader count and compare

--- tests/pkg/webhook/pods.go
+++ tests/pkg/webhook/pods.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/golang/glog"
 	"github.com/pingcap/kvproto/pkg/pdpb"
+	"github.com/pingcap/tidb-operator/pkg/apis/pingcap.com/v1alpha1"
 	"github.com/pingcap/tidb-operator/pkg/controller"
 	"github.com/pingcap/tidb-operator/pkg/label"
 	"github.com/pingcap/tidb-operator/tests/pkg/client"
@@ -21,6 +22,11 @@ import (
 type dbInfo struct {
 	IsOwner bool `json:"is_owner"`
 }
+
+var (
+	kvLeaders map[string]int
+	kvShoot   map[string]bool
+)
 
 func HttpHandler(url string, method string, httpClient *http.Client) (content []byte, err error) {
 	req, err := http.NewRequest(method, url, nil)
@@ -46,10 +52,42 @@ func HttpHandler(url string, method string, httpClient *http.Client) (content []
 
 }
 
+func getAllKVLeaders(tc *v1alpha1.TidbCluster, httpClient *http.Client) (ret map[string]int, err error) {
+	ret = make(map[string]int)
+	podIP := tc.Status.PD.Leader.ClientURL
+	for _, store := range tc.Status.TiKV.Stores {
+		url := fmt.Sprintf("%s/pd/api/v1/store/%s", podIP, store.ID)
+
+		content, err := HttpHandler(url, "GET", httpClient)
+		if err != nil {
+			glog.Errorf("fail to read response %v", err)
+			return nil, err
+		}
+
+		storeInfo := &controller.StoreInfo{}
+		err = json.Unmarshal(content, storeInfo)
+		if err != nil {
+			glog.Errorf("unmarshal failed, %v", err)
+			return nil, err
+		}
+
+		ret[store.PodName] = storeInfo.Status.LeaderCount
+	}
+
+	return ret, nil
+}
+
 // only allow pods to be delete when it is not ddlowner of tidb, not leader of pd and not
 // master of tikv.
 func admitPods(ar v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
 	glog.Infof("admitting pods")
+
+	if kvShoot == nil {
+		kvShoot = make(map[string]bool)
+	}
+
+	glog.Infof("kvshoot [%#v]",kvShoot)
+
 	httpClient := &http.Client{}
 	podResource := metav1.GroupVersionResource{Group: "", Version: "v1", Resource: "pods"}
 	if ar.Request.Resource != podResource {
@@ -62,6 +100,7 @@ func admitPods(ar v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
 
 	name := ar.Request.Name
 	nameSpace := ar.Request.Namespace
+	shootName := fmt.Sprintf("%s:%s", nameSpace, name)
 
 	reviewResponse := v1beta1.AdmissionResponse{}
 	reviewResponse.Allowed = true
@@ -73,14 +112,14 @@ func admitPods(ar v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
 		return &reviewResponse
 	}
 
+	glog.Infof("delete pod %s", pod.Labels[label.ComponentLabelKey])
+
 	tc, err := versionCli.PingcapV1alpha1().TidbClusters(nameSpace).Get(pod.Labels["app.kubernetes.io/instance"], metav1.GetOptions{})
 	if err != nil {
 		reviewResponse.Allowed = false
 		glog.Infof("%v", err)
 		return &reviewResponse
 	}
-
-	glog.Infof("delete pod %s", pod.Labels[label.ComponentLabelKey])
 
 	if pod.Labels[label.ComponentLabelKey] == "tidb" {
 		podIP := pod.Status.PodIP
@@ -107,6 +146,8 @@ func admitPods(ar v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
 			glog.Infof("savely delete pod namespace %s name %s content %s", nameSpace, name, string(content))
 		}
 
+		kvShoot[shootName] = true
+
 	} else if pod.Labels[label.ComponentLabelKey] == "pd" {
 		podIP := tc.Status.PD.Leader.ClientURL
 		url := fmt.Sprintf("%s/pd/api/v1/leader", podIP)
@@ -132,7 +173,14 @@ func admitPods(ar v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
 			glog.Infof("savely delete pod namespace %s name %s leader name %s", nameSpace, name, leader.Name)
 		}
 
+		kvShoot[shootName] = true
+
 	} else if pod.Labels[label.ComponentLabelKey] == "tikv" {
+
+		if _, ok := kvShoot[shootName]; ok {
+			return &reviewResponse
+		}
+
 		var storeID string
 		podIP := tc.Status.PD.Leader.ClientURL
 		for _, store := range tc.Status.TiKV.Stores {
@@ -156,14 +204,27 @@ func admitPods(ar v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
 			return &reviewResponse
 		}
 
-		if storeInfo.Status.LeaderCount > 0 && tc.Status.TiKV.StatefulSet.Replicas > 1 {
+		beforeCount := kvLeaders[name]
+		afterCount := storeInfo.Status.LeaderCount
+		glog.Infof("before evict the leader is %d after evict the leader is %d", beforeCount, afterCount)
+
+		if beforeCount != 0 && afterCount >= beforeCount && tc.Status.TiKV.StatefulSet.Replicas > 1 {
 			time.Sleep(10 * time.Second)
 			glog.Errorf("kv leader is not zero, can't be deleted namespace %s name %s leaderCount %d", nameSpace, name, storeInfo.Status.LeaderCount)
 			os.Exit(3)
 		} else {
 			glog.Infof("savely delete pod namespace %s name %s", nameSpace, name)
 		}
+
+		kvShoot[shootName] = true
+
 	}
+
+	if kvLeaders, err = getAllKVLeaders(tc, httpClient); err != nil {
+		glog.Errorf("fail to get kv infos #v", err)
+	}
+
+	glog.Infof("%#v", kvLeaders)
 
 	return &reviewResponse
 }
