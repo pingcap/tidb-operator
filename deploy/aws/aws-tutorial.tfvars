0a33426782b9ce5f4b5422afaed20798efa7077d
Jacob Lerche <jacob.lerche@gmail.com>
2019-05-07 11:17:12 -0700

    Adds TF variables for AWS EKS tutorial, generalizes userdata.sh script

---
+++ deploy/aws/aws-tutorial.tfvars
@@ -0,0 +1,11 @@
+pd_instance_type = "c5d.large"
+tikv_instance_type = "c5d.large"
+tidb_instance_type = "c5d.large"
+monitor_instance_type = "c5d.large"
+
+pd_count = 1
+tikv_count = 1
+tidb_count = 1
+
+cluster_name = "aws_tutorial"
+tikv_root_volume_size = "50"
--- deploy/aws/variables.tf
+++ deploy/aws/variables.tf
@@ -96,64 +96,68 @@ variable "tidb_instance_type" {
   default = "c4.4xlarge"
 }
 
-variable "monitor_instance type" {
+variable "monitor_instance_type" {
   default = "c5.xlarge"
 }
 
-variable "pd_userdata" {
-  default = <<Endofmessage
-cat <<EOF > /etc/security/limits.d/99-tidb.conf
-root        soft        nofile        1000000
-root        hard        nofile        1000000
-root        soft        core          unlimited
-root        soft        stack         10240
-EOF
-# config docker ulimit
-cp /usr/lib/systemd/system/docker.service /etc/systemd/system/docker.service
-sed -i 's/LimitNOFILE=infinity/LimitNOFILE=1048576/' /etc/systemd/system/docker.service
-sed -i 's/LimitNPROC=infinity/LimitNPROC=1048576/' /etc/systemd/system/docker.service
-systemctl daemon-reload
-systemctl restart docker
-
-# format and mount nvme disk
-if grep nvme1n1 /etc/fstab; then
-    echo "disk already mounted"
-else
-    mkfs -t ext4 /dev/nvme1n1
-    mkdir -p /mnt/disks/ssd1
-    cat <<EOF >> /etc/fstab
-/dev/nvme1n1 /mnt/disks/ssd1 ext4 defaults,nofail,noatime,nodelalloc 0 2
-EOF
-    mount -a
-fi
-Endofmessage
+variable "tikv_root_volume_size" {
+  default = "100"
 }
 
-variable "tikv_userdata" {
-  default = <<Endofmessage
-cat <<EOF > /etc/security/limits.d/99-tidb.conf
-root        soft        nofile        1000000
-root        hard        nofile        1000000
-root        soft        core          unlimited
-root        soft        stack         10240
-EOF
-# config docker ulimits
-cp /usr/lib/systemd/system/docker.service /etc/systemd/system/docker.service
-sed -i 's/LimitNOFILE=infinity/LimitNOFILE=1048576/' /etc/systemd/system/docker.service
-sed -i 's/LimitNPROC=infinity/LimitNPROC=1048576/' /etc/systemd/system/docker.service
-systemctl daemon-reload
-systemctl restart docker
-
-# format and mount nvme disk
-if grep nvme0n1 /etc/fstab; then
-    echo "disk already mounted"
-else
-    mkfs -t ext4 /dev/nvme0n1
-    mkdir -p /mnt/disks/ssd1
-    cat <<EOF >> /etc/fstab
-/dev/nvme0n1 /mnt/disks/ssd1 ext4 defaults,nofail,noatime,nodelalloc 0 2
-EOF
-    mount -a
-fi
-Endofmessage
-}
+//variable "pd_userdata" {
+//  default = <<Endofmessage
+//cat <<EOF > /etc/security/limits.d/99-tidb.conf
+//root        soft        nofile        1000000
+//root        hard        nofile        1000000
+//root        soft        core          unlimited
+//root        soft        stack         10240
+//EOF
+//# config docker ulimit
+//cp /usr/lib/systemd/system/docker.service /etc/systemd/system/docker.service
+//sed -i 's/LimitNOFILE=infinity/LimitNOFILE=1048576/' /etc/systemd/system/docker.service
+//sed -i 's/LimitNPROC=infinity/LimitNPROC=1048576/' /etc/systemd/system/docker.service
+//systemctl daemon-reload
+//systemctl restart docker
+//
+//# format and mount nvme disk
+//if grep nvme1n1 /etc/fstab; then
+//    echo "disk already mounted"
+//else
+//    mkfs -t ext4 /dev/nvme1n1
+//    mkdir -p /mnt/disks/ssd1
+//    cat <<EOF >> /etc/fstab
+///dev/nvme1n1 /mnt/disks/ssd1 ext4 defaults,nofail,noatime,nodelalloc 0 2
+//EOF
+//    mount -a
+//fi
+//Endofmessage
+//}
+//
+//variable "tikv_userdata" {
+//  default = <<Endofmessage
+//cat <<EOF > /etc/security/limits.d/99-tidb.conf
+//root        soft        nofile        1000000
+//root        hard        nofile        1000000
+//root        soft        core          unlimited
+//root        soft        stack         10240
+//EOF
+//# config docker ulimits
+//cp /usr/lib/systemd/system/docker.service /etc/systemd/system/docker.service
+//sed -i 's/LimitNOFILE=infinity/LimitNOFILE=1048576/' /etc/systemd/system/docker.service
+//sed -i 's/LimitNPROC=infinity/LimitNPROC=1048576/' /etc/systemd/system/docker.service
+//systemctl daemon-reload
+//systemctl restart docker
+//
+//# format and mount nvme disk
+//if grep nvme0n1 /etc/fstab; then
+//    echo "disk already mounted"
+//else
+//    mkfs -t ext4 /dev/nvme0n1
+//    mkdir -p /mnt/disks/ssd1
+//    cat <<EOF >> /etc/fstab
+///dev/nvme0n1 /mnt/disks/ssd1 ext4 defaults,nofail,noatime,nodelalloc 0 2
+//EOF
+//    mount -a
+//fi
+//Endofmessage
+//}
