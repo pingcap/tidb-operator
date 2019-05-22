97c220e3101bc4bf688373c1de07e5e7d2d14988
shuijing198799 <yinliang@pingcap.com>
2019-05-22 15:22:30 +0800

    no sidecar-injector

--- manifests/create-cert.sh
+++ manifests/create-cert.sh
@@ -4,10 +4,10 @@ set -e
 
 usage() {
     cat <<EOF
-Generate certificate suitable for use with an sidecar-injector webhook service.
+Generate certificate suitable for use with an webhook service.
 
 This script uses k8s' CertificateSigningRequest API to a generate a
-certificate signed by k8s CA suitable for use with sidecar-injector webhook
+certificate signed by k8s CA suitable for use with webhook
 services. This requires permissions to create and approve CSR. See
 https://kubernetes.io/docs/tasks/tls/managing-tls-in-a-cluster for
 detailed explantion and additional instructions.
