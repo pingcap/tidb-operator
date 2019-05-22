681713897c3ee0482b97d590ae87d6c583ed0e6e
xiaojingchen <chenxiaojing141@gmail.com>
2019-05-22 18:35:52 +0800

    support the affinity feature of k8s which define the rule of assigning pods to nodes (#475)

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -15,7 +15,6 @@ package util
 
 import (
 	"fmt"
-	"sort"
 	"strconv"
 	"strings"
 
@@ -23,126 +22,7 @@ import (
 	"github.com/pingcap/tidb-operator/pkg/apis/pingcap.com/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/kubernetes/pkg/kubelet/apis"
 )
-
-var (
-	// weight is in range 1-100
-	topologySchedulingWeight = map[string]int32{
-		"region":           10,
-		"zone":             20,
-		"rack":             40,
-		apis.LabelHostname: 80,
-	}
-)
-
-// AntiAffinityForPod creates a PodAntiAffinity with antiLabels
-func AntiAffinityForPod(namespace string, antiLabels map[string]string) *corev1.PodAntiAffinity {
-	keys := []string{}
-	for key := range topologySchedulingWeight {
-		keys = append(keys, key)
-	}
-	sort.Strings(keys) // we must use sorted selector, otherwise affinity may vary causing new statefulset generated and pod recreated
-	terms := []corev1.WeightedPodAffinityTerm{}
-	for _, key := range keys {
-		term := corev1.WeightedPodAffinityTerm{
-			Weight: topologySchedulingWeight[key],
-			PodAffinityTerm: corev1.PodAffinityTerm{
-				LabelSelector: &metav1.LabelSelector{MatchLabels: antiLabels},
-				TopologyKey:   key,
-				Namespaces:    []string{namespace},
-			},
-		}
-		terms = append(terms, term)
-	}
-	return &corev1.PodAntiAffinity{
-		PreferredDuringSchedulingIgnoredDuringExecution: terms,
-	}
-}
-
-// AffinityForNodeSelector creates an Affinity for NodeSelector
-// Externally we use NodeSelector for simplicity,
-// while internally we convert it to affinity which can express complex scheduling rules
-func AffinityForNodeSelector(namespace string, required bool, antiLabels, selector map[string]string) *corev1.Affinity {
-	if selector == nil {
-		return nil
-	}
-	affinity := &corev1.Affinity{}
-	if antiLabels != nil {
-		affinity.PodAntiAffinity = AntiAffinityForPod(namespace, antiLabels)
-	}
-
-	keys := []string{}
-	for key := range selector {
-		keys = append(keys, key)
-	}
-	sort.Strings(keys) // we must use sorted selector, otherwise affinity may vary causing new statefulset generated and pod recreated
-
-	requiredTerms := []corev1.NodeSelectorTerm{}
-	if required { // all nodeSelectors are required
-		var exps []corev1.NodeSelectorRequirement
-		for _, key := range keys {
-			requirement := corev1.NodeSelectorRequirement{
-				Key:      key,
-				Operator: corev1.NodeSelectorOpIn,
-				Values:   strings.Split(selector[key], ","),
-			}
-			// NodeSelectorRequirement in the same MatchExpressions are ANDed otherwise ORed
-			exps = append(exps, requirement)
-		}
-		requiredTerms = append(requiredTerms, corev1.NodeSelectorTerm{MatchExpressions: exps})
-		affinity.NodeAffinity = &corev1.NodeAffinity{
-			RequiredDuringSchedulingIgnoredDuringExecution: &corev1.NodeSelector{
-				NodeSelectorTerms: requiredTerms,
-			},
-		}
-		return affinity
-	}
-
-	preferredTerms := []corev1.PreferredSchedulingTerm{}
-	exps := []corev1.NodeSelectorRequirement{}
-	for _, key := range keys {
-		if selector[key] == "" {
-			continue
-		}
-		values := strings.Split(selector[key], ",")
-		// region,zone,rack,host are preferred labels, others are must match labels
-		if weight, ok := topologySchedulingWeight[key]; ok {
-			t := corev1.PreferredSchedulingTerm{
-				Weight: weight,
-				Preference: corev1.NodeSelectorTerm{
-					MatchExpressions: []corev1.NodeSelectorRequirement{
-						{
-							Key:      key,
-							Operator: corev1.NodeSelectorOpIn,
-							Values:   values,
-						},
-					},
-				},
-			}
-			preferredTerms = append(preferredTerms, t)
-		} else {
-			requirement := corev1.NodeSelectorRequirement{
-				Key:      key,
-				Operator: corev1.NodeSelectorOpIn,
-				Values:   values,
-			}
-			// NodeSelectorRequirement in the same MatchExpressions are ANDed otherwise ORed
-			exps = append(exps, requirement)
-		}
-	}
-	requiredTerms = append(requiredTerms, corev1.NodeSelectorTerm{MatchExpressions: exps})
-
-	affinity.NodeAffinity = &corev1.NodeAffinity{
-		RequiredDuringSchedulingIgnoredDuringExecution: &corev1.NodeSelector{
-			NodeSelectorTerms: requiredTerms,
-		},
-		PreferredDuringSchedulingIgnoredDuringExecution: preferredTerms,
-	}
-
-	return affinity
-}
 
 // ResourceRequirement creates ResourceRequirements for MemberSpec
 // Optionally pass in a default value
