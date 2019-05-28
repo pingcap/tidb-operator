ae1af87a20f6416d7f3ad603c2d8e9d465703974
shuijing198799 <yinliang@pingcap.com>
2019-05-28 16:32:16 +0800

    make webhook configurable

--- manifests/create-cert.sh
+++ manifests/create-cert.sh
@@ -14,11 +14,26 @@ detailed explantion and additional instructions.
 
 The server key/cert k8s CA cert are stored in a k8s secret.
 
+       --namespace        Namespace where webhook service and secret reside.
 EOF
     exit 1
 }
 
-namespace=default
+while [[ $# -gt 0 ]]; do
+    case ${1} in
+        --namespace)
+            namespace="$2"
+            shift
+            ;;
+        *)
+            usage
+            ;;
+    esac
+    shift
+done
+
+
+[ -z ${namespace} ] && namespace=default
 service=admission-controller-svc
 secret=admission-controller-certs
 
@@ -26,6 +41,12 @@ if [ ! -x "$(command -v openssl)" ]; then
     echo "openssl not found"
     exit 1
 fi
+
+CURDIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd )
+
+# reset namespace and ca_bundle of webhook.yaml
+sed -i "/caBundle:*/c\      caBundle: \${CA_BUNDLE}" $CURDIR/webhook.yaml
+sed -i "s/namespace:.*/namespace: \${NAMESPACE}/g" $CURDIR/webhook.yaml
 
 csrName=${service}.${namespace}
 tmpdir=$(mktemp -d)
@@ -99,3 +120,5 @@ kubectl create secret generic ${secret} \
         --from-file=cert.pem=${tmpdir}/server-cert.pem \
         --dry-run -o yaml |
     kubectl -n ${namespace} apply -f -
+
+sed -i "s/namespace: .*$/namespace: ${namespace}/g" $CURDIR/webhook.yaml
