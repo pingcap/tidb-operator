306e3e9e98230a2bb7f099facfd9b75523cd66aa
Jacob Lerche <jacob.lerche@gmail.com>
2019-06-03 11:07:29 -0700

    Upgrades to terraform 0.12

--- deploy/gcp/data.tf
+++ deploy/gcp/data.tf
@@ -1,31 +1,32 @@
 data "template_file" "tidb_cluster_values" {
-  template = "${file("${path.module}/templates/tidb-cluster-values.yaml.tpl")}"
+  template = file("${path.module}/templates/tidb-cluster-values.yaml.tpl")
 
-  vars {
-    cluster_version  = "${var.tidb_version}"
-    pd_replicas      = "${var.pd_replica_count}"
-    tikv_replicas    = "${var.tikv_replica_count}"
-    tidb_replicas    = "${var.tidb_replica_count}"
-    operator_version = "${var.tidb_operator_version}"
+  vars = {
+    cluster_version  = var.tidb_version
+    pd_replicas      = var.pd_replica_count
+    tikv_replicas    = var.tikv_replica_count
+    tidb_replicas    = var.tidb_replica_count
+    operator_version = var.tidb_operator_version
   }
 }
 
 data "external" "tidb_ilb_ip" {
-  depends_on = ["null_resource.deploy-tidb-cluster"]
+  depends_on = [null_resource.deploy-tidb-cluster]
   program    = ["bash", "-c", "kubectl --kubeconfig ${local.kubeconfig} get svc -n tidb tidb-cluster-tidb -o json | jq '.status.loadBalancer.ingress[0]'"]
 }
 
 data "external" "monitor_ilb_ip" {
-  depends_on = ["null_resource.deploy-tidb-cluster"]
+  depends_on = [null_resource.deploy-tidb-cluster]
   program    = ["bash", "-c", "kubectl --kubeconfig ${local.kubeconfig} get svc -n tidb tidb-cluster-grafana -o json | jq '.status.loadBalancer.ingress[0]'"]
 }
 
 data "external" "tidb_port" {
-  depends_on = ["null_resource.deploy-tidb-cluster"]
+  depends_on = [null_resource.deploy-tidb-cluster]
   program    = ["bash", "-c", "kubectl --kubeconfig ${local.kubeconfig} get svc -n tidb tidb-cluster-tidb -o json | jq '.spec.ports | .[] | select( .name == \"mysql-client\") | {port: .port|tostring}'"]
 }
 
 data "external" "monitor_port" {
-  depends_on = ["null_resource.deploy-tidb-cluster"]
+  depends_on = [null_resource.deploy-tidb-cluster]
   program    = ["bash", "-c", "kubectl --kubeconfig ${local.kubeconfig} get svc -n tidb tidb-cluster-grafana -o json | jq '.spec.ports | .[] | select( .name == \"grafana\") | {port: .port|tostring}'"]
 }
+
--- deploy/gcp/outputs.tf
+++ deploy/gcp/outputs.tf
@@ -1,37 +1,37 @@
 output "region" {
-  value = "${var.GCP_REGION}"
+  value = var.GCP_REGION
 }
 
 output "cluster_id" {
-  value = "${google_container_cluster.cluster.id}"
+  value = google_container_cluster.cluster.id
 }
 
 output "cluster_name" {
-  value = "${google_container_cluster.cluster.name}"
+  value = google_container_cluster.cluster.name
 }
 
 output "kubeconfig_file" {
-  value = "${local.kubeconfig}"
+  value = local.kubeconfig
 }
 
 output "tidb_version" {
-  value = "${var.tidb_version}"
+  value = var.tidb_version
 }
 
 output "tidb_ilb_ip" {
-  value = "${data.external.tidb_ilb_ip.result["ip"]}"
+  value = data.external.tidb_ilb_ip.result["ip"]
 }
 
 output "tidb_port" {
-  value = "${data.external.tidb_port.result["port"]}"
+  value = data.external.tidb_port.result["port"]
 }
 
 output "monitor_ilb_ip" {
-  value = "${data.external.monitor_ilb_ip.result["ip"]}"
+  value = data.external.monitor_ilb_ip.result["ip"]
 }
 
 output "monitor_port" {
-  value = "${data.external.monitor_port.result["port"]}"
+  value = data.external.monitor_port.result["port"]
 }
 
 output "how_to_ssh_to_bastion" {
@@ -41,3 +41,4 @@ output "how_to_ssh_to_bastion" {
 output "how_to_connect_to_mysql_from_bastion" {
   value = "mysql -h ${data.external.tidb_ilb_ip.result["ip"]} -P ${data.external.tidb_port.result["port"]} -u root"
 }
+
