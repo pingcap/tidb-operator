042b1a97fbbdf342297002990564828e6644a3f0
Aylei <rayingecho@gmail.com>
2019-07-23 19:44:58 +0800

    Alibaba Cloud multiple tidb clusters management support (#658)
    * Refactor aliyun tf to multiple cloud
    
    Signed-off-by: Aylei <rayingecho@gmail.com>
    
    * Small fixes for terraform aws
    - fix hardcode chart version
    - fix unreachable bastion
    - adjust tidb-cluster no-wait to avoid scheduled backup pvc blocking
    - add version check
    - disable pd node public_ip
    
    Signed-off-by: Aylei <rayingecho@gmail.com>
    
    * aliyun multiple cluster
    
    Signed-off-by: Aylei <rayingecho@gmail.com>
    
    * Modularization for AWS terraform scripts
    
    Signed-off-by: Aylei <rayingecho@gmail.com>
    
    * Refine README of AWS terraform
    
    Signed-off-by: Aylei <rayingecho@gmail.com>
    
    * Fix bastion ssh
    
    Signed-off-by: Aylei <rayingecho@gmail.com>
    
    * Alibaba cloud multiple clusters support
    
    Signed-off-by: Aylei <rayingecho@gmail.com>
    
    * Fix endpoints output
    
    Signed-off-by: Aylei <rayingecho@gmail.com>

--- deploy/modules/share/tidb-cluster-release/data.tf
+++ deploy/modules/share/tidb-cluster-release/data.tf
@@ -9,3 +9,15 @@ data "external" "monitor_hostname" {
   working_dir = path.cwd
   program     = ["bash", "-c", "kubectl --kubeconfig ${var.kubeconfig_filename} get svc -n ${var.cluster_name} ${var.cluster_name}-grafana -o json | jq '.status.loadBalancer.ingress[0]'"]
 }
+
+data "external" "tidb_port" {
+  depends_on  = [helm_release.tidb-cluster]
+  working_dir = path.cwd
+  program    = ["bash", "-c", "kubectl --kubeconfig ${var.kubeconfig_filename} get svc -n ${var.cluster_name} ${var.cluster_name}-tidb -o json | jq '.spec.ports | .[] | select( .name == \"mysql-client\") | {port: .port|tostring}'"]
+}
+
+data "external" "monitor_port" {
+  depends_on  = [helm_release.tidb-cluster]
+  working_dir = path.cwd
+  program    = ["bash", "-c", "kubectl --kubeconfig ${var.kubeconfig_filename} get svc -n ${var.cluster_name} ${var.cluster_name}-grafana -o json | jq '.spec.ports | .[] | select( .name == \"grafana\") | {port: .port|tostring}'"]
+}
--- deploy/modules/share/tidb-cluster-release/outputs.tf
+++ deploy/modules/share/tidb-cluster-release/outputs.tf
@@ -1,7 +1,20 @@
+locals {
+  tidb_hostname = lookup(data.external.tidb_hostname.result, var.service_ingress_key, "empty")
+  monitor_hostname = lookup(data.external.monitor_hostname.result, var.service_ingress_key, "emtpy")
+}
+
 output "tidb_hostname" {
-  value = lookup(data.external.tidb_hostname.result, "hostname", "empty")
+  value = local.tidb_hostname
 }
 
 output "monitor_hostname" {
-  value = lookup(data.external.monitor_hostname.result, "hostname", "emtpy")
+  value = local.monitor_hostname
+}
+
+output "tidb_endpoint" {
+  value = "${local.tidb_hostname}:${data.external.tidb_port.result["port"]}"
+}
+
+output "monitor_endpoint" {
+  value = "${local.monitor_hostname}:${data.external.monitor_port.result["port"]}"
 }
