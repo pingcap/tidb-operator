cf89eae32ed958da1cf73a62e75843f4dab50119
Jacob Lerche <jacob.lerche@gmail.com>
2019-08-01 20:26:52 -0700

    Uncomments and fixes data.tf

--- deploy/gcp/data.tf
+++ deploy/gcp/data.tf
@@ -1,20 +1,20 @@
-//data "external" "tidb_ilb_ip" {
-//  depends_on = [null_resource.deploy-tidb-cluster]
-//  program    = ["bash", "-c", "kubectl --kubeconfig ${local.kubeconfig} get svc -n tidb tidb-cluster-tidb -o json | jq '.status.loadBalancer.ingress[0]'"]
-//}
-//
-//data "external" "monitor_ilb_ip" {
-//  depends_on = [null_resource.deploy-tidb-cluster]
-//  program    = ["bash", "-c", "kubectl --kubeconfig ${local.kubeconfig} get svc -n tidb tidb-cluster-grafana -o json | jq '.status.loadBalancer.ingress[0]'"]
-//}
-//
-//data "external" "tidb_port" {
-//  depends_on = [null_resource.deploy-tidb-cluster]
-//  program    = ["bash", "-c", "kubectl --kubeconfig ${local.kubeconfig} get svc -n tidb tidb-cluster-tidb -o json | jq '.spec.ports | .[] | select( .name == \"mysql-client\") | {port: .port|tostring}'"]
-//}
-//
-//data "external" "monitor_port" {
-//  depends_on = [null_resource.deploy-tidb-cluster]
-//  program    = ["bash", "-c", "kubectl --kubeconfig ${local.kubeconfig} get svc -n tidb tidb-cluster-grafana -o json | jq '.spec.ports | .[] | select( .name == \"grafana\") | {port: .port|tostring}'"]
-//}
-//
+data "external" "tidb_ilb_ip" {
+  depends_on = [module.default-tidb-cluster]
+  program    = ["bash", "-c", "kubectl --kubeconfig ${local.kubeconfig} get svc -n ${var.default_tidb_cluster_name} ${var.default_tidb_cluster_name}-tidb -o json | jq '.status.loadBalancer.ingress[0]'"]
+}
+
+data "external" "monitor_ilb_ip" {
+  depends_on = [module.default-tidb-cluster]
+  program    = ["bash", "-c", "kubectl --kubeconfig ${local.kubeconfig} get svc -n ${var.default_tidb_cluster_name} ${var.default_tidb_cluster_name}-grafana -o json | jq '.status.loadBalancer.ingress[0]'"]
+}
+
+data "external" "tidb_port" {
+  depends_on = [module.default-tidb-cluster]
+  program    = ["bash", "-c", "kubectl --kubeconfig ${local.kubeconfig} get svc -n ${var.default_tidb_cluster_name} ${var.default_tidb_cluster_name}-tidb -o json | jq '.spec.ports | .[] | select( .name == \"mysql-client\") | {port: .port|tostring}'"]
+}
+
+data "external" "monitor_port" {
+  depends_on = [module.default-tidb-cluster]
+  program    = ["bash", "-c", "kubectl --kubeconfig ${local.kubeconfig} get svc -n ${var.default_tidb_cluster_name} ${var.default_tidb_cluster_name}-grafana -o json | jq '.spec.ports | .[] | select( .name == \"grafana\") | {port: .port|tostring}'"]
+}
+
