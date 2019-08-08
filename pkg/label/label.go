d8be96600aa52e1b0cd958139d6a07ed0d69efc7
onlymellb <luolibin@pingcap.com>
2019-08-08 23:56:54 +0800

    Add backup manager tool (#694)
    * add function for operating condition status
    
    * refactor restore and backup's status updater
    
    * add backup-manager tool
    
    * add BackupProtectionFinalizer key
    
    * add backup-manager makefile
    
    * add backup-manager Dockerfile

--- pkg/label/label.go
+++ pkg/label/label.go
@@ -45,6 +45,13 @@ const (
 	StoreIDLabelKey string = "tidb.pingcap.com/store-id"
 	// MemberIDLabelKey is member id label key
 	MemberIDLabelKey string = "tidb.pingcap.com/member-id"
+
+	// BackupScheduleLabelKey is backup schedule key
+	BackupScheduleLabelKey string = "tidb.pingcap.com/backup-schedule"
+
+	// BackupProtectionFinalizer is the name of finalizer on backups
+	BackupProtectionFinalizer string = "tidb.pingcap.com/backup-protection"
+
 	// AnnFailTiDBScheduler is for injecting a failure into the TiDB custom scheduler
 	// A pod with this annotation will produce an error when scheduled.
 	AnnFailTiDBScheduler string = "tidb.pingcap.com/fail-scheduler"
