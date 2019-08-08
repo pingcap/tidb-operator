70737597fce67b3422d2138f527aa29eccff5b6d
luolibin <luolibin@pingcap.com>
2019-08-08 14:18:40 +0800

    implement job control

--- pkg/controller/job_control.go
+++ pkg/controller/job_control.go
@@ -14,51 +14,88 @@
 package controller
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/golang/glog"
+	"github.com/pingcap/tidb-operator/pkg/label"
 	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes"
-	batchlisters "k8s.io/client-go/listers/batch/v1"
 	"k8s.io/client-go/tools/record"
 )
 
 // JobControlInterface manages Jobs used in backup、restore and clean
 type JobControlInterface interface {
-	CreateJob(job *batchv1.Job) error
-	DeleteJob(namespace, name string) error
-	GetJob(namespace, name string) (*batchv1.Job, error)
+	CreateJob(object runtime.Object, job *batchv1.Job) error
+	DeleteJob(object runtime.Object, job *batchv1.Job) error
 }
 
 type realJobControl struct {
-	kubeCli   kubernetes.Interface
-	jobLister batchlisters.JobLister
-	recorder  record.EventRecorder
+	kubeCli  kubernetes.Interface
+	recorder record.EventRecorder
 }
 
 // NewRealJobControl creates a new JobControlInterface
 func NewRealJobControl(
 	kubeCli kubernetes.Interface,
-	jobLister batchlisters.JobLister,
 	recorder record.EventRecorder,
 ) JobControlInterface {
 	return &realJobControl{
-		kubeCli:   kubeCli,
-		jobLister: jobLister,
-		recorder:  recorder,
+		kubeCli:  kubeCli,
+		recorder: recorder,
 	}
 }
 
-func (rjc *realJobControl) CreateJob(job *batchv1.Job) error {
-	// TODO: Implement this method
-	return nil
+func (rjc *realJobControl) CreateJob(object runtime.Object, job *batchv1.Job) error {
+	ns := job.GetNamespace()
+	jobName := job.GetName()
+	instanceName := job.GetLabels()[label.InstanceLabelKey]
+	kind := object.GetObjectKind().GroupVersionKind().Kind
+
+	_, err := rjc.kubeCli.BatchV1().Jobs(ns).Create(job)
+	if err != nil {
+		glog.Errorf("failed to create Job: [%s/%s], %s: %s, %v", ns, jobName, kind, instanceName, err)
+	} else {
+		glog.V(4).Infof("create Job: [%s/%s] successfully, %s: %s", ns, jobName, kind, instanceName)
+	}
+	rjc.recordJobEvent("create", object, job, err)
+	return err
 }
 
-func (rjc *realJobControl) DeleteJob(namespace, name string) error {
-	// TODO: Implement this method
-	return nil
+func (rjc *realJobControl) DeleteJob(object runtime.Object, job *batchv1.Job) error {
+	ns := job.GetNamespace()
+	jobName := job.GetName()
+	instanceName := job.GetLabels()[label.InstanceLabelKey]
+	kind := object.GetObjectKind().GroupVersionKind().Kind
+
+	err := rjc.kubeCli.BatchV1().Jobs(ns).Delete(jobName, nil)
+	if err != nil {
+		glog.Errorf("failed to delete Job: [%s/%s], %s/%s, err: %v", ns, jobName, kind, instanceName, err)
+	} else {
+		glog.V(4).Infof("delete job: [%s/%s] successfully, %s/%s", ns, jobName, kind, instanceName)
+	}
+	rjc.recordJobEvent("delete", object, job, err)
+	return err
 }
 
-func (rjc *realJobControl) GetJob(namespace, name string) (*batchv1.Job, error) {
-	// TODO: Implement this method
-	return nil, nil
+func (rjc *realJobControl) recordJobEvent(verb string, obj runtime.Object, job *batchv1.Job, err error) {
+	jobName := job.GetName()
+	ns := job.GetNamespace()
+	instanceName := job.GetLabels()[label.InstanceLabelKey]
+	kind := obj.GetObjectKind().GroupVersionKind().Kind
+	if err == nil {
+		reason := fmt.Sprintf("Successful%s", strings.Title(verb))
+		msg := fmt.Sprintf("%s Job %s/%s for %s/%s successful",
+			strings.ToLower(verb), ns, jobName, kind, instanceName)
+		rjc.recorder.Event(obj, corev1.EventTypeNormal, reason, msg)
+	} else {
+		reason := fmt.Sprintf("Failed%s", strings.Title(verb))
+		msg := fmt.Sprintf("%s Job %s/%s for %s/%s failed error: %s",
+			strings.ToLower(verb), ns, jobName, kind, instanceName, err)
+		rjc.recorder.Event(obj, corev1.EventTypeWarning, reason, msg)
+	}
 }
 
 var _ JobControlInterface = &realJobControl{}
