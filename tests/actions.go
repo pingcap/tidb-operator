a5a1f30b71c58c16f21157418d1d49ffe2429991
尹亮 <30903849+shuijing198799@users.noreply.github.com>
2019-08-12 19:14:13 +0800

    e2e:extend the wating time of CheckManualPauseTiDB process (#752)

--- tests/actions.go
+++ tests/actions.go
@@ -2693,7 +2693,7 @@ func (oa *operatorActions) CheckManualPauseTiDB(info *TidbClusterConfig) error {
 	}
 
 	// wait for the tidb statefulset is upgrade to the protect one
-	if err = wait.Poll(DefaultPollInterval, DefaultPollTimeout, fn); err != nil {
+	if err = wait.Poll(DefaultPollInterval, 30*time.Minute, fn); err != nil {
 		return fmt.Errorf("fail to upgrade to annotation TiDB pod : %v", err)
 	}
 
