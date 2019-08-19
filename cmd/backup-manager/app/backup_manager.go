1012c8c02e1ebd55ad6c52cc59709a4a12e54289
onlymellb <luolibin@pingcap.com>
2019-08-19 11:30:26 +0800

    Implement backup operator (#747)
    * add BackupProtectionFinalizer key
    
    * update backup status and add clean backup logic
    
    * add backup-manager makefile
    
    * add backup-manager Dockerfile
    
    * add backup related CRD yaml
    
    * add rabc for backup related CRD object
    
    * add storageClass and backupImage fields
    
    * turn on the backup related controller
    
    * implement backup controller logic
    
    * implement restore controller logic
    
    * implement backup schedule controller logic
    
    * implement job control
    
    * implement backup control for backup schedule
    
    * implement generate pvc control for backup and restore
    
    * use foreground cascading deletion
    
    * increase image cache hit rate
    
    * add some constants definitions
    
    * add a valid check for ceph endpoint
    
    * rename backup-manager to tidb-backup-manager
    
    * display commitTs on backup CR
    
    * change resync time to 10 second
    
    * specify a serviceaccount for clean job
    
    * add backup related yaml file for test

--- cmd/backup-manager/app/backup_manager.go
+++ cmd/backup-manager/app/backup_manager.go
@@ -14,7 +14,11 @@
 package app
 
 import (
+	"flag"
+
 	"github.com/pingcap/tidb-operator/cmd/backup-manager/app/cmd"
+	"github.com/spf13/pflag"
+	utilflag "k8s.io/apiserver/pkg/util/flag"
 	"k8s.io/apiserver/pkg/util/logs"
 )
 
@@ -22,6 +26,24 @@ func Run() error {
 	logs.InitLogs()
 	defer logs.FlushLogs()
 
+	// fix glog parse error
+	flag.CommandLine.Parse([]string{})
+
+	pflag.CommandLine.SetNormalizeFunc(utilflag.WordSepNormalizeFunc)
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+
+	pflag.Set("logtostderr", "true")
+	// We do not want these flags to show up in --help
+	// These MarkHidden calls must be after the lines above
+	pflag.CommandLine.MarkHidden("version")
+	pflag.CommandLine.MarkHidden("google-json-key")
+	pflag.CommandLine.MarkHidden("log-flush-frequency")
+	pflag.CommandLine.MarkHidden("alsologtostderr")
+	pflag.CommandLine.MarkHidden("log-backtrace-at")
+	pflag.CommandLine.MarkHidden("log-dir")
+	pflag.CommandLine.MarkHidden("logtostderr")
+	pflag.CommandLine.MarkHidden("stderrthreshold")
+	pflag.CommandLine.MarkHidden("vmodule")
 	command := cmd.NewBackupMgrCommand()
 	return command.Execute()
 }
