4f8aa3085743f741216bb3f4ab2f5b7ae82e65ca
Yecheng Fu <cofyc.jackson@gmail.com>
2019-08-23 10:09:15 +0800

    return empty locations result if cluster does not exist

--- deploy/modules/gcp/tidb-cluster/data.tf
+++ deploy/modules/gcp/tidb-cluster/data.tf
@@ -24,6 +24,8 @@ locals {
   # TODO Update related code when node locations is avaiable in attributes of cluster resource.
   cmd_get_cluster_locations = <<EOT
 gcloud --project ${var.gcp_project} container clusters list --filter='name=${var.gke_cluster_name}' --format='json[no-heading](locations)' ${local.cluster_location_args} | jq '.[0] | .locations |= join(",")'
+=======
+gcloud --project ${var.gcp_project} container clusters list --filter='name=${var.gke_cluster_name}' --format='json[no-heading](locations)' ${local.cluster_location_args} | jq '{"locations": (if (. | length) > 0 then .[0].locations | join(",") else "" end) }'
 EOT
 }
 
