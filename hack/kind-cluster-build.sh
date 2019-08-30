cb8f949a4b66a8d3c166abadfe54a743ee3f3262
xiaojingchen <chenxiaojing141@gmail.com>
2019-08-30 14:44:01 +0800

    remove useless info

--- hack/kind-cluster-build.sh
+++ hack/kind-cluster-build.sh
@@ -5,7 +5,6 @@ usage() {
     cat <<EOF
 This script use kind to create Kubernetes cluster,about kind please refer: https://kind.sigs.k8s.io/
 Before run this script,please ensure that:
-* have installed git (version >= v2.15.1)
 * have installed docker
 
 Options:
