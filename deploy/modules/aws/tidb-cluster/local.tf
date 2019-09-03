6c96f1f29e03c5a56e923ee1c6180b544d165a76
Aylei <rayingecho@gmail.com>
2019-09-03 16:31:53 +0900

    AWS terraform: add zone label and reserved resources for nodes
    Signed-off-by: Aylei <rayingecho@gmail.com>

--- deploy/modules/aws/tidb-cluster/local.tf
+++ deploy/modules/aws/tidb-cluster/local.tf
@@ -35,15 +35,18 @@ locals {
     placement_group               = ""                         # The name of the placement group into which to launch the instances, if any.
   }
 
+  # 169.254.169.254 is the authoritative AWS metadata server, see https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-instance-metadata.html
+  aws_zone_getter = "$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)"
+
   tidb_cluster_worker_groups = [
     {
-      name             = "${var.cluster_name}-pd"
-      key_name         = var.ssh_key_name
-      instance_type    = var.pd_instance_type
-      root_volume_size = "50"
-      public_ip        = false
+      name                 = "${var.cluster_name}-pd"
+      key_name             = var.ssh_key_name
+      instance_type        = var.pd_instance_type
+      root_volume_size     = "50"
+      public_ip            = false
       # the space separator is safe when the extra args is empty or prefixed by spaces (the same hereafter)
-      kubelet_extra_args = join(" ",
+      kubelet_extra_args   = join(" ",
         [
           "--register-with-taints=dedicated=${var.cluster_name}-pd:NoSchedule",
           "--node-labels=dedicated=${var.cluster_name}-pd,pingcap.com/aws-local-ssd=true,zone=${local.aws_zone_getter}",
@@ -55,12 +58,12 @@ locals {
       # additional_userdata  = file("userdata.sh")
     },
     {
-      name             = "${var.cluster_name}-tikv"
-      key_name         = var.ssh_key_name
-      instance_type    = var.tikv_instance_type
-      root_volume_size = "50"
-      public_ip        = false
-      kubelet_extra_args = join(" ",
+      name                 = "${var.cluster_name}-tikv"
+      key_name             = var.ssh_key_name
+      instance_type        = var.tikv_instance_type
+      root_volume_size     = "50"
+      public_ip            = false
+      kubelet_extra_args   = join(" ",
         [
           "--register-with-taints=dedicated=${var.cluster_name}-tikv:NoSchedule",
           "--node-labels=dedicated=${var.cluster_name}-tikv,pingcap.com/aws-local-ssd=true,zone=${local.aws_zone_getter}",
@@ -73,13 +76,13 @@ locals {
       # additional_userdata  = file("userdata.sh")
     },
     {
-      name             = "${var.cluster_name}-tidb"
-      key_name         = var.ssh_key_name
-      instance_type    = var.tidb_instance_type
-      root_volume_type = "gp2"
-      root_volume_size = "50"
-      public_ip        = false
-      kubelet_extra_args = join(" ",
+      name                 = "${var.cluster_name}-tidb"
+      key_name             = var.ssh_key_name
+      instance_type        = var.tidb_instance_type
+      root_volume_type     = "gp2"
+      root_volume_size     = "50"
+      public_ip            = false
+      kubelet_extra_args   = join(" ",
         [
           "--allowed-unsafe-sysctls=\\\"net.*\\\"",
           "--register-with-taints=dedicated=${var.cluster_name}-tidb:NoSchedule",
@@ -91,13 +94,13 @@ locals {
       asg_max_size         = var.tidb_count + 2
     },
     {
-      name             = "${var.cluster_name}-monitor"
-      key_name         = var.ssh_key_name
-      instance_type    = var.monitor_instance_type
-      root_volume_type = "gp2"
-      root_volume_size = "50"
-      public_ip        = false
-      kubelet_extra_args = join(" ",
+      name                 = "${var.cluster_name}-monitor"
+      key_name             = var.ssh_key_name
+      instance_type        = var.monitor_instance_type
+      root_volume_type     = "gp2"
+      root_volume_size     = "50"
+      public_ip            = false
+      kubelet_extra_args   = join(" ",
         [
           "--node-labels=zone=${local.aws_zone_getter}",
           lookup(var.group_kubelet_extra_args, "monitor", var.kubelet_extra_args)
