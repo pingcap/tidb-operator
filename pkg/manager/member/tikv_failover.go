f91fc5833566104fd24e273ce1b4f08786ab5bdd
weekface <weekface@gmail.com>
2019-09-30 14:57:25 +0800

    add maxFailoverCount limit to TiKV (#965)

--- pkg/manager/member/tikv_failover.go
+++ pkg/manager/member/tikv_failover.go
@@ -16,6 +16,7 @@ package member
 import (
 	"time"
 
+	"github.com/golang/glog"
 	"github.com/pingcap/tidb-operator/pkg/apis/pingcap.com/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -30,6 +31,9 @@ func NewTiKVFailover(tikvFailoverPeriod time.Duration) Failover {
 }
 
 func (tf *tikvFailover) Failover(tc *v1alpha1.TidbCluster) error {
+	ns := tc.GetNamespace()
+	tcName := tc.GetName()
+
 	for storeID, store := range tc.Status.TiKV.Stores {
 		podName := store.PodName
 		if store.LastTransitionTime.IsZero() {
@@ -47,6 +51,11 @@ func (tf *tikvFailover) Failover(tc *v1alpha1.TidbCluster) error {
 			if tc.Status.TiKV.FailureStores == nil {
 				tc.Status.TiKV.FailureStores = map[string]v1alpha1.TiKVFailureStore{}
 			}
+			if len(tc.Status.TiKV.FailureStores) >= int(tc.Spec.TiKV.MaxFailoverCount) {
+				glog.Warningf("%s/%s failure stores count reached the limit: %d", ns, tcName, tc.Spec.TiKV.MaxFailoverCount)
+				return nil
+			}
+
 			tc.Status.TiKV.FailureStores[storeID] = v1alpha1.TiKVFailureStore{
 				PodName:   podName,
 				StoreID:   store.ID,
