966e07b0e6b1b6671a8cd3f4cb96b4aa27104bd8
Song Gao <2695690803@qq.com>
2019-10-12 11:14:10 +0800

    fix shell

--- hack/generate-crds.sh
+++ hack/generate-crds.sh
@@ -1,30 +1,18 @@
 #!/bin/bash -e
 
-# Copyright 2017 PingCAP, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 GO_PKG="github.com/pingcap/tidb-operator"
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-to_crdgen="${scriptdir}/../cmd/to-crdgen/main.go"
-crd_target="${scriptdir}/../manifests/crd.yaml"
+to_crdgen="$scriptdir/../cmd/to-crdgen"
+crd_target="$scriptdir/../manifests/crd.yaml"
 
 GO111MODULE=on go get k8s.io/code-generator/cmd/openapi-gen@kubernetes-1.12.5
 
-${GOPATH}/bin/openapi-gen --go-header-file=${scriptdir}/boilerplate.go.txt \
-    -i ${GO_PKG}/pkg/apis/pingcap.com/v1alpha1,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/api/core/v1 \
-    -p apis/pingcap.com/v1alpha1  -O openapi_generated -o ../pkg
+$GOPATH/bin/openapi-gen --go-header-file=$scriptdir/boilerplate.go.txt \
+-i $GO_PKG/pkg/apis/pingcap.com/v1alpha1,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/api/core/v1 \
+-p apis/pingcap.com/v1alpha1  -O openapi_generated -o ../pkg
 
-go run ${to_crdgen} tidbcluster > ${crd_target}
-go run ${to_crdgen} backup >> ${crd_target}
-go run ${to_crdgen} restore >> ${crd_target}
-go run ${to_crdgen} backupschedule >> ${crd_target}
+go install $to_crdgen
+$GOPATH/bin/to-crdgen tidbcluster > $crd_target
+$GOPATH/bin/to-crdgen backup >> $crd_target
+$GOPATH/bin/to-crdgen restore >> $crd_target
+$GOPATH/bin/to-crdgen backupschedule >> ${crd_target}
--- hack/verify-crdgen.sh
+++ hack/verify-crdgen.sh
@@ -1,43 +1,26 @@
 #!/usr/bin/env bash
 
-# Copyright 2017 PingCAP, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 GO_PKG="github.com/pingcap/tidb-operator"
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-to_crdgen="${scriptdir}/../cmd/to-crdgen/main.go"
-crd_target="${scriptdir}/../manifests/crd.yaml"
-crd_verify_target="${scriptdir}/../manifests/crd-verify.yaml"
+to_crdgen="$scriptdir/../cmd/to-crdgen/main.go"
+crd_target="$scriptdir/../manifests/crd.yaml"
+crd_verify_target="$scriptdir/../manifests/crd-verify.yaml"
 
 GO111MODULE=on go get k8s.io/code-generator/cmd/openapi-gen@kubernetes-1.12.5
 
-${GOPATH}/bin/openapi-gen --go-header-file=${scriptdir}/boilerplate.go.txt \
-    -i ${GO_PKG}/pkg/apis/pingcap.com/v1alpha1,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/api/core/v1 \
-    -p apis/pingcap.com/v1alpha1  -O openapi_generated -o ../pkg
+$GOPATH/bin/openapi-gen --go-header-file=$scriptdir/boilerplate.go.txt \
+-i $GO_PKG/pkg/apis/pingcap.com/v1alpha1,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/api/core/v1 \
+-p apis/pingcap.com/v1alpha1  -O openapi_generated -o ../pkg
 
-go run ${to_crdgen} tidbcluster > ${crd_verify_target}
-go run ${to_crdgen} backup >> ${crd_verify_target}
-go run ${to_crdgen} restore >> ${crd_verify_target}
-go run ${to_crdgen} backupschedule >> ${crd_verify_target}
+go run $to_crdgen tidbcluster > $crd_verify_target
+go run $to_crdgen backup >> $crd_verify_target
+go run $to_crdgen restore >> $crd_verify_target
+go run $to_crdgen backupschedule >> $crd_verify_target
 
-diff_func(){
-   r="$(diff "$1" "$2")"
+r="$(diff "$crd_target" "$crd_verify_target")"
 if [[ -n $r ]]; then
-    echo $1 is not latest
-    exit 1
+	echo $1 is not latest
+	exit 1
 fi
-}
-
-diff_func ${crd_target} ${crd_verify_target}
 
 echo crds are latest
