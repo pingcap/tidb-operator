567f8a542c641985292f31d8b99fa64dcfc220c5
DanielZhangQD <zhanghailong810@aliyun.com>
2019-10-23 16:08:51 +0800

    tidb lb is empty after terraform apply

--- deploy/modules/share/tidb-cluster-release/data.tf
+++ deploy/modules/share/tidb-cluster-release/data.tf
@@ -1,5 +1,5 @@
 data "external" "tidb_hostname" {
-  depends_on  = [helm_release.tidb-cluster]
+  depends_on  = [helm_release.tidb-cluster, null_resource.wait-lb-ip]
   working_dir = path.cwd
   program     = ["bash", "-c", "kubectl --kubeconfig ${var.kubeconfig_filename} get svc -n ${var.cluster_name} ${var.cluster_name}-tidb -o json | jq '.status.loadBalancer.ingress[0]'"]
 }
--- deploy/modules/share/tidb-cluster-release/main.tf
+++ deploy/modules/share/tidb-cluster-release/main.tf
@@ -139,3 +139,25 @@ EOS
     }
   }
 }
+
+resource "null_resource" "wait-lb-ip" {
+  depends_on = [
+    helm_release.tidb-cluster
+  ]
+  provisioner "local-exec" {
+    interpreter = ["bash", "-c"]
+    working_dir = path.cwd
+    command     = <<EOS
+set -euo pipefail
+
+until kubectl get svc -n ${var.cluster_name} ${var.cluster_name}-tidb -o json | jq '.status.loadBalancer.ingress[0]' | grep "${var.service_ingress_key}"; do
+  echo "Wait for TiDB internal loadbalancer IP"
+  sleep 5
+done
+EOS
+
+    environment = {
+      KUBECONFIG = var.kubeconfig_filename
+    }
+  }
+}
