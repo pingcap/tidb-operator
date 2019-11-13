30daac4cb98ed204db250815568874380daa96c8
weekface <weekface@gmail.com>
2019-11-13 16:47:05 +0800

    release v1.0.3 (#1130)

--- deploy/modules/share/tidb-cluster-release/variables.tf
+++ deploy/modules/share/tidb-cluster-release/variables.tf
@@ -5,7 +5,7 @@ variable "kubeconfig_filename" {
 
 variable "tidb_cluster_chart_version" {
   description = "tidb-cluster chart version"
-  default     = "v1.0.2"
+  default     = "v1.0.3"
 }
 
 variable "create" {
