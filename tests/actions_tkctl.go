7cd1d53fe535304fd034ac20bd18c2965a0f9123
y14988 <yang.yangB@h3c.com>
2019-11-15 11:50:38 +0800

    add tkctl file tcktl test error

--- tests/actions_tkctl.go
+++ tests/actions_tkctl.go
@@ -1,44 +1,44 @@
 package tests
 
 import (
-	"regexp"
 	"fmt"
 	"os/exec"
+	"regexp"
 
-	glog "k8s.io/klog"
 	"github.com/stretchr/testify/assert"
+	glog "k8s.io/klog"
 )
 
 var (
 	t = MyTestT{}
 )
 
-type MyTestT struct {}
+type MyTestT struct{}
 
-func (mt *MyTestT) Errorf(format string, args ...interface{}){
-	fmt.Printf(format,args)
+func (mt *MyTestT) Errorf(format string, args ...interface{}) {
+	fmt.Printf(format, args)
 }
 
-func newTkctlCmd(args []string)  *exec.Cmd{
-	return exec.Command("tkctl",args...)
+func newTkctlCmd(args []string) *exec.Cmd {
+	return exec.Command("tkctl", args...)
 }
 
 func checkListOrDie(info *TidbClusterConfig) {
-	output,err:=newTkctlCmd([]string{"list","--namespace",info.Namespace}).Output()
-	if assert.Nil(&t,err) {
-		glog.Fatalf("command 'list' not run as expect %v",err)
+	output, err := newTkctlCmd([]string{"list", "--namespace", info.Namespace}).Output()
+	if assert.Nil(&t, err) {
+		glog.Fatalf("command 'list' not run as expect %v", err)
 	}
-	if assert.Regexp(&t,regexp.MustCompile(`.*3/3.*3/3.*2/2.*`),string(output)) {
+	if assert.Regexp(&t, regexp.MustCompile(`.*3/3.*3/3.*2/2.*`), string(output)) {
 		glog.Fatalf("command 'list' not run as expect")
 	}
 }
 
 func checkUseOrDie(info *TidbClusterConfig) {
-	output,err:=newTkctlCmd([]string{"use",info.ClusterName}).Output()
-	if assert.Nil(&t,err) {
-		glog.Fatalf("command 'use' not run as expect %v",err)
+	output, err := newTkctlCmd([]string{"use", info.ClusterName}).Output()
+	if assert.Nil(&t, err) {
+		glog.Fatalf("command 'use' not run as expect %v", err)
 	}
-	if assert.Regexp(&t,regexp.MustCompile(fmt.Sprintf("switched to %s/%s",info.Namespace,info.ClusterName)),,string(output)) {
+	if assert.Regexp(&t, regexp.MustCompile(fmt.Sprintf("switched to %s/%s", info.Namespace, info.ClusterName)), string(output)) {
 		glog.Fatalf("command 'use' not run as expect")
 	}
 }
