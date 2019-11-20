36617932622d206c28c3484cf732e1d268905ba8
Yecheng Fu <cofyc.jackson@gmail.com>
2019-11-20 19:01:36 +0800

    remove tests

--- tests/cmd/e2e/main.go
+++ tests/cmd/e2e/main.go
@@ -115,16 +115,6 @@ func main() {
 			UpdateTiDBTokenLimit(cfg.TiDBTokenLimit)
 		oa.UpgradeTidbClusterOrDie(cluster1)
 		oa.CheckTidbClusterStatusOrDie(cluster1)
-
-		// switch to host network
-		cluster1.RunInHost(true)
-		oa.UpgradeTidbClusterOrDie(cluster1)
-		oa.CheckTidbClusterStatusOrDie(cluster1)
-
-		// switch to pod network
-		cluster1.RunInHost(false)
-		oa.UpgradeTidbClusterOrDie(cluster1)
-		oa.CheckTidbClusterStatusOrDie(cluster1)
 	}
 	fn2 := func(wg *sync.WaitGroup) {
 		defer wg.Done()
