77579e035ca92d578f779b482488d2a5a533c86c
onlymellb <luolibin@pingcap.com>
2019-11-21 20:12:19 +0800

    Fix check terraform failed (#1215)
    * fix check-terraform failed in Darwin OS
    
    * fix build master job failed

--- ci/pingcap_tidb_operator_build_kind.groovy
+++ ci/pingcap_tidb_operator_build_kind.groovy
@@ -37,7 +37,7 @@ def call(BUILD_BRANCH, CREDENTIALS_ID, CODECOV_CREDENTIALS_ID) {
 							submoduleCfg: [],
 							userRemoteConfigs: [[
 								credentialsId: "${CREDENTIALS_ID}",
-								refspec: '+refs/pull/*:refs/remotes/origin/pr/* +refs/heads/*:refs/remotes/origin/heads/*',
+								refspec: '+refs/heads/*:refs/remotes/origin/* +refs/pull/*:refs/remotes/origin/pr/*',
 								url: "${BUILD_URL}",
 							]]
 						]
