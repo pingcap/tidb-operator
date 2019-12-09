670dec9f620470831ca29cdee890ef046c16f1d0
Song Gao <2695690803@qq.com>
2019-12-09 14:23:58 +0800

    add new webhook

--- manifests/e2e/patch-ns.sh
+++ manifests/e2e/patch-ns.sh
@@ -1,18 +1,48 @@
 #!/usr/bin/env bash
-set -o errexit
-set -o nounset
-set -o pipefail
+
+set -e
+
+usage() {
+    cat <<EOF
+
+       -n,--namespace        Namespace where webhook service reside
+EOF
+    exit 1
+}
+
+optstring=":-:n"
+
+while getopts "$optstring" opt; do
+    case $opt in
+        -)
+            case "$OPTARG" in
+                namespace)
+                    namespace="${2}"
+                    ;;
+                *)
+                    usage
+                    ;;
+            esac
+            ;;
+        n)
+            namespace="${2}"
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+namespace=${namespace:-tidb-admin}
 
 CURDIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd )
 os=`uname -s| tr '[A-Z]' '[a-z]'`
 
 case ${os} in
   linux)
-    sed -i "s/namespace:.*/namespace: \${NAMESPACE}/g" $CURDIR/webhook.yaml
     sed -i "s/namespace: \${NAMESPACE}/namespace: ${namespace}/g" $CURDIR/webhook.yaml
     ;;
   darwin)
-    sed -i "" "s/namespace:.*/namespace: \${NAMESPACE}/g" $CURDIR/webhook.yaml
     sed -i "" "s/namespace: \${NAMESPACE}/namespace: ${namespace}/g" $CURDIR/webhook.yaml
     ;;
     *)
