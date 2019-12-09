b26841ebd5f5dcd07c5f815cb9b9af360565b2cb
Song Gao <2695690803@qq.com>
2019-12-09 16:01:50 +0800

    fix e2e ca_bundle

--- manifests/patch-e2e.sh
+++ manifests/patch-e2e.sh
@@ -38,15 +38,20 @@ namespace=${namespace:-tidb-admin}
 CURDIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd )
 os=`uname -s| tr '[A-Z]' '[a-z]'`
 
+CA_BUNDLE=$(kubectl get configmap -n kube-system extension-apiserver-authentication -o=jsonpath='{.data.client-ca-file}' | base64 | tr -d '\n')
+echo $CA_BUNDLE
+
+
 case ${os} in
   linux)
+    sed -i "s/caBundle: .*$/caBundle: ${CA_BUNDLE}/g" $CURDIR/webhook.yaml
     sed -i "s/namespace: \${NAMESPACE}/namespace: ${namespace}/g" $CURDIR/webhook.yaml
     ;;
   darwin)
+    sed -i "" "s/caBundle: .*$/caBundle: ${CA_BUNDLE}/g" $CURDIR/webhook.yaml
     sed -i "" "s/namespace: \${NAMESPACE}/namespace: ${namespace}/g" $CURDIR/webhook.yaml
     ;;
     *)
     echo "invalid os ${os}, only support Linux and Darwin" >&2
     ;;
 esac
-
