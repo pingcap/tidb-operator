b4e9608994e2f70853675c68dd09eac64cb7897b
Mike <842725815@qq.com>
2019-12-17 21:18:19 +0800

    clear TiDB failureMembers when scale TiDB to 0  (#1334)
    * Create go.yml
    
    * scale TiDB to 0 did not clear failureMembers
    
    * scale TiDB to 0 did not clear failureMembers
    
    * fix tidb sts is upgrading,update replicas to 0 successfully
    
    * while scaling tidb replica to 0 ,clear failmembers
    
    * optimize code and add e2e test
    
    * take code into failover enable block code
    
    * optimize unit test and rm useless code

--- pkg/manager/member/tidb_upgrader.go
+++ pkg/manager/member/tidb_upgrader.go
@@ -35,6 +35,13 @@ func NewTiDBUpgrader(tidbControl controller.TiDBControlInterface, podLister core
 }
 
 func (tdu *tidbUpgrader) Upgrade(tc *v1alpha1.TidbCluster, oldSet *apps.StatefulSet, newSet *apps.StatefulSet) error {
+
+	// when scale replica to 0 , all nodes crash and tidb is in upgrade phase, this method will throw error about pod is upgrade.
+	// so  directly return nil when scale replica to 0.
+	if tc.Spec.TiDB.Replicas == int32(0) {
+		return nil
+	}
+
 	ns := tc.GetNamespace()
 	tcName := tc.GetName()
 
