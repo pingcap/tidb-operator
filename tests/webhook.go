f953a9ad78aed7f14bba3da70e3019ffd41421e8
Song Gao <2695690803@qq.com>
2019-12-23 18:08:10 +0800

    remove useless code

--- tests/webhook.go
+++ tests/webhook.go
@@ -15,13 +15,6 @@ package tests
 
 import (
 	"fmt"
-	"strconv"
-	"time"
-
-	"github.com/pingcap/tidb-operator/pkg/apis/pingcap/v1alpha1"
-	"github.com/pingcap/tidb-operator/tests/slack"
-	apps "k8s.io/api/apps/v1"
-	"k8s.io/apimachinery/pkg/util/wait"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	utilversion "k8s.io/apimachinery/pkg/util/version"
@@ -52,82 +45,4 @@ func (oa *operatorActions) setCabundleFromApiServer(info *OperatorConfig) error 
 		return nil
 	}
 	return nil
-}
-
-func (oa *operatorActions) CheckUpgradeWithPodWebhook(info *TidbClusterConfig) error {
-	ns := info.Namespace
-	tcName := info.ClusterName
-
-	pdStsName := getStsName(tcName, v1alpha1.PDMemberType)
-	tikvStsName := getStsName(tcName, v1alpha1.TiKVMemberType)
-	tidbStsName := getStsName(tcName, v1alpha1.TiDBMemberType)
-
-	pdDesiredReplicas, err := strconv.ParseInt(info.Resources["pd.replicas"], 10, 32)
-	if err != nil {
-		return err
-	}
-	tikvDesiredReplicas, err := strconv.ParseInt(info.Resources["tikv.replicas"], 10, 32)
-	if err != nil {
-		return err
-	}
-	tidbDesiredReplicas, err := strconv.ParseInt(info.Resources["tidb.replicas"], 10, 32)
-	if err != nil {
-		return err
-	}
-
-	f := func(stsName, namespace string, desiredReplicas int64) (*apps.StatefulSet, bool, error) {
-		sts, err := oa.tcStsGetter.StatefulSets(namespace).Get(stsName, metav1.GetOptions{})
-		if err != nil {
-			klog.Infof("failed to fetch sts[%s/%s]", namespace, stsName)
-			return nil, false, err
-		}
-		if sts.Status.UpdatedReplicas != int32(desiredReplicas) {
-			klog.Infof("sts[%s/%s]'s updatedReplicas[%d]!=desiredReplicas[%d]", namespace, stsName, sts.Status.UpdatedReplicas, int32(desiredReplicas))
-			return nil, false, nil
-		}
-		if sts.Status.CurrentReplicas != int32(desiredReplicas) {
-			klog.Infof("sts[%s/%s]'s currentReplicas[%d]!=desiredReplicas[%d]", namespace, stsName, sts.Status.CurrentReplicas, int32(desiredReplicas))
-			return nil, false, nil
-		}
-		return sts, true, nil
-	}
-
-	return wait.Poll(10*time.Second, 50*time.Minute, func() (done bool, err error) {
-
-		pdsts, ready, err := f(pdStsName, ns, pdDesiredReplicas)
-		if !ready || err != nil {
-			return ready, err
-		}
-		if pdsts.Spec.Template.Spec.Containers[0].Image != info.PDImage {
-			return false, nil
-		}
-
-		tikvsts, ready, err := f(tikvStsName, ns, tikvDesiredReplicas)
-		if !ready || err != nil {
-			return ready, err
-		}
-
-		if tikvsts.Spec.Template.Spec.Containers[0].Image != info.TiKVImage {
-			return false, nil
-		}
-		tidbsts, ready, err := f(tidbStsName, ns, tidbDesiredReplicas)
-		if !ready || err != nil {
-			return ready, err
-		}
-
-		if tidbsts.Spec.Template.Spec.Containers[0].Image != info.TiDBImage && tidbsts.Spec.Template.Spec.Containers[1].Image != info.TiDBImage {
-			return false, nil
-		}
-		return true, nil
-	})
-}
-
-func (oa *operatorActions) CheckUpgradeWithPodWebhookOrDie(info *TidbClusterConfig) {
-	if err := oa.CheckUpgradeWithPodWebhook(info); err != nil {
-		slack.NotifyAndPanic(err)
-	}
-}
-
-func getStsName(tcName string, memberType v1alpha1.MemberType) string {
-	return fmt.Sprintf("%s-%s", tcName, memberType.String())
 }
