11c638632f1e413eb9a1cf196b82bd608f822091
shonge <soog2008@hotmail.com>
2019-12-26 21:03:08 +0800

    fix

--- hack/check-EOF.sh
+++ hack/check-EOF.sh
@@ -6,41 +6,33 @@ set -o pipefail
 
 ROOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd)"
 
-CMD="/usr/bin/find $ROOTDIR/"
+cd $ROOTDIR
 
-EXCLUDE_DIR=(
-    vendor
-    output
-    _tools
-    .git
-    static/tidb-operator-overview.png
-)
-
-for DIR in ${EXCLUDE_DIR[@]}; do
-    CMD="$CMD -path $ROOTDIR/$DIR -prune -o"
-done
-
-FILELIST=$($CMD -type f -print)
-
-check() {
-    x=$(tail -c 1 "$1")
-    if [ "$x" != "" ]; then
-        echo "$1"
-    fi
-}
+FILELIST=($(find . -type f -not \( -path './vendor/*' \
+    -o -path './output/*' \
+    -o -path './_tools/*' \
+    -o -path './.git/*' \
+    -o -path './*/.terraform/*' \
+    -o -path './images/*/bin/*' \
+    -o -path './tests/images/*/bin/*' \
+    -o -path '*.png' \
+    -o -path './tkctl' \
+    \)))
 
 NUM=0
+declare FAILED_FILE
+
 for f in ${FILELIST[@]}; do
-    c=$(tail -c 1 "$f")
-    if [ "$c" != "" ]; then
-        failed_file+=($f)
+    c=$(tail -c 1 "$f" | wc -l)
+    if [ "$c" -eq 0 ]; then
+        FAILED_FILE+=($f)
         NUM=$((NUM + 1))
     fi
 done
 
 if [ $NUM -ne 0 ]; then
     echo "error: following files do not end with newline, please fix them"
-    printf '%s\n' "${failed_file[@]}"
+    printf '%s\n' "${FAILED_FILE[@]}"
     exit 1
 else
     echo "all files pass checking."
