2fd52f723664a76405429636b70b51fff780e35d
Yecheng Fu <cofyc.jackson@gmail.com>
2019-12-30 15:35:15 +0800

    Update hack/e2e.sh
    Co-Authored-By: weekface <weekface@gmail.com>

--- hack/e2e.sh
+++ hack/e2e.sh
@@ -63,7 +63,7 @@ Examples:
 
 4) use registry mirrors
 
-    DOCKER_IO_MIRROR=https://dockerhub.azk8s.cn ./hack/e2e.sh -- <e2e args>
+    DOCKER_IO_MIRROR=https://dockerhub.azk8s.cn QUAY_IO_MIRROR=quay.azk8s.cn GCR_IO_MIRROR=gcr.azk8s.cn ./hack/e2e.sh -- <e2e args>
 
     Also, you can configure mirrors for quay.io/gcr.io via
     QUAY_IO_MIRROR/GCR_IO_MIRROR.
