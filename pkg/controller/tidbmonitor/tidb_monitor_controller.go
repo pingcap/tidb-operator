dab3e77899c38ad59f3b204716c5aa9c0a33ade0
Song Gao <2695690803@qq.com>
2020-01-07 14:04:25 +0800

    fix error log which don't requeue

--- pkg/controller/tidbmonitor/tidb_monitor_controller.go
+++ pkg/controller/tidbmonitor/tidb_monitor_controller.go
@@ -115,7 +115,7 @@ func (tmc *Controller) processNextWorkItem() bool {
 		if perrors.Find(err, controller.IsRequeueError) != nil {
 			klog.Infof("TidbMonitor: %v, still need sync: %v, requeuing", key.(string), err)
 		} else {
-			utilruntime.HandleError(fmt.Errorf("TidbMonitor: %v, sync failed, err: %v, requeuing", key.(string), err))
+			utilruntime.HandleError(fmt.Errorf("TidbMonitor: %v, sync failed, err: %v", key.(string), err))
 		}
 		tmc.queue.AddRateLimited(key)
 	} else {
