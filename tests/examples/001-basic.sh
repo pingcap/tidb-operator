e4fbfb55a87b0391436824bab94fe7704dbfc322
Yecheng Fu <fuyecheng@pingcap.com>
2020-03-11 13:29:23 +0800

    use cert-manager to create and renew tidb-server certificates (self-signed example) (#1844)
    * selfsigned tls cert created by cert-manager
    
    * add tests

--- tests/examples/001-basic.sh
+++ tests/examples/001-basic.sh
@@ -17,36 +17,20 @@ ROOT=$(unset CDPATH && cd $(dirname "${BASH_SOURCE[0]}")/../.. && pwd)
 cd $ROOT
 
 source "${ROOT}/hack/lib.sh"
+source "${ROOT}/tests/examples/t.sh"
+
+NS=$(basename ${0%.*})
 
 function cleanup() {
-    kubectl delete -f examples/basic/tidb-cluster.yaml 
+    kubectl -n $NS delete -f examples/basic/tidb-cluster.yaml
+    kubectl delete ns $NS
 }
 
 trap cleanup EXIT
 
-function checkReplicas() {
-    local pdDesiredReplicas="$1"
-    local tikvDesiredReplicas="$2"
-    local tidbDesiredReplicas="$3"
-    local pdReplicas=$(kubectl get tc basic -ojsonpath='{.status.pd.statefulSet.readyReplicas}')
-    if [[ "$pdReplicas" != "$pdDesiredReplicas" ]]; then
-        echo "info: got pd replicas $pdReplicas, expects $pdDesiredReplicas"
-        return 1
-    fi
-    local tikvReplicas=$(kubectl get tc basic -ojsonpath='{.status.tikv.statefulSet.readyReplicas}')
-    if [[ "$tikvReplicas" != "$tikvDesiredReplicas" ]]; then
-        echo "info: got tikv replicas $tikvReplicas, expects $tikvDesiredReplicas"
-        return 1
-    fi
-    local tidbReplicas=$(kubectl get tc basic -ojsonpath='{.status.tidb.statefulSet.readyReplicas}')
-    if [[ "$tidbReplicas" != "$tidbDesiredReplicas" ]]; then
-        echo "info: got tidb replicas $tidbReplicas, expects $tidbDesiredReplicas"
-        return 1
-    fi
-    echo "info: pd replicas $pdReplicas, tikv replicas $tikvReplicas, tidb replicas $tidbReplicas"
-    return 0
-}
+kubectl create ns $NS
+hack::wait_for_success 10 3 "t::ns_is_active $NS"
 
-kubectl apply -f examples/basic/tidb-cluster.yaml 
+kubectl -n $NS apply -f examples/basic/tidb-cluster.yaml
 
-hack::wait_for_success 600 3 "checkReplicas 3 3 2"
+hack::wait_for_success 600 3 "t::tc_is_ready $NS basic"
