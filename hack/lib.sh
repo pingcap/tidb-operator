86bdd93350e70cfb6d80d20534fa0a0f7227d531
Song Gao <disxiaofei@163.com>
2020-03-16 16:22:53 +0800

    Add API document and its generating util (#1929)

--- hack/lib.sh
+++ hack/lib.sh
@@ -27,12 +27,14 @@ TERRAFORM_VERSION=${TERRAFORM_VERSION:-0.12.12}
 KUBECTL_VERSION=${KUBECTL_VERSION:-1.12.10}
 KUBECTL_BIN=$OUTPUT_BIN/kubectl
 HELM_BIN=$OUTPUT_BIN/helm
+DOCS_BIN=$OUTPUT_BIN/gen-crd-api-reference-docs
 #
 # Don't ugprade to 2.15.x/2.16.x until this issue
 # (https://github.com/helm/helm/issues/6361) has been fixed.
 #
 HELM_VERSION=${HELM_VERSION:-2.9.1}
 KIND_VERSION=${KIND_VERSION:-0.7.0}
+DOCS_VERSION=${DOCS_VERSION:-0.1.5}
 KIND_BIN=$OUTPUT_BIN/kind
 KUBETEST2_VERSION=v0.0.8
 KUBETSTS2_BIN=$OUTPUT_BIN/kubetest2
@@ -200,4 +202,25 @@ function hack::ensure_aws_k8s_tester() {
     curl --retry 10 -L -o $tmpfile https://github.com/aws/aws-k8s-tester/releases/download/$AWS_K8S_TESTER_VERSION/aws-k8s-tester-$AWS_K8S_TESTER_VERSION-$OS-$ARCH
 	mv $tmpfile $AWS_K8S_TESTER_BIN
 	chmod +x $AWS_K8S_TESTER_BIN
+}
+
+function hack::verify_gen_crd_api_references_docs() {
+    if test -x "$DOCS_BIN"; then
+        # TODO check version when the binary version is available.
+        return
+    fi
+    return 1
+}
+
+function hack::ensure_gen_crd_api_references_docs() {
+    if hack::verify_gen_crd_api_references_docs; then
+        return 0
+    fi
+    echo "Installing gen_crd_api_references_docs v$DOCS_VERSION..."
+    tmpdir=$(mktemp -d)
+    trap "test -d $tmpdir && rm -r $tmpdir" RETURN
+    curl --retry 10 -L -o ${tmpdir}/docs-bin.tar.gz https://github.com/ahmetb/gen-crd-api-reference-docs/releases/download/v${DOCS_VERSION}/gen-crd-api-reference-docs_${OS}_${ARCH}.tar.gz
+    tar -zvxf ${tmpdir}/docs-bin.tar.gz -C ${tmpdir}
+    mv ${tmpdir}/gen-crd-api-reference-docs ${DOCS_BIN}
+    chmod +x ${DOCS_BIN}
 }
