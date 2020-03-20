d02a94f57d386d63d12ada2e6d136db7d6f79274
尹亮 <30903849+shuijing198799@users.noreply.github.com>
2020-03-20 14:13:58 +0800

    backup: support br compatible with new TLS interface (#1988)
    * backup: support br compatiable with new TLS interface

--- pkg/apis/pingcap/v1alpha1/openapi_generated.go
+++ pkg/apis/pingcap/v1alpha1/openapi_generated.go
@@ -375,11 +375,10 @@ func schema_pkg_apis_pingcap_v1alpha1_BRConfig(ref common.ReferenceCallback) com
 				Description: "BRConfig contains config for BR",
 				Type:        []string{"object"},
 				Properties: map[string]spec.Schema{
-					"enableTLSClient": {
+					"tlsCluster": {
 						SchemaProps: spec.SchemaProps{
-							Description: "Whether enable TLS in TiDBCluster",
-							Type:        []string{"boolean"},
-							Format:      "",
+							Description: "Whether enable the TLS connection between TiDB server components Optional: Defaults to nil",
+							Ref:         ref("github.com/pingcap/tidb-operator/pkg/apis/pingcap/v1alpha1.TLSCluster"),
 						},
 					},
 					"cluster": {
@@ -470,6 +469,8 @@ func schema_pkg_apis_pingcap_v1alpha1_BRConfig(ref common.ReferenceCallback) com
 				Required: []string{"cluster"},
 			},
 		},
+		Dependencies: []string{
+			"github.com/pingcap/tidb-operator/pkg/apis/pingcap/v1alpha1.TLSCluster"},
 	}
 }
 
