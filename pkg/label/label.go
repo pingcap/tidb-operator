174afb93b61c104bd08dddddb95124620b08e4fd
DanielZhangQD <zhanghailong810@aliyun.com>
2020-04-10 22:17:03 +0800

    deploy controller for tiflash

--- pkg/label/label.go
+++ pkg/label/label.go
@@ -127,6 +127,8 @@ const (
 	TiDBLabelVal string = "tidb"
 	// TiKVLabelVal is TiKV label value
 	TiKVLabelVal string = "tikv"
+	// TiFlashLabelVal is TiKV label value
+	TiFlashLabelVal string = "tiflash"
 	// PumpLabelVal is Pump label value
 	PumpLabelVal string = "pump"
 	// DiscoveryLabelVal is Discovery label value
@@ -301,6 +303,12 @@ func (l Label) TiDB() Label {
 // TiKV assigns tikv to component key in label
 func (l Label) TiKV() Label {
 	l.Component(TiKVLabelVal)
+	return l
+}
+
+// TiFlash assigns tiflash to component key in label
+func (l Label) TiFlash() Label {
+	l.Component(TiFlashLabelVal)
 	return l
 }
 
