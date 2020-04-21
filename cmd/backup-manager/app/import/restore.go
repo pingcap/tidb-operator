c7256d48375a23c5e24ad8865ef172928d735c50
Song Gao <2695690803@qq.com>
2020-04-21 23:17:48 +0800

    Update restore.go

--- cmd/backup-manager/app/import/restore.go
+++ cmd/backup-manager/app/import/restore.go
@@ -22,6 +22,7 @@ import (
 	"github.com/mholt/archiver"
 	"github.com/pingcap/tidb-operator/cmd/backup-manager/app/constants"
 	"github.com/pingcap/tidb-operator/cmd/backup-manager/app/util"
+	"k8s.io/klog"
 )
 
 // Options contains the input arguments to the restore command
@@ -62,12 +63,14 @@ func (ro *Options) loadTidbClusterData(restorePath string) error {
 		"-backend=tidb",
 		"-server-mode=false",
 		"-log-file=",
+		"-tidb-port=4000",
 		fmt.Sprintf("-tidb-user=%s", ro.User),
 		fmt.Sprintf("-tidb-password=%s", ro.Password),
 		fmt.Sprintf("-tidb-host=%s", ro.Host),
 		fmt.Sprintf("-d=%s", restorePath),
 	}
 
+	klog.Infof("/tidb-lightning %v", args)
 	output, err := exec.Command("/tidb-lightning", args...).CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("cluster %s, execute loader command %v failed, output: %s, err: %v", ro, args, string(output), err)
