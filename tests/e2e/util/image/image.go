f088fa64b4068e0cb570a027826fc69e9a33e2b7
Yecheng Fu <fuyecheng@pingcap.com>
2020-04-23 18:51:26 +0800

    e2e: reduce ephemeral storage usage and request more  (#2262)
    * add tidb v4 image into preload list
    
    * set tidb version explicitly
    
    * remove v2 tidb image
    
    * reduce storage usage and request more in CI job
    
    * fix
    
    * fix some issues
    
    * more QPS allowed in e2e
    
    * more debug information

--- tests/e2e/util/image/image.go
+++ tests/e2e/util/image/image.go
@@ -21,31 +21,41 @@ import (
 	"strings"
 
 	"github.com/ghodss/yaml"
-	e2econfig "github.com/pingcap/tidb-operator/tests/e2e/config"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/klog"
 	"k8s.io/kubernetes/test/e2e/framework"
 )
 
 const (
-	TiDBV3Version        = "v3.0.8"
-	TiDBV3UpgradeVersion = "v3.0.9"
-	TiDBTLSVersion       = TiDBV3Version // must >= 3.0.5
-	TiDBV2Version        = "v2.1.19"
-	TiDBV4Version        = "v4.0.0-rc"
+	TiDBV3Version                 = "v3.0.8"
+	TiDBV3UpgradeVersion          = "v3.0.9"
+	TiDBV4Version                 = "v4.0.0-rc"
+	TiDBV4UpgradeVersion          = "nightly"
+	PrometheusImage               = "prom/prometheus"
+	PrometheusVersion             = "v2.11.1"
+	TiDBMonitorReloaderImage      = "pingcap/tidb-monitor-reloader"
+	TiDBMonitorReloaderVersion    = "v1.0.1"
+	TiDBMonitorInitializerImage   = "pingcap/tidb-monitor-initializer"
+	TiDBMonitorInitializerVersion = "v3.0.8"
+	GrafanaImage                  = "grafana/grafana"
+	GrafanaVersion                = "6.0.1"
 )
 
 func ListImages() []string {
 	images := []string{}
-	versions := strings.Split(e2econfig.TestConfig.TidbVersions, ",")
+	versions := make([]string, 0)
 	versions = append(versions, TiDBV3Version)
-	versions = append(versions, TiDBTLSVersion)
-	versions = append(versions, TiDBV2Version)
+	versions = append(versions, TiDBV4Version)
+	versions = append(versions, TiDBV4UpgradeVersion)
 	for _, v := range versions {
 		images = append(images, fmt.Sprintf("pingcap/pd:%s", v))
 		images = append(images, fmt.Sprintf("pingcap/tidb:%s", v))
 		images = append(images, fmt.Sprintf("pingcap/tikv:%s", v))
 	}
+	images = append(images, fmt.Sprintf("%s:%s", PrometheusImage, PrometheusVersion))
+	images = append(images, fmt.Sprintf("%s:%s", TiDBMonitorReloaderImage, TiDBMonitorReloaderVersion))
+	images = append(images, fmt.Sprintf("%s:%s", TiDBMonitorInitializerImage, TiDBMonitorInitializerVersion))
+	images = append(images, fmt.Sprintf("%s:%s", GrafanaImage, GrafanaVersion))
 	imagesFromOperator, err := readImagesFromValues(filepath.Join(framework.TestContext.RepoRoot, "charts/tidb-operator/values.yaml"))
 	if err != nil {
 		framework.ExpectNoError(err)
@@ -137,6 +147,11 @@ func PreloadImages() error {
 			continue
 		}
 		if _, err := nsenter(kindBin, "load", "docker-image", "--name", cluster, "--nodes", strings.Join(nodes, ","), image); err != nil {
+			return err
+		}
+	}
+	for _, image := range images {
+		if _, err := nsenter("docker", "rmi", image); err != nil {
 			return err
 		}
 	}
