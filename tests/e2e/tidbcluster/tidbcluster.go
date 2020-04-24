09c0b4a609bb9250666c94f27f6eba69da3e0b20
Song Gao <2695690803@qq.com>
2020-04-24 20:32:29 +0800

    add check

--- tests/e2e/tidbcluster/tidbcluster.go
+++ tests/e2e/tidbcluster/tidbcluster.go
@@ -530,7 +530,7 @@ var _ = ginkgo.Describe("[tidb-operator] TiDBCluster", func() {
 		framework.ExpectNoError(err)
 
 		// Create TidbCluster with NodePort to check whether node port would change
-		nodeTc := fixture.GetTidbCluster(ns, "nodePort", utilimage.TiDBV3Version)
+		nodeTc := fixture.GetTidbCluster(ns, "nodeport", utilimage.TiDBV3Version)
 		nodeTc.Spec.PD.Replicas = 1
 		nodeTc.Spec.TiKV.Replicas = 1
 		nodeTc.Spec.TiDB.Replicas = 1
@@ -543,7 +543,7 @@ var _ = ginkgo.Describe("[tidb-operator] TiDBCluster", func() {
 		framework.ExpectNoError(err, "Expected TiDB cluster created")
 		err = oa.WaitForTidbClusterReady(tc, 30*time.Minute, 15*time.Second)
 		framework.ExpectNoError(err, "Expected TiDB cluster ready")
-		s, err := c.CoreV1().Services(ns).Get("nodePort-tidb", metav1.GetOptions{})
+		s, err := c.CoreV1().Services(ns).Get("nodeport-tidb", metav1.GetOptions{})
 		framework.ExpectNoError(err)
 		if s.Spec.Type != corev1.ServiceTypeNodePort {
 			framework.Failf("nodePort tidbcluster tidb service type isn't NodePort")
@@ -551,7 +551,7 @@ var _ = ginkgo.Describe("[tidb-operator] TiDBCluster", func() {
 		ports := s.Spec.Ports
 		// check node port unchanged for 5mins
 		err = wait.Poll(5*time.Second, 5*time.Minute, func() (done bool, err error) {
-			s, err := c.CoreV1().Services(ns).Get("nodePort-tidb", metav1.GetOptions{})
+			s, err := c.CoreV1().Services(ns).Get("nodeport-tidb", metav1.GetOptions{})
 			if err != nil {
 				klog.Errorf(err.Error())
 				return true, nil
@@ -570,8 +570,37 @@ var _ = ginkgo.Describe("[tidb-operator] TiDBCluster", func() {
 			}
 			return false, nil
 		})
-		klog.Info("nodePort tidbcluster tidb service NodePort haven't changed")
 		framework.ExpectError(err)
+		klog.Info("nodePort tidbcluster tidb service NodePort haven't changed")
+		tc, err = cli.PingcapV1alpha1().TidbClusters(ns).Get("nodeport", metav1.GetOptions{})
+		framework.ExpectNoError(err)
+		tc.Spec.TiDB.Service.Annotations = map[string]string{
+			"foo": "bar",
+		}
+		_, err = cli.PingcapV1alpha1().TidbClusters(ns).Update(tc)
+		framework.ExpectNoError(err)
+		err = wait.Poll(5*time.Second, 5*time.Minute, func() (done bool, err error) {
+			s, err = c.CoreV1().Services(ns).Get("nodeport-tidb", metav1.GetOptions{})
+			if err != nil {
+				return false, nil
+			}
+			if s.Annotations == nil {
+				return false, nil
+			}
+			v, ok := s.Annotations["foo"]
+			if !ok || v != "bar" {
+				return false, nil
+			}
+			for _, oport := range ports {
+				for _, eport := range s.Spec.Ports {
+					if oport.Port == eport.Port && oport.Protocol == eport.Protocol && oport.NodePort != eport.NodePort {
+						return false, fmt.Errorf("node port has been changed")
+					}
+				}
+			}
+			return true, nil
+		})
+		framework.ExpectNoError(err)
 	})
 
 	updateStrategy := v1alpha1.ConfigUpdateStrategyInPlace
