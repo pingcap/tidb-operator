fbd8cbc46b414906a30839a10d769c3343021af0
shonge <soog2008@hotmail.com>
2020-04-30 18:50:05 +0800

    write unit test in table-driven style.

--- pkg/apis/pingcap/v1alpha1/validation/validation_test.go
+++ pkg/apis/pingcap/v1alpha1/validation/validation_test.go
@@ -17,9 +17,10 @@ import (
 	"strings"
 	"testing"
 
+	. "github.com/onsi/gomega"
 	"github.com/pingcap/tidb-operator/pkg/apis/pingcap/v1alpha1"
 	"github.com/pingcap/tidb-operator/pkg/label"
-	v1 "k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/validation/field"
@@ -182,108 +183,50 @@ func TestValidateAnnotations(t *testing.T) {
 	}
 }
 
-func TestValidateNewTiDBCluster(t *testing.T) {
-	successCases := []struct {
-		name string
-		tc   v1alpha1.TidbCluster
+func TestValidateRequestsStorage(t *testing.T) {
+	g := NewGomegaWithT(t)
+	tests := []struct {
+		name                 string
+		haveRequest          bool
+		resourceRequirements corev1.ResourceRequirements
+		expectedErrors       int
 	}{
 		{
-			name: "all-fields-valid",
-			tc: v1alpha1.TidbCluster{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "test",
-				},
-				Spec: v1alpha1.TidbClusterSpec{
-					Version: "v3.0.8",
-					PD: v1alpha1.PDSpec{
-						BaseImage: "pingcap/pd",
-						Config:    &v1alpha1.PDConfig{},
-						ResourceRequirements: v1.ResourceRequirements{
-							Requests: v1.ResourceList{
-								v1.ResourceStorage: resource.MustParse("10G"),
-							},
-						},
-					},
-					TiKV: v1alpha1.TiKVSpec{
-						BaseImage: "pingcap/tikv",
-						Config:    &v1alpha1.TiKVConfig{},
-						ResourceRequirements: v1.ResourceRequirements{
-							Requests: v1.ResourceList{
-								v1.ResourceStorage: resource.MustParse("10G"),
-							},
-						},
-					},
-					TiDB: v1alpha1.TiDBSpec{
-						BaseImage: "pingcap/tidb",
-						Config:    &v1alpha1.TiDBConfig{},
-					},
+			name:        "has request storage",
+			haveRequest: true,
+			resourceRequirements: corev1.ResourceRequirements{
+				Requests: corev1.ResourceList{
+					corev1.ResourceStorage: resource.MustParse("10G"),
 				},
 			},
+			expectedErrors: 0,
+		},
+		{
+			name:        "Empty request storage",
+			haveRequest: false,
+			resourceRequirements: corev1.ResourceRequirements{
+				Requests: corev1.ResourceList{},
+			},
+			expectedErrors: 2,
 		},
 	}
-	for _, v := range successCases {
-		if errs := ValidateCreateTidbCluster(&v.tc); len(errs) != 0 {
-			t.Errorf("[%s]: unexpected error: %v", v.name, errs)
-		}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tc := newTidbCluster()
+			if tt.haveRequest {
+				tc.Spec.PD.ResourceRequirements = tt.resourceRequirements
+				tc.Spec.TiKV.ResourceRequirements = tt.resourceRequirements
+			}
+			err := ValidateTidbCluster(tc)
+			r := len(err)
+			g.Expect(r).Should(Equal(tt.expectedErrors))
+		})
 	}
+}
 
-	errorCases := []struct {
-		name string
-		tc   v1alpha1.TidbCluster
-		errs []field.Error
-	}{
-		{
-			name: "all-fields-valid",
-			tc: v1alpha1.TidbCluster{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "test",
-				},
-				Spec: v1alpha1.TidbClusterSpec{
-					Version: "v3.0.8",
-					PD: v1alpha1.PDSpec{
-						BaseImage: "pingcap/pd",
-						Config:    &v1alpha1.PDConfig{},
-					},
-					TiKV: v1alpha1.TiKVSpec{
-						BaseImage: "pingcap/tikv",
-						Config:    &v1alpha1.TiKVConfig{},
-					},
-					TiDB: v1alpha1.TiDBSpec{
-						BaseImage: "pingcap/tidb",
-						Config:    &v1alpha1.TiDBConfig{},
-					},
-				},
-			},
-			errs: []field.Error{
-				{
-					Type:   field.ErrorTypeRequired,
-					Detail: `PD or TiKV request storage must not be empty`,
-				},
-				{
-					Type:   field.ErrorTypeRequired,
-					Detail: `PD or TiKV request storage must not be empty`,
-				},
-			},
-		},
-	}
-	for _, v := range errorCases {
-		errs := ValidateCreateTidbCluster(&v.tc)
-		if len(errs) != len(v.errs) {
-			t.Errorf("[%s]: expected %d failures, got %d failures: %v", v.name, len(v.errs), len(errs), errs)
-			continue
-		}
-		for i := range errs {
-			if errs[i].Type != v.errs[i].Type {
-				t.Errorf("[%s]: expected error type %q, got %q", v.name, v.errs[i].Type, errs[i].Type)
-			}
-			if !strings.Contains(errs[i].Detail, v.errs[i].Detail) {
-				t.Errorf("[%s]: expected error errs[i].Detail %q, got %q", v.name, v.errs[i].Detail, errs[i].Detail)
-			}
-			if len(v.errs[i].Field) > 0 {
-				if errs[i].Field != v.errs[i].Field {
-					t.Errorf("[%s]: expected error field %q, got %q", v.name, v.errs[i].Field, errs[i].Field)
-				}
-			}
-		}
-	}
+func newTidbCluster() *v1alpha1.TidbCluster {
+	tc := &v1alpha1.TidbCluster{}
+	tc.Name = "test-validate-requests-storage"
+	tc.Namespace = "default"
+	return tc
 }
