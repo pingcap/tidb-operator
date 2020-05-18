53d6970811eb08937998a6ae411bc35c30ed0085
Yecheng Fu <fuyecheng@pingcap.com>
2020-05-18 09:07:00 +0800

    must cache the clientset in BeforeEach, because f.ClientSet will be nil in inner AfterEach

--- tests/e2e/framework/framework.go
+++ tests/e2e/framework/framework.go
@@ -19,16 +19,21 @@ import (
 	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/kubernetes/test/e2e/framework"
 )
 
 func NewDefaultFramework(baseName string) *framework.Framework {
 	f := framework.NewDefaultFramework(baseName)
+	var c kubernetes.Interface
+	ginkgo.BeforeEach(func() {
+		c = f.ClientSet
+	})
 	ginkgo.AfterEach(func() {
 		// tidb-operator may set persistentVolumeReclaimPolicy to Retain if
 		// users reqeust this. To reduce storage usage, we try to clean them if
 		// namespace is deleted.
-		pvList, err := f.ClientSet.CoreV1().PersistentVolumes().List(metav1.ListOptions{})
+		pvList, err := c.CoreV1().PersistentVolumes().List(metav1.ListOptions{})
 		if err != nil {
 			framework.Logf("failed to list pvs: %v", err)
 			return
@@ -54,7 +59,7 @@ func NewDefaultFramework(baseName string) *framework.Framework {
 				failed++
 				continue
 			}
-			_, err := f.ClientSet.CoreV1().Namespaces().Get(pvcNamespaceName, metav1.GetOptions{})
+			_, err := c.CoreV1().Namespaces().Get(pvcNamespaceName, metav1.GetOptions{})
 			if err != nil && !apierrors.IsNotFound(err) {
 				framework.Logf("failed to get namespace %q: %v", pvcNamespaceName, err)
 				failed++
@@ -65,7 +70,7 @@ func NewDefaultFramework(baseName string) *framework.Framework {
 				continue
 			}
 			pv.Spec.PersistentVolumeReclaimPolicy = v1.PersistentVolumeReclaimDelete
-			_, err = f.ClientSet.CoreV1().PersistentVolumes().Update(&pv)
+			_, err = c.CoreV1().PersistentVolumes().Update(&pv)
 			if err != nil {
 				failed++
 				framework.Logf("failed to set PersistentVolumeReclaimPolicy of PV %q to Delete: %v", pv.Name, err)
