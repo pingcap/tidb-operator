e88d1bd9c6c9a1a5704ec6bae5c5b5abb4714e94
Song Gao <disxiaofei@163.com>
2020-05-19 15:45:22 +0800

    Upgrade e2e 4.0 version and fix 1.12 env error (#2491)

--- tests/e2e/util/image/image.go
+++ tests/e2e/util/image/image.go
@@ -30,7 +30,7 @@ const (
 	TiDBV3Version                 = "v3.0.8"
 	TiDBV3UpgradeVersion          = "v3.0.9"
 	TiDBV4Version                 = "v4.0.0-rc"
-	TiDBV4UpgradeVersion          = "nightly"
+	TiDBV4UpgradeVersion          = "v4.0.0-rc.2"
 	PrometheusImage               = "prom/prometheus"
 	PrometheusVersion             = "v2.11.1"
 	TiDBMonitorReloaderImage      = "pingcap/tidb-monitor-reloader"
