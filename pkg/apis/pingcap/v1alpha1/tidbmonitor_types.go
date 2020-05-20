14274712a9a27263f21d0543c6397b0551236302
mikechengwei <842725815@qq.com>
2020-05-20 22:36:31 +0800

    fix type

--- pkg/apis/pingcap/v1alpha1/tidbmonitor_types.go
+++ pkg/apis/pingcap/v1alpha1/tidbmonitor_types.go
@@ -105,7 +105,7 @@ type PrometheusConfiguration struct {
 // ConfigMapRef is the external configMap
 // +k8s:openapi-gen=true
 type ConfigMapRef struct {
-	Name      string  `json:"name,omitempty"`
+	Name string `json:"name,omitempty"`
 
 	// +optional
 	// if the namespace is omited, the operator controller would use the Tidbmonitor's namespace instead.
