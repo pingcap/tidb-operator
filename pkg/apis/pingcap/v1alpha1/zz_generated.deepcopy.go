63915e64ca27a653f67bb7d6999f5f233b03f49c
vincent178 <vh7157@gmail.com>
2020-05-27 12:59:53 +0800

    auto scaling noise reduction support (#2307)

--- pkg/apis/pingcap/v1alpha1/zz_generated.deepcopy.go
+++ pkg/apis/pingcap/v1alpha1/zz_generated.deepcopy.go
@@ -6837,6 +6837,11 @@ func (in *TidbMonitorStatus) DeepCopy() *TidbMonitorStatus {
 func (in *TikvAutoScalerSpec) DeepCopyInto(out *TikvAutoScalerSpec) {
 	*out = *in
 	in.BasicAutoScalerSpec.DeepCopyInto(&out.BasicAutoScalerSpec)
+	if in.ReadyToScaleThresholdSeconds != nil {
+		in, out := &in.ReadyToScaleThresholdSeconds, &out.ReadyToScaleThresholdSeconds
+		*out = new(int32)
+		**out = **in
+	}
 	return
 }
 
