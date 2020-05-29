5302a534f923361e10d1f41167e63c1e13cdc313
Song Gao <2695690803@qq.com>
2020-05-29 23:41:59 +0800

    add openapi

--- pkg/apis/pingcap/v1alpha1/tikv_config.go
+++ pkg/apis/pingcap/v1alpha1/tikv_config.go
@@ -946,14 +946,12 @@ type TiKVPessimisticTxn struct {
 	// The default and maximum delay before responding to TiDB when pessimistic
 	// transactions encounter locks
 	// +optional
-	// +k8s:openapi-gen=false
 	WaitForLockTimeout *string `json:"wait-for-lock-timeout,omitempty" toml:"wait-for-lock-timeout,omitempty"`
 	// If more than one transaction is waiting for the same lock, only the one with smallest
 	// start timestamp will be waked up immediately when the lock is released. Others will
 	// be waked up after `wake_up_delay_duration` to reduce contention and make the oldest
 	// one more likely acquires the lock.
 	// +optional
-	// +k8s:openapi-gen=false
 	WakeUpDelayDuration *string `json:"wake-up-delay-duration,omitempty" toml:"wake-up-delay-duration,omitempty"`
 	// +optional
 	Pipelined *bool `json:"pipelined,omitempty" toml:"pipelined,omitempty"`
