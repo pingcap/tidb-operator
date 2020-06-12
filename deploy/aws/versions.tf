a00b7bc87c0d45fbd3ba1c4cb3631a8104ce7294
Kolbe Kegel <kolbe@pingcap.com>
2020-06-11 19:58:22 -0700

    cherry pick #2688 to release-1.1
    Signed-off-by: sre-bot <sre-bot@pingcap.com>

--- deploy/aws/versions.tf
+++ deploy/aws/versions.tf
@@ -2,7 +2,7 @@
 terraform {
   required_version = ">= 0.12"
   required_providers {
-    aws      = "~> 2.27"
+    aws      = "~> 2.27, != 2.65.0"
     external = "~> 1.2"
     helm     = "~> 0.10"
     local    = "~> 1.3"
