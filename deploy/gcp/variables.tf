db1ae6dd5af2844cba91b09a3548baef64b39cf8
Kolbe Kegel <kolbe@pingcap.com>
2020-06-17 07:07:50 -0700

    Bumped image versions to v4.0.0 (#2600)
    * Bumped image versions to v4.0.0
    
    * fix backup/restore case
    
    Co-authored-by: pingcap-github-bot <sre-bot@pingcap.com>
    Co-authored-by: Yecheng Fu <fuyecheng@pingcap.com>
    Co-authored-by: DanielZhangQD <36026334+DanielZhangQD@users.noreply.github.com>

--- deploy/gcp/variables.tf
+++ deploy/gcp/variables.tf
@@ -24,7 +24,7 @@ variable "node_locations" {
 
 variable "tidb_version" {
   description = "TiDB version"
-  default     = "v3.0.13"
+  default     = "v4.0.0"
 }
 
 variable "tidb_operator_version" {
