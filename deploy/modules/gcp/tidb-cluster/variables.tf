b70a5e219a93201850f4594a4d6694643031601a
Kolbe Kegel <kolbe@pingcap.com>
2020-06-17 07:07:50 -0700

    cherry pick #2600 to release-1.1
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- deploy/modules/gcp/tidb-cluster/variables.tf
+++ deploy/modules/gcp/tidb-cluster/variables.tf
@@ -9,7 +9,7 @@ variable "tidb_operator_id" {
 variable "cluster_name" {}
 variable "cluster_version" {
   description = "The TiDB cluster version"
-  default     = "v3.0.13"
+  default     = "v4.0.0"
 }
 variable "tidb_cluster_chart_version" {
   description = "The TiDB cluster chart version"
