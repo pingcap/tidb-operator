d6143c8bb65eda03019c232f9c0941a8e175afca
Song Gao <2695690803@qq.com>
2020-06-18 15:23:36 +0800

    add tidbmonitor default policy

--- pkg/monitor/monitor/util.go
+++ pkg/monitor/monitor/util.go
@@ -864,4 +864,8 @@ func defaultTidbMonitor(monitor *v1alpha1.TidbMonitor) {
 		}
 		monitor.Spec.Clusters[id] = tcRef
 	}
+	recyclePVP := core.PersistentVolumeReclaimRecycle
+	if monitor.Spec.PVReclaimPolicy == nil {
+		monitor.Spec.PVReclaimPolicy = &recyclePVP
+	}
 }
