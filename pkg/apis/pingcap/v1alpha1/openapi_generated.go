6635d611de7f2513a697f39ae5f316a4007a5a43
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2020-07-24 22:50:09 +0800

    cherry pick #3002 to release-1.1
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- pkg/apis/pingcap/v1alpha1/openapi_generated.go
+++ pkg/apis/pingcap/v1alpha1/openapi_generated.go
@@ -850,10 +850,10 @@ func schema_pkg_apis_pingcap_v1alpha1_BackupSpec(ref common.ReferenceCallback) c
 							Format:      "",
 						},
 					},
-					"cleanData": {
+					"cleanPolicy": {
 						SchemaProps: spec.SchemaProps{
-							Description: "CleanData denotes whether to clean backup data before the object is deleted from the cluster",
-							Type:        []string{"boolean"},
+							Description: "CleanPolicy denotes whether to clean backup data when the object is deleted from the cluster, if not set, the backup data will be retained",
+							Type:        []string{"string"},
 							Format:      "",
 						},
 					},
