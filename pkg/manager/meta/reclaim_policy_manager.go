36d2833321acf98a630588b75342c98788397cc9
Lonng <heng@lonng.org>
2020-08-19 21:49:16 +0800

    fix lint
    Signed-off-by: Lonng <heng@lonng.org>

--- pkg/manager/meta/reclaim_policy_manager.go
+++ pkg/manager/meta/reclaim_policy_manager.go
@@ -15,6 +15,7 @@ package meta
 
 import (
 	"fmt"
+
 	"github.com/pingcap/tidb-operator/pkg/apis/pingcap/v1alpha1"
 	"github.com/pingcap/tidb-operator/pkg/controller"
 	"github.com/pingcap/tidb-operator/pkg/label"
