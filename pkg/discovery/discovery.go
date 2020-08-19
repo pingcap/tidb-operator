421e9b8f079a6c443ac20d235babed35a6392db2
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2020-08-19 10:49:37 +0800

    fix bug

--- pkg/discovery/discovery.go
+++ pkg/discovery/discovery.go
@@ -130,7 +130,7 @@ func (td *tidbDiscovery) DiscoverDM(advertisePeerUrl string) (string, error) {
 	}
 	klog.Infof("dm advertisePeerUrl is: %s", advertisePeerUrl)
 	strArr := strings.Split(advertisePeerUrl, ".")
-	if len(strArr) != 4 {
+	if len(strArr) != 3 {
 		return "", fmt.Errorf("dm advertisePeerUrl format is wrong: %s", advertisePeerUrl)
 	}
 
