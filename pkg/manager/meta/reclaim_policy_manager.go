7109034b458b4c8d17afe5841f4daa1bb083f70e
Lonng <heng@lonng.org>
2020-08-20 13:27:51 +0800

    cherry pick #3128 to release-1.1
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- pkg/manager/meta/reclaim_policy_manager.go
+++ pkg/manager/meta/reclaim_policy_manager.go
@@ -22,6 +22,8 @@ import (
 	"github.com/pingcap/tidb-operator/pkg/manager"
 	"github.com/pingcap/tidb-operator/pkg/monitor"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	corelisters "k8s.io/client-go/listers/core/v1"
 )
@@ -55,13 +57,14 @@ func NewReclaimPolicyMonitorManager(pvcLister corelisters.PersistentVolumeClaimL
 }
 
 func (rpm *reclaimPolicyManager) Sync(tc *v1alpha1.TidbCluster) error {
-	return rpm.sync(v1alpha1.TiDBClusterKind, tc.GetNamespace(), tc.GetInstanceName(), tc.IsPVReclaimEnabled(), *tc.Spec.PVReclaimPolicy, tc)
+	return rpm.sync(v1alpha1.TiDBClusterKind, tc, tc.IsPVReclaimEnabled(), *tc.Spec.PVReclaimPolicy)
 }
 
 func (rpm *reclaimPolicyManager) SyncMonitor(tm *v1alpha1.TidbMonitor) error {
-	return rpm.sync(v1alpha1.TiDBMonitorKind, tm.GetNamespace(), tm.GetName(), false, *tm.Spec.PVReclaimPolicy, tm)
+	return rpm.sync(v1alpha1.TiDBMonitorKind, tm, false, *tm.Spec.PVReclaimPolicy)
 }
 
+<<<<<<< HEAD
 func (rpm *reclaimPolicyManager) sync(kind, ns, instanceName string, isPVReclaimEnabled bool, policy corev1.PersistentVolumeReclaimPolicy, obj runtime.Object) error {
 	selector, err := label.New().Instance(instanceName).Selector()
 	if err != nil {
@@ -87,11 +90,49 @@ func (rpm *reclaimPolicyManager) sync(kind, ns, instanceName string, isPVReclaim
 				continue
 			}
 		default:
+=======
+func (rpm *reclaimPolicyManager) SyncTiKVGroup(tg *v1alpha1.TiKVGroup, tc *v1alpha1.TidbCluster) error {
+	return rpm.sync(v1alpha1.TiKVGroupKind, tg, tc.IsPVReclaimEnabled(), *tc.Spec.PVReclaimPolicy)
+}
+
+func (rpm *reclaimPolicyManager) sync(kind string, obj runtime.Object, isPVReclaimEnabled bool, policy corev1.PersistentVolumeReclaimPolicy) error {
+	var (
+		meta         = obj.(metav1.ObjectMetaAccessor).GetObjectMeta()
+		ns           = meta.GetNamespace()
+		instanceName = meta.GetName()
+		selector     labels.Selector
+		err          error
+	)
+
+	switch kind {
+	case v1alpha1.TiDBClusterKind:
+		selector, err = label.New().Instance(instanceName).Selector()
+	case v1alpha1.TiDBMonitorKind:
+		selector, err = label.NewMonitor().Instance(instanceName).Monitor().Selector()
+	case v1alpha1.TiKVGroupKind:
+		selector, err = label.NewGroup().Instance(instanceName).TiKV().Selector()
+	default:
+		return fmt.Errorf("unsupported kind %s", kind)
+	}
+	if err != nil {
+		return err
+	}
+
+	pvcs, err := rpm.pvcLister.PersistentVolumeClaims(ns).List(selector)
+	if err != nil {
+		return fmt.Errorf("reclaimPolicyManager.sync: failed to list pvc for %s %s/%s, selector %s, error: %s", kind, ns, instanceName, selector, err)
+	}
+	for _, pvc := range pvcs {
+		if pvc.Spec.VolumeName == "" {
+>>>>>>> 63e0e20... Refactor the reclaim_policy_manager and fix TiKVGroup.GetName() (#3128)
 			continue
 		}
 
 		if isPVReclaimEnabled && len(pvc.Annotations[label.AnnPVCDeferDeleting]) != 0 {
 			// If the pv reclaim function is turned on, and when pv is the candidate pv to be reclaimed, skip patch this pv.
+			continue
+		}
+		if l := label.Label(pvc.Labels); kind == v1alpha1.TiDBClusterKind && (!l.IsPD() && !l.IsTiDB() && !l.IsTiKV() && !l.IsTiFlash() && !l.IsPump()) {
 			continue
 		}
 		pv, err := rpm.pvLister.Get(pvc.Spec.VolumeName)
