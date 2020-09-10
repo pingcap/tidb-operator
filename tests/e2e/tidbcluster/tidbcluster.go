349025fee1b235ec36314b4959e5512283692f3b
mikechengwei <842725815@qq.com>
2020-09-10 20:16:25 +0800

    fix unit test

--- tests/e2e/tidbcluster/tidbcluster.go
+++ tests/e2e/tidbcluster/tidbcluster.go
@@ -1194,10 +1194,10 @@ var _ = ginkgo.Describe("[tidb-operator] TiDBCluster", func() {
 
 			ginkgo.By("Creating heterogeneous tidb cluster")
 			heterogeneousTc := fixture.GetTidbCluster(ns, heterogeneousTcName, utilimage.TiDBV4Version)
-			heterogeneousTc.Spec.PD.Replicas = 0
+			heterogeneousTc.Spec.PD = nil
 			heterogeneousTc.Spec.TiKV.Replicas = 1
 			heterogeneousTc.Spec.TiDB.Replicas = 1
-			heterogeneousTc.Spec.TiFlash = &v1alpha1.TiFlashSpec{Replicas: 1, ComponentSpec: v1alpha1.ComponentSpec{Version: pointer.StringPtr("4.0.5")},
+			heterogeneousTc.Spec.TiFlash = &v1alpha1.TiFlashSpec{Replicas: 1, ComponentSpec: v1alpha1.ComponentSpec{Version: pointer.StringPtr(utilimage.TiDBV4UpgradeVersion)},
 				BaseImage: "pingcap/tiflash", StorageClaims: []v1alpha1.StorageClaim{
 					{Resources: v1.ResourceRequirements{
 						Requests: v1.ResourceList{
