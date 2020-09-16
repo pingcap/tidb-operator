804feacf9706f1701c6af8f73685de7300d0c78b
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2020-09-16 10:22:11 +0800

    bump tidb cluster version to v4.0.6 (#3266)
    Co-authored-by: july2993 <july2993@gmail.com>

--- deploy/aws/variables.tf
+++ deploy/aws/variables.tf
@@ -80,7 +80,7 @@ variable "bastion_instance_type" {
 
 # For aws tutorials compatiablity
 variable "default_cluster_version" {
-  default = "v4.0.4"
+  default = "v4.0.6"
 }
 
 variable "default_cluster_pd_count" {
