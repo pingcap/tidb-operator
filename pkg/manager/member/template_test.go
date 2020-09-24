06bc3078a99a2b74676581bf41edcd46470e78c8
张允禹 <zhangyunyu@zhihu.com>
2020-09-24 18:09:57 +0800

    add render tidb start script ut

--- pkg/manager/member/template_test.go
+++ pkg/manager/member/template_test.go
@@ -20,6 +20,143 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
+func TestRenderTiDBInitStartScript(t *testing.T) {
+	tests := []struct {
+		name          string
+		path          string
+		clusterDomain string
+		result        string
+	}{
+		{
+			name:          "basic",
+			path:          "cluster01-pd:2379",
+			clusterDomain: "",
+			result:        `#!/bin/sh
+
+# This script is used to start tidb containers in kubernetes cluster
+
+# Use DownwardAPIVolumeFiles to store informations of the cluster:
+# https://kubernetes.io/docs/tasks/inject-data-application/downward-api-volume-expose-pod-information/#the-downward-api
+#
+#   runmode="normal/debug"
+#
+set -uo pipefail
+
+ANNOTATIONS="/etc/podinfo/annotations"
+
+if [[ ! -f "${ANNOTATIONS}" ]]
+then
+    echo "${ANNOTATIONS} does't exist, exiting."
+    exit 1
+fi
+source ${ANNOTATIONS} 2>/dev/null
+runmode=${runmode:-normal}
+if [[ X${runmode} == Xdebug ]]
+then
+    echo "entering debug mode."
+    tail -f /dev/null
+fi
+
+# Use HOSTNAME if POD_NAME is unset for backward compatibility.
+POD_NAME=${POD_NAME:-$HOSTNAME}
+ARGS="--store=tikv \
+--advertise-address=${POD_NAME}.${HEADLESS_SERVICE_NAME}.${NAMESPACE}.svc \
+--host=0.0.0.0 \
+--path=cluster01-pd:2379 \
+--config=/etc/tidb/tidb.toml
+"
+
+if [[ X${BINLOG_ENABLED:-} == Xtrue ]]
+then
+    ARGS="${ARGS} --enable-binlog=true"
+fi
+
+SLOW_LOG_FILE=${SLOW_LOG_FILE:-""}
+if [[ ! -z "${SLOW_LOG_FILE}" ]]
+then
+    ARGS="${ARGS} --log-slow-query=${SLOW_LOG_FILE:-}"
+fi
+
+echo "start tidb-server ..."
+echo "/tidb-server ${ARGS}"
+exec /tidb-server ${ARGS}
+`,
+		},
+		{
+			name:          "basic with cluster domain",
+			path:          "cluster01-pd:2379",
+			clusterDomain: "test.com",
+			result:        `#!/bin/sh
+
+# This script is used to start tidb containers in kubernetes cluster
+
+# Use DownwardAPIVolumeFiles to store informations of the cluster:
+# https://kubernetes.io/docs/tasks/inject-data-application/downward-api-volume-expose-pod-information/#the-downward-api
+#
+#   runmode="normal/debug"
+#
+set -uo pipefail
+
+ANNOTATIONS="/etc/podinfo/annotations"
+
+if [[ ! -f "${ANNOTATIONS}" ]]
+then
+    echo "${ANNOTATIONS} does't exist, exiting."
+    exit 1
+fi
+source ${ANNOTATIONS} 2>/dev/null
+runmode=${runmode:-normal}
+if [[ X${runmode} == Xdebug ]]
+then
+    echo "entering debug mode."
+    tail -f /dev/null
+fi
+
+# Use HOSTNAME if POD_NAME is unset for backward compatibility.
+POD_NAME=${POD_NAME:-$HOSTNAME}
+ARGS="--store=tikv \
+--advertise-address=${POD_NAME}.${HEADLESS_SERVICE_NAME}.${NAMESPACE}.svc.test.com \
+--host=0.0.0.0 \
+--path=cluster01-pd:2379 \
+--config=/etc/tidb/tidb.toml
+"
+
+if [[ X${BINLOG_ENABLED:-} == Xtrue ]]
+then
+    ARGS="${ARGS} --enable-binlog=true"
+fi
+
+SLOW_LOG_FILE=${SLOW_LOG_FILE:-""}
+if [[ ! -z "${SLOW_LOG_FILE}" ]]
+then
+    ARGS="${ARGS} --log-slow-query=${SLOW_LOG_FILE:-}"
+fi
+
+echo "start tidb-server ..."
+echo "/tidb-server ${ARGS}"
+exec /tidb-server ${ARGS}
+`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			model := TidbStartScriptModel{
+				EnablePlugin:  false,
+				ClusterDomain: tt.clusterDomain,
+				Path:          "cluster01-pd:2379",
+			}
+			script, err := RenderTiDBStartScript(&model)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if diff := cmp.Diff(tt.result, script); diff != "" {
+				t.Errorf("unexpected (-want, +got): %s", diff)
+			}
+		})
+	}
+}
+
 func TestRenderTiKVStartScript(t *testing.T) {
 	tests := []struct {
 		name                string
