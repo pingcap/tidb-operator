1b078c5543df18992545fea0ffa5a2b623c3db90
Mike <842725815@qq.com>
2020-09-30 12:50:35 +0800

    TidbMonitor monitor multiple cluster crossed multiple namespaces in non-TLS environment (#3308)
    * multiple cluster cross multiple namespaces
    
    Co-authored-by: DanielZhangQD <36026334+DanielZhangQD@users.noreply.github.com>
    
    * optimize doc
    * multiple cluster monitor example
    
    Co-authored-by: DanielZhangQD <36026334+DanielZhangQD@users.noreply.github.com>

--- pkg/monitor/monitor/monitor_manager.go
+++ pkg/monitor/monitor/monitor_manager.go
@@ -355,18 +355,36 @@ func (mm *MonitorManager) syncTidbMonitorRbac(monitor *v1alpha1.TidbMonitor) (*c
 		})
 	}
 
-	role := getMonitorRole(monitor, policyRules)
-	role, err = mm.typedControl.CreateOrUpdateRole(monitor, role)
-	if err != nil {
-		klog.Errorf("tm[%s/%s]'s role failed to sync,err: %v", monitor.Namespace, monitor.Name, err)
-		return nil, err
-	}
+	if monitor.Spec.ClusterScoped {
+		role := getMonitorClusterRole(monitor, policyRules)
+		role, err = mm.typedControl.CreateOrUpdateClusterRole(monitor, role)
+		if err != nil {
+			klog.Errorf("tm[%s/%s]'s clusterrole failed to sync, err: %v", monitor.Namespace, monitor.Name, err)
+			return nil, err
+		}
 
-	rb := getMonitorRoleBinding(sa, role, monitor)
-	_, err = mm.typedControl.CreateOrUpdateRoleBinding(monitor, rb)
-	if err != nil {
-		klog.Errorf("tm[%s/%s]'s rolebinding failed to sync,err: %v", monitor.Namespace, monitor.Name, err)
-		return nil, err
+		rb := getMonitorClusterRoleBinding(sa, role, monitor)
+
+		_, err = mm.typedControl.CreateOrUpdateClusterRoleBinding(monitor, rb)
+		if err != nil {
+			klog.Errorf("tm[%s/%s]'s clusterrolebinding failed to sync, err: %v", monitor.Namespace, monitor.Name, err)
+			return nil, err
+		}
+	} else {
+		role := getMonitorRole(monitor, policyRules)
+		role, err = mm.typedControl.CreateOrUpdateRole(monitor, role)
+		if err != nil {
+			klog.Errorf("tm[%s/%s]'s role failed to sync,err: %v", monitor.Namespace, monitor.Name, err)
+			return nil, err
+		}
+
+		rb := getMonitorRoleBinding(sa, role, monitor)
+
+		_, err = mm.typedControl.CreateOrUpdateRoleBinding(monitor, rb)
+		if err != nil {
+			klog.Errorf("tm[%s/%s]'s rolebinding failed to sync,err: %v", monitor.Namespace, monitor.Name, err)
+			return nil, err
+		}
 	}
 
 	return sa, nil
