e73736a7fb2e84afac7bb9da71a9c2dfdbb98ae8
Howard Lau <howardlau1999@hotmail.com>
2020-10-13 02:46:08 +0000

    log
    Signed-off-by: Howard Lau <howardlau1999@hotmail.com>

--- tests/e2e/tidbcluster/serial.go
+++ tests/e2e/tidbcluster/serial.go
@@ -611,6 +611,7 @@ var _ = ginkgo.Describe("[tidb-operator][Serial]", func() {
 			tc := fixture.GetTidbCluster(ns, clusterName, "nightly")
 			tc.Spec.PD.Replicas = 1
 			tc.Spec.TiKV.Replicas = 3
+			tc.Spec.TiKV.Version = pointer.StringPtr("v4.0.6")
 			tc.Spec.TiDB.Replicas = 2
 			tc.Spec.PD.Config.Set("pd-server.metric-storage", "http://monitor-prometheus:9090")
 			tc.Spec.PD.BaseImage = "hub.pingcap.net/lhh/pd"
