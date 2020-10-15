f30eef72f653441951507420c9f9f45448be78fd
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2020-10-15 21:11:04 +0800

    cherry pick #3379 to release-1.1 (#3382)
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>
    
    Co-authored-by: july2993 <july2993@gmail.com>
    Co-authored-by: DanielZhangQD <36026334+DanielZhangQD@users.noreply.github.com>

--- deploy/modules/aliyun/tidb-cluster/variables.tf
+++ deploy/modules/aliyun/tidb-cluster/variables.tf
@@ -12,7 +12,7 @@ variable "image_id" {
 
 variable "tidb_version" {
   description = "TiDB cluster version"
-  default     = "v4.0.6"
+  default     = "v4.0.7"
 }
 
 variable "tidb_cluster_chart_version" {
