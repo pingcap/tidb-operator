d1e9a6f916a3435b598fcf668a49e208ad68dbb3
Jiahao Huang <july2993@gmail.com>
2020-10-15 11:19:19 +0800

    bump cluster version to v4.0.7 & chart version to v1.1.6

--- deploy/gcp/variables.tf
+++ deploy/gcp/variables.tf
@@ -24,11 +24,11 @@ variable "node_locations" {
 
 variable "tidb_version" {
   description = "TiDB version"
-  default     = "v4.0.6"
+  default     = "v4.0.7"
 }
 
 variable "tidb_operator_version" {
-  default = "v1.1.5"
+  default = "v1.1.6"
 }
 
 variable "tidb_operator_chart_version" {
