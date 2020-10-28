1cc2f6fa1f7d824baa3298f3738d8683e0409283
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2020-10-28 08:49:55 +0800

    heterogeneous TiKV cluster cannot be scaled in to less than 3 replicas (#3428) (#3432)
    * cherry pick #3428 to release-1.1
    
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>
    
    * fix conflict
    
    Co-authored-by: weizhi <ftdchenwz@gmail.com>

--- pkg/manager/member/tikv_scaler.go
+++ pkg/manager/member/tikv_scaler.go
@@ -79,33 +79,8 @@ func (s *tikvScaler) ScaleIn(tc *v1alpha1.TidbCluster, oldSet *apps.StatefulSet,
 		return fmt.Errorf("tikvScaler.ScaleIn: failed to get pods %s for cluster %s/%s, error: %s", podName, ns, tcName, err)
 	}
 
-	upNumber := 0
-	storeState := ""
-	for _, store := range tc.Status.TiKV.Stores {
-		if store.State == v1alpha1.TiKVStateUp {
-			upNumber++
-		}
-		if store.PodName == podName {
-			storeState = store.State
-		}
-	}
-	config, err := controller.GetPDClient(s.deps.PDControl, tc).GetConfig()
-	if err != nil {
+	if pass, err := s.preCheckUpStores(tc, podName); !pass {
 		return err
-	}
-	maxReplicas := *(config.Replication.MaxReplicas)
-	if upNumber < int(maxReplicas) {
-		errMsg := fmt.Sprintf("the number of stores in Up state of TidbCluster [%s/%s] is %d, less than MaxReplicas in PD configuration(%d), can't scale in TiKV, podname %s ", tc.GetNamespace(), tc.GetName(), upNumber, maxReplicas, podName)
-		klog.Error(errMsg)
-		s.deps.Recorder.Event(tc, v1.EventTypeWarning, "FailedScaleIn", errMsg)
-		return nil
-	} else if upNumber == int(maxReplicas) {
-		if storeState == v1alpha1.TiKVStateUp {
-			errMsg := fmt.Sprintf("can't scale in TiKV of TidbCluster [%s/%s], cause the number of up stores is equal to MaxReplicas in PD configuration(%d), and the store in Pod %s which is going to be deleted is up too", tc.GetNamespace(), tc.GetName(), maxReplicas, podName)
-			klog.Error(errMsg)
-			s.deps.Recorder.Event(tc, v1.EventTypeWarning, "FailedScaleIn", errMsg)
-			return nil
-		}
 	}
 
 	if s.deps.CLIConfig.PodWebhookEnabled {
@@ -231,6 +206,50 @@ func (s *tikvScaler) SyncAutoScalerAnn(tc *v1alpha1.TidbCluster, actual *apps.St
 		return nil
 	}
 	return nil
+}
+
+func (s *tikvScaler) preCheckUpStores(tc *v1alpha1.TidbCluster, podName string) (bool, error) {
+	pdClient := controller.GetPDClient(s.deps.PDControl, tc)
+	// get the number of stores whose state is up
+	upNumber := 0
+	storesInfo, err := pdClient.GetStores()
+	if err != nil {
+		return false, fmt.Errorf("failed to get stores info in TidbCluster %s/%s", tc.GetNamespace(), tc.GetName())
+	}
+	for _, store := range storesInfo.Stores {
+		if store.Store != nil && store.Store.StateName == v1alpha1.TiKVStateUp {
+			upNumber++
+		}
+	}
+
+	// get the state of the store which is about to be scaled in
+	storeState := ""
+	for _, store := range tc.Status.TiKV.Stores {
+		if store.PodName == podName {
+			storeState = store.State
+		}
+	}
+
+	config, err := pdClient.GetConfig()
+	if err != nil {
+		return false, err
+	}
+	maxReplicas := *(config.Replication.MaxReplicas)
+	if upNumber < int(maxReplicas) {
+		errMsg := fmt.Sprintf("the number of stores in Up state of TidbCluster [%s/%s] is %d, less than MaxReplicas in PD configuration(%d), can't scale in TiKV, podname %s ", tc.GetNamespace(), tc.GetName(), upNumber, maxReplicas, podName)
+		klog.Error(errMsg)
+		s.deps.Recorder.Event(tc, v1.EventTypeWarning, "FailedScaleIn", errMsg)
+		return false, nil
+	} else if upNumber == int(maxReplicas) {
+		if storeState == v1alpha1.TiKVStateUp {
+			errMsg := fmt.Sprintf("can't scale in TiKV of TidbCluster [%s/%s], cause the number of up stores is equal to MaxReplicas in PD configuration(%d), and the store in Pod %s which is going to be deleted is up too", tc.GetNamespace(), tc.GetName(), maxReplicas, podName)
+			klog.Error(errMsg)
+			s.deps.Recorder.Event(tc, v1.EventTypeWarning, "FailedScaleIn", errMsg)
+			return false, nil
+		}
+	}
+
+	return true, nil
 }
 
 type fakeTiKVScaler struct{}
