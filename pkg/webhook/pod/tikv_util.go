af4e4de5297acefd77d7b37e6f67b5a716e84bb9
handlerww <lohipoo@yeah.net>
2020-11-03 11:44:17 +0800

    Fix PD and TiKV member name error when multi-cluster enabled in webhook.

--- pkg/webhook/pod/tikv_util.go
+++ pkg/webhook/pod/tikv_util.go
@@ -152,7 +152,7 @@ func getStoreByPod(pod *core.Pod, storesInfo *pdapi.StoresInfo) (*pdapi.StoreInf
 	namespace := pod.Namespace
 
 	for _, store := range storesInfo.Stores {
-		// It works when multi-cluster is enabled, the stores out of the cluster are listed in PeerStores.
+		// It also works well when multi-cluster is enabled, the stores out of the cluster are listed in PeerStores.
 		ip := strings.Split(store.Store.GetAddress(), ":")[0]
 		podName := strings.Split(ip, ".")[0]
 		if podName == name {
