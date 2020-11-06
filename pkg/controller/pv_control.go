fe6a0822f95c81bcebbffd52b065b49400216ff2
mikechengwei <842725815@qq.com>
2020-11-07 04:09:22 +0800

    optimize code

--- pkg/controller/pv_control.go
+++ pkg/controller/pv_control.go
@@ -183,11 +183,23 @@ func (c *realPVControl) UpdatePVInfo(obj runtime.Object, pv *corev1.PersistentVo
 	pvName := pv.Name
 	var updatePV *corev1.PersistentVolume
 	klog.Infof("PV: [%s] updated pv claimRef new xxx , %s: %s/%s", pvName, kind, ns, name)
-	updatePV, updateErr := c.kubeCli.CoreV1().PersistentVolumes().Update(pv)
-	if updateErr == nil {
-		klog.Errorf("PV: [%s] updated successfully, %s: %s/%s %s", pvName, kind, ns, name, pv.Spec.ClaimRef.Name)
-		return nil, nil
-	}
+	updateErr := retry.RetryOnConflict(retry.DefaultBackoff, func() error {
+		var updateErr error
+		updatePV, updateErr = c.kubeCli.CoreV1().PersistentVolumes().Update(pv)
+		if updateErr == nil {
+			klog.Infof("PV: [%s] updated successfully, %s: %s/%s", pvName, kind, ns, name)
+			return nil
+		}
+		klog.Errorf("failed to update PV: [%s], %s %s/%s, error: %v", pvName, kind, ns, name, err)
+
+		if updated, err := c.pvLister.Get(pvName); err == nil {
+			// make a copy so we don't mutate the shared cache
+			pv = updated.DeepCopy()
+		} else {
+			utilruntime.HandleError(fmt.Errorf("error getting updated PV %s/%s from lister: %v", ns, pvName, err))
+		}
+		return updateErr
+	})
 	klog.Errorf("failed to update PV: [%s], %s %s/%s %S , error: %v", pvName, kind, ns, name, pv.Spec.ClaimRef.Name, updateErr)
 
 	return updatePV, updateErr
