57b7f94061d758a843ede7a1f058b75e73641548
mikechengwei <842725815@qq.com>
2020-11-10 13:36:07 +0800

    optimize code

--- pkg/manager/meta/reclaim_policy_manager.go
+++ pkg/manager/meta/reclaim_policy_manager.go
@@ -14,6 +14,8 @@
 package meta
 
 import (
+	"fmt"
+
 	"github.com/pingcap/tidb-operator/pkg/apis/pingcap/v1alpha1"
 	"github.com/pingcap/tidb-operator/pkg/controller"
 	"github.com/pingcap/tidb-operator/pkg/label"
