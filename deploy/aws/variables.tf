d6c5a5d17566b3c85636b627bcd01cf1fe805417
Yilong Li <liyilongko@gmail.com>
2020-11-12 21:27:21 +0800

    cherry pick #3490 to release-1.1
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- deploy/aws/variables.tf
+++ deploy/aws/variables.tf
@@ -19,7 +19,7 @@ variable "eks_version" {
 
 variable "operator_version" {
   description = "TiDB operator version"
-  default     = "v1.1.6"
+  default     = "v1.1.7"
 }
 
 variable "operator_values" {
@@ -80,7 +80,7 @@ variable "bastion_instance_type" {
 
 # For aws tutorials compatiablity
 variable "default_cluster_version" {
-  default = "v4.0.7"
+  default = "v4.0.8"
 }
 
 variable "default_cluster_pd_count" {
