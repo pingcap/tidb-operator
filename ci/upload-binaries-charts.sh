636481bf097381c85d8ad179390a7dd90acc607d
DanielZhangQD <zhanghailong810@aliyun.com>
2020-11-13 23:53:20 +0800

    update the path of filemgr

--- ci/upload-binaries-charts.sh
+++ ci/upload-binaries-charts.sh
@@ -63,15 +63,15 @@ function upload() {
 EOF
 
     echo "info: downloading filemgr from $FILEMGR_URL"
-    curl --retry 10 -L -s "$FILEMGR_URL" | tar --strip-components 1 -C $dir -xzvf - filemgr-linux64.elf/filemgr-linux64
+    curl --retry 10 -L -s "$FILEMGR_URL" | tar --strip-components 1 -C $dir -xzvf -
 
     echo "info: uploading charts and binaries"
     tar -zcvf $dir/tidb-operator.tar.gz images/tidb-operator images/tidb-backup-manager charts
-    $dir/filemgr-linux64 --config $dir/config.cfg --action mput --bucket ${UCLOUD_UFILE_BUCKET} --nobar --key builds/pingcap/operator/${GITHASH}/centos7/tidb-operator.tar.gz --file $dir/tidb-operator.tar.gz
+    $dir/linux64/filemgr-linux64 --config $dir/linux64/config.cfg --action mput --bucket ${UCLOUD_UFILE_BUCKET} --nobar --key builds/pingcap/operator/${GITHASH}/centos7/tidb-operator.tar.gz --file $dir/tidb-operator.tar.gz
 
     echo "info: update ref of branch '$BUILD_BRANCH'"
     echo -n $GITHASH > $dir/sha1
-	$dir/filemgr-linux64 --config $dir/config.cfg --action mput --bucket ${UCLOUD_UFILE_BUCKET} --nobar --key refs/pingcap/operator/${BUILD_BRANCH}/centos7/sha1 --file $dir/sha1
+	$dir/linux64/filemgr-linux64 --config $dir/linux64/config.cfg --action mput --bucket ${UCLOUD_UFILE_BUCKET} --nobar --key refs/pingcap/operator/${BUILD_BRANCH}/centos7/sha1 --file $dir/sha1
 }
 
 # retry a few times until it succeeds, this can avoid temporary network flakes
