d46b32d734f90f40933d16b7081bbbbcc9b6a0de
mikechengwei <842725815@qq.com>
2020-11-29 14:05:59 +0800

    optimize code

--- ci/pull_e2e_kind.groovy
+++ ci/pull_e2e_kind.groovy
@@ -305,7 +305,7 @@ try {
                             sh """#!/bin/bash
                             set -eu
                             echo "info: logging into hub.pingcap.net"
-                            docker login hub.docker.com  -u mikechengwei -p  aini1314
+                            docker login -u \$USERNAME --password-stdin hub.pingcap.net <<< \$PASSWORD
                             echo "info: build and push images for e2e"
                             NO_BUILD=y DOCKER_REPO=hub.pingcap.net/tidb-operator-e2e IMAGE_TAG=${IMAGE_TAG} make docker-push e2e-docker-push
                             echo "info: download binaries for e2e"
