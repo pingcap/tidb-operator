fafcea26c20dc3a423219df1506320b0d531bdaa
sstubbs <stephen.stubbs@vanqor.com>
2020-11-30 03:47:53 +0000

    cherry pick #3521 to release-1.1
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- cmd/backup-manager/app/import/import.go
+++ cmd/backup-manager/app/import/import.go
@@ -81,7 +81,7 @@ func (ro *Options) downloadBackupData(localPath string, opts []string) error {
 	return nil
 }
 
-func (ro *Options) loadTidbClusterData(restorePath string) error {
+func (ro *Options) loadTidbClusterData(restorePath string, tableFilter []string) error {
 	if exist := backupUtil.IsDirExist(restorePath); !exist {
 		return fmt.Errorf("dir %s does not exist or is not a dir", restorePath)
 	}
@@ -97,6 +97,11 @@ func (ro *Options) loadTidbClusterData(restorePath string) error {
 		fmt.Sprintf("--d=%s", restorePath),
 		fmt.Sprintf("--tidb-port=%d", ro.Port),
 	}
+
+	for _, filter := range tableFilter {
+		args = append(args, "-f", filter)
+	}
+
 	if ro.TLSClient {
 		args = append(args, fmt.Sprintf("--ca=%s", path.Join(util.TiDBClientTLSPath, corev1.ServiceAccountRootCAKey)))
 		args = append(args, fmt.Sprintf("--cert=%s", path.Join(util.TiDBClientTLSPath, corev1.TLSCertKey)))
