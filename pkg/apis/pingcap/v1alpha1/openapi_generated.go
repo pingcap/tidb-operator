db192a38b016f07003a87392202c43a1565f4ed5
BinChenn <binchinsmail@gmail.com>
2020-12-11 17:55:08 +0800

    allow empty secret in gcs BR

--- pkg/apis/pingcap/v1alpha1/openapi_generated.go
+++ pkg/apis/pingcap/v1alpha1/openapi_generated.go
@@ -2461,7 +2461,7 @@ func schema_pkg_apis_pingcap_v1alpha1_GcsStorageProvider(ref common.ReferenceCal
 						},
 					},
 				},
-				Required: []string{"projectId", "secretName"},
+				Required: []string{"projectId"},
 			},
 		},
 	}
