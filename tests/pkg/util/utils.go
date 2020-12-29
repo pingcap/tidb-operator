c2a3f1ec17cc4041f5048d644abed4421ecfae5b
Yilong Li <liyilongko@gmail.com>
2020-12-29 16:47:45 +0800

    Unify e2e test logging (#3639)
    * refactor a bit, add comments
    
    * upgrade shell to the version in pingcap/advanced-statefulset
    
    * prepend "k8s" to distinguish between tidb-operator e2e libraries
    
    * replace klog with k8se2elog in tests/
    
    * fix compile
    
    * prepend ERROR: in error logs
    
    * rename k8se2elog to log
    
    * rename k8se2epod to pod, k8se2enode to node
    
    * remove empty import line
    
    * fix compile
    
    * fix compile
    
    Co-authored-by: DanielZhangQD <36026334+DanielZhangQD@users.noreply.github.com>

--- tests/pkg/util/utils.go
+++ tests/pkg/util/utils.go
@@ -19,7 +19,7 @@ import (
 	"os/exec"
 	"strings"
 
-	"k8s.io/klog"
+	"k8s.io/kubernetes/test/e2e/framework/log"
 )
 
 const (
@@ -70,7 +70,7 @@ func ListK8sNodes(kubectlPath, labels string) ([]string, error) {
 	if len(nodes) == 0 {
 		return nil, fmt.Errorf("get k8s nodes is empty")
 	}
-	klog.Infof("get k8s nodes success: %s, labels: %s", nodes, labels)
+	log.Logf("get k8s nodes success: %s, labels: %s", nodes, labels)
 	return nodes, nil
 }
 
