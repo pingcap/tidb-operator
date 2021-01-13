3805a59acbf6bdf3d84a84c3375b9c8cb74b290f
weizhi <ftdchenwz@gmail.com>
2021-01-13 13:30:28 +0800

    sync controller revision

--- pkg/util/conversion/upgrade.go
+++ pkg/util/conversion/upgrade.go
@@ -1,6 +1,11 @@
 package conversion
 
 import (
+	"fmt"
+	"time"
+
+	"k8s.io/client-go/util/retry"
+
 	kruisev1beta1 "github.com/openkruise/kruise-api/apps/v1beta1"
 	kruiseclientset "github.com/openkruise/kruise-api/client/clientset/versioned"
 	asappsv1 "github.com/pingcap/advanced-statefulset/client/apis/apps/v1"
@@ -40,12 +45,13 @@ func UpgradeFromSts(c clientset.Interface, kruiseCli kruiseclientset.Interface, 
 	if err != nil {
 		return nil, err
 	}
-	for _, revision := range oldRevisionList.Items {
+	for _, r := range oldRevisionList.Items {
+		revision := r.DeepCopy()
 		for key := range sts.Spec.Selector.MatchLabels {
 			delete(revision.Labels, key)
 		}
 		revision.Labels[UpgradeToKruiseAstsAnn] = sts.Name
-		_, err = c.AppsV1().ControllerRevisions(revision.Namespace).Update(&revision)
+		_, err = c.AppsV1().ControllerRevisions(revision.Namespace).Update(revision)
 		if err != nil {
 			return nil, err
 		}
@@ -53,7 +59,7 @@ func UpgradeFromSts(c clientset.Interface, kruiseCli kruiseclientset.Interface, 
 	klog.V(2).Infof("Succesfully marked all controller revisions (%d) of StatefulSet %s/%s", len(oldRevisionList.Items), sts.Namespace, sts.Name)
 
 	// Create or Update
-	asts, err := kruiseCli.AppsV1beta1().StatefulSets(sts.Namespace).Get(sts.Name, metav1.GetOptions{})
+	kAsts, err := kruiseCli.AppsV1beta1().StatefulSets(sts.Namespace).Get(sts.Name, metav1.GetOptions{})
 	if err != nil && !apierrors.IsNotFound(err) {
 		return nil, err
 	}
@@ -63,36 +69,36 @@ func UpgradeFromSts(c clientset.Interface, kruiseCli kruiseclientset.Interface, 
 		return nil, err
 	}
 	if notFound {
-		asts = upgradedSts.DeepCopy()
+		kAsts = upgradedSts.DeepCopy()
 		// https://github.com/kubernetes/apiserver/blob/kubernetes-1.16.0/pkg/storage/etcd3/store.go#L141-L143
-		asts.ObjectMeta.ResourceVersion = ""
+		kAsts.ObjectMeta.ResourceVersion = ""
 		// https://kubernetes.io/docs/reference/using-api/api-concepts/#server-side-apply
 		// old ManagedFields belongs to apps/v1 and kube-controller-manager,
 		// nil it and the ownership will be transferred to
 		// advanced-statefulset-controller-manager
-		asts.ObjectMeta.ManagedFields = nil
-		asts, err = kruiseCli.AppsV1beta1().StatefulSets(asts.Namespace).Create(asts)
+		kAsts.ObjectMeta.ManagedFields = nil
+		kAsts, err = kruiseCli.AppsV1beta1().StatefulSets(kAsts.Namespace).Create(kAsts)
 		if err != nil {
 			return nil, err
 		}
-		klog.V(2).Infof("Succesfully created the new Advanced StatefulSet %s/%s", asts.Namespace, asts.Name)
+		klog.V(2).Infof("Succesfully created the new Advanced StatefulSet %s/%s", kAsts.Namespace, kAsts.Name)
 	} else {
-		asts.Spec = upgradedSts.Spec
-		asts, err = kruiseCli.AppsV1beta1().StatefulSets(asts.Namespace).Update(asts)
+		kAsts.Spec = upgradedSts.Spec
+		kAsts, err = kruiseCli.AppsV1beta1().StatefulSets(kAsts.Namespace).Update(kAsts)
 		if err != nil {
 			return nil, err
 		}
-		klog.V(2).Infof("Succesfully updated the Advanced StatefulSet %s/%s", asts.Namespace, asts.Name)
+		klog.V(2).Infof("Succesfully updated the Advanced StatefulSet %s/%s", kAsts.Namespace, kAsts.Name)
 	}
 
 	// Status must be updated via UpdateStatus
-	asts.Status = upgradedSts.Status
-	asts, err = kruiseCli.AppsV1beta1().StatefulSets(asts.Namespace).UpdateStatus(asts)
+	kAsts.Status = upgradedSts.Status
+	kAsts, err = kruiseCli.AppsV1beta1().StatefulSets(kAsts.Namespace).UpdateStatus(kAsts)
 	if err != nil {
 		return nil, err
 	}
 
-	// At the last, delete the builtin StatefulSet
+	// delete the builtin StatefulSet
 	policy := metav1.DeletePropagationOrphan
 	err = c.AppsV1().StatefulSets(sts.Namespace).Delete(sts.Name, &metav1.DeleteOptions{
 		PropagationPolicy: &policy,
@@ -103,12 +109,25 @@ func UpgradeFromSts(c clientset.Interface, kruiseCli kruiseclientset.Interface, 
 	}
 	klog.V(2).Infof("Succesfully deleted the old builtin StatefulSet %s/%s", sts.Namespace, sts.Name)
 
-	// After old StatefulSet was deleted actually from etcd, we sync the label to controller revisions
-	return asts, nil
+	// We wait for old StatefulSet to be deleted actually from etcd, then write back the labels to controller revisions
+	// just like what is done in Advanced-Statefulset: https://github.com/pingcap/advanced-statefulset/blob/master/pkg/controller/statefulset/stateful_set.go#L358-L363
+	for i := 0; i < 5; i++ {
+		_, err = c.AppsV1().StatefulSets(sts.Namespace).Get(sts.Name, metav1.GetOptions{})
+		if err != nil && apierrors.IsNotFound(err) {
+			if err = syncControllerRevision(c, oldRevisionList); err != nil {
+				return nil, err
+			}
+			klog.V(2).Infof("successfully sync controller revisions owned by %s/%s", sts.Namespace, sts.Name)
+			return kAsts, nil
+		}
+		time.Sleep(time.Duration(6) * time.Second)
+	}
+
+	return nil, fmt.Errorf("failed to sync controller revisions: statefulset %s/%s is not deleted from etcd", sts.Namespace, sts.Name)
 }
 
 // UpgradeFromAsts upgrades PingCAP Advanced StatefulSet to Kruise Advanced StatefulSet.
-// Its procedure is basically the same as UpgradeFromSts
+// The procedure is basically the same as UpgradeFromSts
 func UpgradeFromAsts(c clientset.Interface, asCli asclientset.Interface, kruiseCli kruiseclientset.Interface, asts *asappsv1.StatefulSet) (*kruisev1beta1.StatefulSet, error) {
 	selector, err := metav1.LabelSelectorAsSelector(asts.Spec.Selector)
 	if err != nil {
@@ -123,12 +142,13 @@ func UpgradeFromAsts(c clientset.Interface, asCli asclientset.Interface, kruiseC
 	if err != nil {
 		return nil, err
 	}
-	for _, revision := range oldRevisionList.Items {
+	for _, r := range oldRevisionList.Items {
+		revision := r.DeepCopy()
 		for key := range asts.Spec.Selector.MatchLabels {
 			delete(revision.Labels, key)
 		}
 		revision.Labels[UpgradeToKruiseAstsAnn] = asts.Name
-		_, err = c.AppsV1().ControllerRevisions(revision.Namespace).Update(&revision)
+		_, err = c.AppsV1().ControllerRevisions(revision.Namespace).Update(revision)
 		if err != nil {
 			return nil, err
 		}
@@ -180,7 +200,7 @@ func UpgradeFromAsts(c clientset.Interface, asCli asclientset.Interface, kruiseC
 		return nil, err
 	}
 
-	// At the last, delete the PingCAP Advanced StatefulSet
+	// delete the PingCAP Advanced StatefulSet
 	policy := metav1.DeletePropagationOrphan
 	err = asCli.AppsV1().StatefulSets(asts.Namespace).Delete(asts.Name, &metav1.DeleteOptions{
 		PropagationPolicy: &policy,
@@ -190,5 +210,37 @@ func UpgradeFromAsts(c clientset.Interface, asCli asclientset.Interface, kruiseC
 		return nil, err
 	}
 	klog.V(2).Infof("Succesfully deleted the old Pingcap Advanced StatefulSet %s/%s", asts.Namespace, asts.Name)
-	return kruiseAsts, nil
+
+	// After old StatefulSet was deleted actually from etcd, we sync the labels to controller revisions
+	// Just like what is done here: https://github.com/pingcap/advanced-statefulset/blob/master/pkg/controller/statefulset/stateful_set.go#L331-L341
+	for i := 0; i < 5; i++ {
+		_, err = asCli.AppsV1().StatefulSets(asts.Namespace).Get(asts.Name, metav1.GetOptions{})
+		if err != nil && apierrors.IsNotFound(err) {
+			if err = syncControllerRevision(c, oldRevisionList); err != nil {
+				return nil, err
+			}
+			klog.V(2).Infof("successfully sync controller revisions owned by %s/%s", asts.Namespace, asts.Name)
+			return kruiseAsts, nil
+		}
+		time.Sleep(time.Duration(6) * time.Second)
+	}
+
+	return nil, fmt.Errorf("failed to sync controller revisions: statefulset %s/%s is not deleted from etcd", asts.Namespace, asts.Name)
+}
+
+func syncControllerRevision(c clientset.Interface, oldRevisionList *appsv1.ControllerRevisionList) error {
+	for _, revision := range oldRevisionList.Items {
+
+		revision.ObjectMeta.ResourceVersion = ""
+		revision.ObjectMeta.OwnerReferences = nil
+		revision.ObjectMeta.ManagedFields = nil
+		err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+			_, updateErr := c.AppsV1().ControllerRevisions(revision.Namespace).Update(&revision)
+			return updateErr
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
