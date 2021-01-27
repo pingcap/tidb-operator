1b64ac6c5518a6178441be2298fe32aa7be648f1
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2021-01-27 14:25:43 +0800

    Bump TiDB version to v4.0.10 (#3757) (#3758)
    * cherry pick #3757 to release-1.1
    
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>
    
    * Update hack/bump-version.sh
    
    Co-authored-by: BinChen <binchinsmail@gmail.com>
    Co-authored-by: DanielZhangQD <36026334+DanielZhangQD@users.noreply.github.com>

--- deploy/aliyun/variables.tf
+++ deploy/aliyun/variables.tf
@@ -36,7 +36,7 @@ variable "cluster_name" {
 
 variable "tidb_version" {
   description = "TiDB cluster version"
-  default     = "v4.0.9"
+  default     = "v4.0.10"
 }
 variable "tidb_cluster_chart_version" {
   description = "tidb-cluster chart version"
