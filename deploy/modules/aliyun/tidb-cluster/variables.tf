9c6d5c40d402f91572e7bb595426bf4d911ae0ae
BinChen <binchinsmail@gmail.com>
2021-01-27 13:32:04 +0800

    bump TiDB version to v4.0.10 (#3757)

--- deploy/modules/aliyun/tidb-cluster/variables.tf
+++ deploy/modules/aliyun/tidb-cluster/variables.tf
@@ -12,7 +12,7 @@ variable "image_id" {
 
 variable "tidb_version" {
   description = "TiDB cluster version"
-  default     = "v4.0.9"
+  default     = "v4.0.10"
 }
 
 variable "tidb_cluster_chart_version" {
