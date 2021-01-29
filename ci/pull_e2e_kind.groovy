e67cceb4c848b1c163fcc56c5560334c87ad2650
csuzhangxc <csuzhangxc@gmail.com>
2021-01-29 18:12:20 +0800

    fix env

--- ci/pull_e2e_kind.groovy
+++ ci/pull_e2e_kind.groovy
@@ -217,11 +217,8 @@ def build(String name, String code, Map resources = e2ePodResources) {
                                 export BUILD_NUMBER=${BUILD_NUMBER}
                                 export GIT_COMMIT=${GIT_COMMIT}
                                 export PR_ID=${PR_ID}
-                                export COV_TAGS=e2e
-                                export COV_NAME=tidb-operator
-                                export COV_FILE=./coverage2.txt
                                 curl -L "https://codecov.io/bash" -o "/codecov" && chmod 755 /codecov
-                                /codecov -t ${CODECOV_TOKEN} -B ${SRC_BRANCH} -b ${BUILD_NUMBER} -C ${GIT_COMMIT} -P ${PR_ID} -F ${COV_TAGS} -n ${COV_NAME} -f ${COV_FILE}
+                                /codecov -t ${CODECOV_TOKEN} -B ${SRC_BRANCH} -b ${BUILD_NUMBER} -C ${GIT_COMMIT} -P ${PR_ID} -F e2e -n tidb-operator -f ./coverage2.txt
                                 """
                             }
                         }
