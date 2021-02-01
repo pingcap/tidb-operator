d8ec7722542bb6f5146e776868a727e42750fa99
csuzhangxc <csuzhangxc@gmail.com>
2021-02-01 18:31:15 +0800

    add time part into coverage filename

--- images/tidb-operator/e2e-entrypoint.sh
+++ images/tidb-operator/e2e-entrypoint.sh
@@ -15,69 +15,22 @@
 
 set -e
 
-# This is a wrapper script used as the entrypoint in E2E tests to:
-# - generate a coverage report for binaries when terminating.
-# - upload the coverage report to codecov
-
-TIMEOUT=30
-COV_FILE=/tmp/coverage.txt
-
-# populate test tags
-COV_TAGS="e2e"
+# This is a wrapper script used as the entrypoint in E2E tests to
+# replace the coverage filename with a time based part.
 
 COV_NAME="tidb-operator.$(date +%s)"
 if [ -n "$COMPONENT" ]; then
     COV_NAME="$COMPONENT.$(date +%s)"
 fi
 
-# run the original process in the background, so that we can send SIGTERM to it.
-eval "($@) &"
-PID=$!
-
-# upload the coverage report to codecov after generated after the original process exited normally.
-upload_cov() {
-  for ((i=1; i<=$TIMEOUT; i++)); do
-    if [ -f $COV_FILE ]; then
-      echo "e2e-entrypoint.sh: coverage file $COV_FILE found"
-      break
-    fi
-    echo "e2e-entrypoint.sh: coverage file not found, already waited for ${i}s"
-    sleep 1
-  done
-
-  if [ ! -f $COV_FILE ]; then
-    echo "e2e-entrypoint.sh: coverage file not found, abort uploading"
-    exit 0
+# add a time part into coverage filename.
+args=( "$@" )
+for ((i=0; i < $#; i++)) ;do
+  if [[ "${args[$i]}" =~ ^-test.coverprofile ]]; then
+    args[$i]="-test.coverprofile=/coverage/$COV_NAME.cov"
+    break
   fi
+done
+set "${args[@]}"
 
-  echo "some env for uploading code coverage"
-  echo "SRC_BRANCH: $SRC_BRANCH"
-  echo "BUILD_NUMBER: $BUILD_NUMBER"
-  echo "GIT_COMMIT: $GIT_COMMIT"
-  echo "PR_ID: $PR_ID"
-  echo "COV_NAME: $COV_NAME"
-
-  echo "e2e-entrypoint.sh: uploading code coverage"
-  /codecov \
-    -t "$CODECOV_TOKEN" \
-    -B "$SRC_BRANCH" \
-    -b "$BUILD_NUMBER" \
-    -C "$GIT_COMMIT" \
-    -P "$PR_ID" \
-    -F "$COV_TAGS" \
-    -n "$COV_NAME" \
-    -f "$COV_FILE" || \
-    echo "e2e-entrypoint.sh: codecov upload failed, ignore and continue"
-}
-
-_term() {
-  echo "e2e-entrypoint.sh: SIGTERM received, start to shutdown"
-  echo "e2e-entrypoint.sh: stopping the process"
-  kill -TERM $PID
-
-  upload_cov
-}
-
-trap _term SIGTERM
-
-wait
+$@
