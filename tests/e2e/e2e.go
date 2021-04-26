d71c15b5a8e944ae05d69615ec8af21328155185
csuzhangxc <csuzhangxc@gmail.com>
2021-04-26 14:18:42 +0800

    skip install MySQL and TiDB for DM if operator not installed

--- tests/e2e/e2e.go
+++ tests/e2e/e2e.go
@@ -316,14 +316,16 @@ var _ = ginkgo.SynchronizedBeforeSuite(func() []byte {
 			operatorKillerStopCh := make(chan struct{})
 			go operatorKiller.Run(operatorKillerStopCh)
 		}
+
+		// only deploy MySQL and TiDB for DM if CRDs and TiDB Operator installed.
+		// setup upstream MySQL instances and the downstream TiDB cluster for DM testing.
+		// if we can only setup these resource for DM tests with something like `--focus` or `--skip`, that should be better.
+		oa.DeployDMMySQLOrDie()
+		oa.DeployDMTiDBOrDie()
 	} else {
 		ginkgo.By("Skip installing tidb-operator")
+		ginkgo.By("Skip installing MySQL and TiDB for DM tests")
 	}
-
-	// setup upstream MySQL instances and the downstream TiDB cluster for DM testing.
-	// if we can only setup these resource for DM tests with something like `--focus` or `--skip`, that should be better.
-	oa.DeployDMMySQLOrDie()
-	oa.DeployDMTiDBOrDie()
 
 	ginkgo.By("Installing cert-manager")
 	err = tidbcluster.InstallCertManager(kubeCli)
