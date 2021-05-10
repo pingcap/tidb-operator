66d91a301ed4608cab248cde5a3d942dc17003b3
july2993 <july2993@gmail.com>
2021-05-10 17:10:34 +0800

    get pd urls from discovery for tiflash/pump/ticdc when enable cluster domain (#3947)
    rename IsHeterogeneous
    &
    fix #3661

--- tests/failover.go
+++ tests/failover.go
@@ -165,8 +165,8 @@ func (oa *OperatorActions) TruncateSSTFileThenCheckFailover(info *TidbClusterCon
 
 	// checkout pd config
 	var pdCfg *pdapi.PDConfigFromAPI
-	if tc.IsHeterogeneous() {
-		pdCfg, err = oa.pdControl.GetPDClient(pdapi.Namespace(tc.GetNamespace()), tc.GetName(), tc.IsTLSClusterEnabled()).GetConfig()
+	if tc.HeterogeneousWithoutLocalPD() {
+		pdCfg, err = oa.pdControl.GetClusterRefPDClient(pdapi.Namespace(tc.Spec.Cluster.Namespace), tc.Spec.Cluster.Name, tc.Spec.Cluster.ClusterDomain, tc.IsTLSClusterEnabled()).GetConfig()
 	} else {
 		pdCfg, err = oa.pdControl.GetPDClient(pdapi.Namespace(tc.GetNamespace()), tc.GetName(), tc.IsTLSClusterEnabled()).GetConfig()
 	}
@@ -524,8 +524,8 @@ func (oa *OperatorActions) CheckRecover(cluster *TidbClusterConfig) (bool, error
 	if int32(len(tc.Status.TiKV.Stores)) > tc.Spec.TiKV.Replicas {
 
 		var pdclient pdapi.PDClient
-		if tc.IsHeterogeneous() {
-			pdclient = oa.pdControl.GetPDClient(pdapi.Namespace(tc.Namespace), tc.Spec.Cluster.Name, tc.IsTLSClusterEnabled())
+		if tc.HeterogeneousWithoutLocalPD() {
+			pdclient = oa.pdControl.GetPDClient(pdapi.Namespace(tc.Spec.Cluster.Namespace), tc.Spec.Cluster.Name, tc.IsTLSClusterEnabled())
 		} else {
 			pdclient = oa.pdControl.GetPDClient(pdapi.Namespace(tc.Namespace), tc.Name, tc.IsTLSClusterEnabled())
 		}
