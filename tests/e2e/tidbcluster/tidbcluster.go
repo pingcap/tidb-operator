4faa6ab1289618c1fb98bbe1740afa5167160d01
Xuecheng Zhang <csuzhangxc@gmail.com>
2021-05-27 15:49:36 +0800

    e2e: fix some It args; replace local-storage sc for PD scale case (#3993)

--- tests/e2e/tidbcluster/tidbcluster.go
+++ tests/e2e/tidbcluster/tidbcluster.go
@@ -1675,11 +1675,11 @@ var _ = ginkgo.Describe("TiDBCluster", func() {
 		ginkgo.Context("while concurrently scale PD", func() {
 			operation := []string{"in", "out"}
 			for _, op := range operation {
+				op := op
 				ginkgo.It(op, func() {
 					ginkgo.By("Deploy initial tc")
 					tcName := fmt.Sprintf("scale-%s-pd-concurrently", op)
 					tc := fixture.GetTidbCluster(ns, tcName, utilimage.TiDBV5Prev)
-					tc.Spec.PD.StorageClassName = pointer.StringPtr("local-storage")
 					if op == "in" {
 						tc.Spec.PD.Replicas = 5
 					} else {
@@ -1738,6 +1738,7 @@ var _ = ginkgo.Describe("TiDBCluster", func() {
 		ginkgo.Context("while concurrently scale TiKV", func() {
 			operation := []string{"in", "out"}
 			for _, op := range operation {
+				op := op
 				ginkgo.It(op, func() {
 					ginkgo.By("Deploy initial tc")
 					tcName := fmt.Sprintf("scale-%s-tikv-concurrently", op)
