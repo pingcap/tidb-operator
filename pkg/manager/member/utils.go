04d2328f75d69f03cf7efcab155c1591e02addfc
Yifan Xu <xuyifan02@pingcap.com>
2021-05-27 20:35:35 +0800

    Enable customized labels for TidbCluster pods and "use-by=end-user" services (#3892)

--- pkg/manager/member/utils.go
+++ pkg/manager/member/utils.go
@@ -166,17 +166,6 @@ func PdName(tcName string, ordinal int32, namespace string, clusterDomain string
 	return PdPodName(tcName, ordinal)
 }
 
-// CombineAnnotations merges two annotations maps
-func CombineAnnotations(a, b map[string]string) map[string]string {
-	if a == nil {
-		a = make(map[string]string)
-	}
-	for k, v := range b {
-		a[k] = v
-	}
-	return a
-}
-
 // NeedForceUpgrade check if force upgrade is necessary
 func NeedForceUpgrade(ann map[string]string) bool {
 	// Check if annotation 'pingcap.com/force-upgrade: "true"' is set
@@ -233,7 +222,8 @@ func getStsAnnotations(tcAnns map[string]string, component string) map[string]st
 	if tcAnns == nil {
 		return anns
 	}
-	// delete slots
+
+	// ensure the delete-slots annotation
 	var key string
 	switch component {
 	case label.PDLabelVal:
@@ -254,6 +244,7 @@ func getStsAnnotations(tcAnns map[string]string, component string) map[string]st
 	if val, ok := tcAnns[key]; ok {
 		anns[helper.DeleteSlotsAnn] = val
 	}
+
 	return anns
 }
 
@@ -276,41 +267,48 @@ func UpdateStatefulSet(setCtl controller.StatefulSetControlInterface, object run
 		oldSet.Annotations = map[string]string{}
 	}
 
-	if !util.StatefulSetEqual(*newSet, *oldSet) || isOrphan {
-		set := *oldSet
-		// Retain the deprecated last applied pod template annotation for backward compatibility
-		var podConfig string
-		var hasPodConfig bool
-		if oldSet.Spec.Template.Annotations != nil {
-			podConfig, hasPodConfig = oldSet.Spec.Template.Annotations[LastAppliedConfigAnnotation]
+	// Check if an upgrade is needed.
+	// If not, early return.
+	if util.StatefulSetEqual(*newSet, *oldSet) && !isOrphan {
+		return nil
+	}
+
+	set := *oldSet
+
+	// update specs for sts
+	*set.Spec.Replicas = *newSet.Spec.Replicas
+	set.Spec.UpdateStrategy = newSet.Spec.UpdateStrategy
+	set.Labels = newSet.Labels
+	set.Annotations = newSet.Annotations
+	set.Spec.Template = newSet.Spec.Template
+	if isOrphan {
+		set.OwnerReferences = newSet.OwnerReferences
+	}
+
+	var podConfig string
+	var hasPodConfig bool
+	if oldSet.Spec.Template.Annotations != nil {
+		podConfig, hasPodConfig = oldSet.Spec.Template.Annotations[LastAppliedConfigAnnotation]
+	}
+	if hasPodConfig {
+		if set.Spec.Template.Annotations == nil {
+			set.Spec.Template.Annotations = map[string]string{}
 		}
-		set.Spec.Template = newSet.Spec.Template
-		if hasPodConfig {
-			if set.Spec.Template.Annotations == nil {
-				set.Spec.Template.Annotations = map[string]string{}
-			}
-			set.Spec.Template.Annotations[LastAppliedConfigAnnotation] = podConfig
-		}
-		set.Annotations = newSet.Annotations
-		v, ok := oldSet.Annotations[label.AnnStsLastSyncTimestamp]
-		if ok {
-			set.Annotations[label.AnnStsLastSyncTimestamp] = v
-		}
-		*set.Spec.Replicas = *newSet.Spec.Replicas
-		set.Spec.UpdateStrategy = newSet.Spec.UpdateStrategy
-		if isOrphan {
-			set.OwnerReferences = newSet.OwnerReferences
-			set.Labels = newSet.Labels
-		}
-		err := SetStatefulSetLastAppliedConfigAnnotation(&set)
-		if err != nil {
-			return err
-		}
-		_, err = setCtl.UpdateStatefulSet(object, &set)
+		set.Spec.Template.Annotations[LastAppliedConfigAnnotation] = podConfig
+	}
+	v, ok := oldSet.Annotations[label.AnnStsLastSyncTimestamp]
+	if ok {
+		set.Annotations[label.AnnStsLastSyncTimestamp] = v
+	}
+
+	err := SetStatefulSetLastAppliedConfigAnnotation(&set)
+	if err != nil {
 		return err
 	}
 
-	return nil
+	// commit to k8s
+	_, err = setCtl.UpdateStatefulSet(object, &set)
+	return err
 }
 
 // findContainerByName finds targetContainer by containerName, If not find, then return nil
@@ -321,17 +319,6 @@ func findContainerByName(sts *apps.StatefulSet, containerName string) *corev1.Co
 		}
 	}
 	return nil
-}
-
-func CopyAnnotations(src map[string]string) map[string]string {
-	if src == nil {
-		return nil
-	}
-	dst := map[string]string{}
-	for k, v := range src {
-		dst[k] = v
-	}
-	return dst
 }
 
 func getTikVConfigMapForTiKVSpec(tikvSpec *v1alpha1.TiKVSpec, tc *v1alpha1.TidbCluster, scriptModel *TiKVStartScriptModel) (*corev1.ConfigMap, error) {
