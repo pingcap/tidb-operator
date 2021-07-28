15518fbd88c9fc6fb564130bb97b8141bd8a6261
DanielZhangQD <zhanghailong810@aliyun.com>
2021-07-28 11:35:14 +0800

    upgrade tiflash after pd

--- pkg/manager/member/pd_upgrader.go
+++ pkg/manager/member/pd_upgrader.go
@@ -45,7 +45,6 @@ func (u *pdUpgrader) gracefulUpgrade(tc *v1alpha1.TidbCluster, oldSet *apps.Stat
 		return fmt.Errorf("tidbcluster: [%s/%s]'s pd status sync failed, can not to be upgraded", ns, tcName)
 	}
 	if tc.Status.TiCDC.Phase == v1alpha1.UpgradePhase ||
-		tc.Status.TiFlash.Phase == v1alpha1.UpgradePhase ||
 		tc.PDScaling() {
 		klog.Infof("TidbCluster: [%s/%s]'s ticdc status is %v, "+
 			"tiflash status is %v, pd status is %v, can not upgrade pd",
