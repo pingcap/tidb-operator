8d08404f5d13742a10fd76fcda0f70ddb4bec762
mianhk <gcyu25@gmail.com>
2021-08-25 11:00:04 +0800

    Fast evict leader (#4091)

--- pkg/manager/member/tikv_upgrader.go
+++ pkg/manager/member/tikv_upgrader.go
@@ -77,6 +77,13 @@ func (u *tikvUpgrader) Upgrade(meta metav1.Object, oldSet *apps.StatefulSet, new
 		return fmt.Errorf("cluster[%s/%s] failed to upgrading tikv due to converting", meta.GetNamespace(), meta.GetName())
 	}
 
+	if *oldSet.Spec.Replicas < 2 {
+		klog.Infof("TiKV statefulset replicas are less than 2, skip evicting region leader for tc %s/%s", ns, tcName)
+		status.Phase = v1alpha1.UpgradePhase
+		setUpgradePartition(newSet, 0)
+		return nil
+	}
+
 	tc, _ := meta.(*v1alpha1.TidbCluster)
 
 	if !status.Synced {
