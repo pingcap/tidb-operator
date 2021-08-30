96037941b341cb8d7be417296a8935802af7cab6
Shiori <sihao.yu@pingcap.com>
2021-08-30 17:25:04 +0800

    update tidb version in e2e

--- tests/e2e/util/image/image.go
+++ tests/e2e/util/image/image.go
@@ -33,14 +33,13 @@ var (
 const (
 	// TiDB Version
 	TiDBV3             = "v3.0.20"
-	TiDBLatestPrev     = "v5.0.3"
-	TiDBLatest         = "v5.1.0"
+	TiDBLatestPrev     = "v5.1.1"
+	TiDBLatest         = "v5.2.0"
 	TiDBNightlyVersion = "nightly"
 	// specific version
 	TiDBV4x0x9 = "v4.0.9"
 	TiDBV5x0x0 = "v5.0.0"
 	TiDBV5x0x2 = "v5.0.2"
-	TiDBV5x1x0 = "v5.1.0"
 
 	PrometheusImage               = "prom/prometheus"
 	PrometheusVersion             = "v2.18.1"
