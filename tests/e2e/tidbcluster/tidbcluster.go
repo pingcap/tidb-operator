d4931e843cb0aaff60d916b193c84ecd0f44e8e5
mikechengwei <842725815@qq.com>
2021-09-22 17:34:52 +0800

    fix bug

--- tests/e2e/tidbcluster/tidbcluster.go
+++ tests/e2e/tidbcluster/tidbcluster.go
@@ -2933,7 +2933,7 @@ var _ = ginkgo.Describe("TiDBCluster", func() {
 	ginkgo.It("deploy tidb monitor specified shards normally", func() {
 		ginkgo.By("Deploy initial tc")
 		tc := fixture.GetTidbCluster(ns, "monitor-test", utilimage.TiDBLatest)
-		tc.Spec.PD.Replicas = 1
+		tc.Spec.PD.Replicas = 3
 		tc.Spec.TiKV.Replicas = 1
 		tc.Spec.TiDB.Replicas = 1
 		tc, err := cli.PingcapV1alpha1().TidbClusters(tc.Namespace).Create(context.TODO(), tc, metav1.CreateOptions{})
