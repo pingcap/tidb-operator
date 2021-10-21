4778d7698e4891062b03f57ff323585dddd34bc5
Shiori <sihao.yu@pingcap.com>
2021-10-21 09:06:45 +0800

    Bump operator to v1.2.4, tidb to v5.2.1, dm to v2.0.7 (#4233)

--- deploy/modules/aliyun/tidb-cluster/variables.tf
+++ deploy/modules/aliyun/tidb-cluster/variables.tf
@@ -12,7 +12,7 @@ variable "image_id" {
 
 variable "tidb_version" {
   description = "TiDB cluster version"
-  default     = "v5.2.0"
+  default     = "v5.2.1"
 }
 
 variable "tidb_cluster_chart_version" {
