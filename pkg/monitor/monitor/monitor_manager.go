4f9443dd397332c77aea7367cb0fbb2d9b61d43d
mikechengwei <842725815@qq.com>
2021-10-29 15:20:19 +0800

    separate dm and tidb monitor logic

--- pkg/monitor/monitor/monitor_manager.go
+++ pkg/monitor/monitor/monitor_manager.go
@@ -161,7 +161,6 @@ func (m *MonitorManager) SyncMonitor(monitor *v1alpha1.TidbMonitor) error {
 		}
 		klog.V(4).Infof("tm[%s/%s]'s pv synced", monitor.Namespace, monitor.Name)
 	}
-	klog.Error("test dm monitor --------2222222")
 	klog.V(4).Infof("tm[%s/%s]'s StatefulSet synced", monitor.Namespace, monitor.Name)
 
 	// Sync Ingress
@@ -264,7 +263,7 @@ func (m *MonitorManager) syncTidbMonitorStatefulset(tc *v1alpha1.TidbCluster, dc
 			return err
 		}
 	}
-	klog.Error("test dm monitor --------")
+
 	if !isAllCreated {
 		return controller.RequeueErrorf("TidbMonitor: [%s/%s], waiting for tidbmonitor running", ns, name)
 	} else {
