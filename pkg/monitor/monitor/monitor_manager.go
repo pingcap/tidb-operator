446cec51ee7e688c9ab04573e2d435d7d2c7d1f6
Mike <842725815@qq.com>
2021-11-03 09:53:32 +0800

    Update pkg/monitor/monitor/monitor_manager.go
    Co-authored-by: july2993 <july2993@gmail.com>

--- pkg/monitor/monitor/monitor_manager.go
+++ pkg/monitor/monitor/monitor_manager.go
@@ -68,7 +68,7 @@ func (m *MonitorManager) SyncMonitor(monitor *v1alpha1.TidbMonitor) error {
 	if monitor.DeletionTimestamp != nil {
 		return nil
 	}
-	if (monitor.Spec.Clusters == nil || len(monitor.Spec.Clusters) < 1) && (monitor.Spec.DM == nil || len(monitor.Spec.DM.Clusters) < 1) {
+	if len(monitor.Spec.Clusters) < 1 && (monitor.Spec.DM == nil || len(monitor.Spec.DM.Clusters) < 1) {
 		klog.Errorf("tm[%s/%s] does not configure the target tidbcluster", monitor.Namespace, monitor.Name)
 		return nil
 	}
