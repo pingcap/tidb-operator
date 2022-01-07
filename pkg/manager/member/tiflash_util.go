4be28b20e971e4a0d26464a5c47aa2c7e9d232f5
Shiori <sihao.yu@pingcap.com>
2022-01-07 17:20:28 +0800

    add ut

--- pkg/manager/member/tiflash_util.go
+++ pkg/manager/member/tiflash_util.go
@@ -138,6 +138,7 @@ func getTiFlashConfigV2(tc *v1alpha1.TidbCluster) *v1alpha1.TiFlashConfigWraper 
 	// common
 	{
 		// storage
+		// check "path" to be compatible with old version
 		if common.Get("path") == nil && common.Get("storage") == nil {
 			paths := []string{}
 			for i := range tc.Spec.TiFlash.StorageClaims {
@@ -148,6 +149,7 @@ func getTiFlashConfigV2(tc *v1alpha1.TidbCluster) *v1alpha1.TiFlashConfigWraper 
 			}
 			common.Set("storage.main.dir", paths)
 		}
+		// check "raft.kvstore_path" to be compatible with old version
 		if common.Get("raft.kvstore_path") == nil {
 			common.SetIfNil("storage.raft.dir", "/data0/kvstore")
 		}
@@ -182,6 +184,9 @@ func getTiFlashConfigV2(tc *v1alpha1.TidbCluster) *v1alpha1.TiFlashConfigWraper 
 			pdAddr = fmt.Sprintf("%s.%s.svc%s:2379", controller.PDMemberName(ref.Name), ref.Namespace, controller.FormatClusterDomain(ref.ClusterDomain))
 		}
 		common.SetIfNil("raft.pd_addr", pdAddr)
+
+		// server
+		common.SetIfNil("server.engine-addr", fmt.Sprintf("%s-POD_NUM.%s.%s.svc%s:3930", controller.TiFlashMemberName(name), controller.TiFlashPeerMemberName(name), ns, controller.FormatClusterDomain(clusterDomain)))
 	}
 
 	// proxy
