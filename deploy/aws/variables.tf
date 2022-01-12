0883eb3c69465f3a516fa9334065387547ef5208
Justin Huang <legendj228@gmail.com>
2022-01-12 15:10:03 +0800

    bump operator version to v1.3.0-beta.1 (#4367)
    * bump operator version to v1.3.0-beta.1
    
    * bump go modules version to v1.3.0-beta.1

--- deploy/aws/variables.tf
+++ deploy/aws/variables.tf
@@ -19,7 +19,7 @@ variable "eks_version" {
 
 variable "operator_version" {
   description = "TiDB operator version"
-  default     = "v1.2.6"
+  default     = "v1.3.0-beta.1"
 }
 
 variable "operator_values" {
