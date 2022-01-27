1d889ef862369d975b431e443a7ee66fff41cd34
Shiori <sihao.yu@pingcap.com>
2022-01-27 09:58:13 +0800

    optimize

--- pkg/discovery/discovery.go
+++ pkg/discovery/discovery.go
@@ -255,7 +255,7 @@ func (d *tidbDiscovery) VerifyPDEndpoint(pdURL string) (string, error) {
 	}
 
 	// if local pd doesn't exist, return target cluster pd peer addr
-	if tc.Heterogeneous() && tc.WithoutLocalPD() {
+	if tc.HeterogeneousWithRemote() && tc.WithoutLocalPD() {
 		addr := controller.PDPeerFullyDomain(tc.Spec.Cluster.Name, tc.Spec.Cluster.Namespace, tc.Spec.Cluster.ClusterDomain)
 		if pdEndpoint.scheme != "" {
 			addr = fmt.Sprintf("%s://%s", pdEndpoint.scheme, addr)
