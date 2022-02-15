17b1e59a4d07b9d3bce32132ff21158d982225cd
Justin Huang <legendj228@gmail.com>
2022-02-15 16:11:58 +0800

    Bump operator version to v1.3.0, tidb to v5.4.0 and dm to v5.4.0 (#4410)

--- deploy/modules/aliyun/tidb-cluster/variables.tf
+++ deploy/modules/aliyun/tidb-cluster/variables.tf
@@ -12,7 +12,7 @@ variable "image_id" {
 
 variable "tidb_version" {
   description = "TiDB cluster version"
-  default     = "v5.3.0"
+  default     = "v5.4.0"
 }
 
 variable "tidb_cluster_chart_version" {
