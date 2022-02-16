2d2579aa10bd95b2b647af2e6c00493228b11bb1
just1900 <legendj228@gmail.com>
2022-02-16 20:03:59 +0800

    bump operator to v1.1.15, tidb components to v5.0.6

--- deploy/modules/aliyun/tidb-cluster/variables.tf
+++ deploy/modules/aliyun/tidb-cluster/variables.tf
@@ -12,7 +12,7 @@ variable "image_id" {
 
 variable "tidb_version" {
   description = "TiDB cluster version"
-  default     = "v5.0.4"
+  default     = "v5.0.6"
 }
 
 variable "tidb_cluster_chart_version" {
--- deploy/modules/aws/tidb-cluster/variables.tf
+++ deploy/modules/aws/tidb-cluster/variables.tf
@@ -19,7 +19,7 @@ variable "cluster_name" {
 
 variable "cluster_version" {
   type    = string
-  default = "v5.0.4"
+  default = "v5.0.6"
 }
 
 variable "ssh_key_name" {
