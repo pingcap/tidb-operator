a862b11d77e8577f14722943f103e5ddb248430b
Shiori <sihao.yu@pingcap.com>
2022-02-23 11:27:58 +0800

    Bump operator version to 1.3.1 (#4433)
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>

--- deploy/aws/variables.tf
+++ deploy/aws/variables.tf
@@ -19,7 +19,7 @@ variable "eks_version" {
 
 variable "operator_version" {
   description = "TiDB operator version"
-  default     = "v1.3.0"
+  default     = "v1.3.1"
 }
 
 variable "operator_values" {
