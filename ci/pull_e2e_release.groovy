bcc75b095aec49e87c90bb514c1732d4587493d7
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2022-06-22 22:14:20 +0800

    cherry pick #4562 to release-1.3 (#4563)
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>
    
    Co-authored-by: Shiori <sihao.yu@pingcap.com>

--- ci/pull_e2e_release.groovy
+++ ci/pull_e2e_release.groovy
@@ -361,18 +361,11 @@ try {
                         dir("${PROJECT_DIR}") {
                             unstash 'tidb-operator'
                             stage('upload tidb-operator binaries and charts'){
-                                withCredentials([
-                                    string(credentialsId: 'UCLOUD_PUBLIC_KEY', variable: 'UCLOUD_PUBLIC_KEY'),
-                                    string(credentialsId: 'UCLOUD_PRIVATE_KEY', variable: 'UCLOUD_PRIVATE_KEY'),
-                                ]) {
-                                    sh """
-                                    export UCLOUD_UFILE_PROXY_HOST=pingcap-dev.hk.ufileos.com
-                                    export UCLOUD_UFILE_BUCKET=pingcap-dev
-                                    export BUILD_BRANCH=${GIT_REF}
-                                    export GITHASH=${GITHASH}
-                                    ./ci/upload-binaries-charts.sh
-                                    """
-                                }
+                              sh """
+                              export BUILD_BRANCH=${GIT_REF}
+                              export GITHASH=${GITHASH}
+                              ./ci/upload-binaries-charts.sh
+                              """
                             }
                         }
                     }
