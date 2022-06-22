86c9a2836c0becae3586a5a0edf587cddde1cb40
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2022-06-22 12:13:57 +0800

    ci use new cluster (#4541) (#4550)
    * cherry pick #4541 to release-1.2
    
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>
    
    * fix conflict
    
    Co-authored-by: Lifu Wu <purelind@users.noreply.github.com>

--- ci/pull_e2e_kind.groovy
+++ ci/pull_e2e_kind.groovy
@@ -171,7 +171,7 @@ e2ePodResources = [
 ]
 
 def build(String name, String code, Map resources = e2ePodResources) {
-    podTemplate(yaml: buildPodYAML(resources: resources)) {
+    podTemplate(yaml: buildPodYAML(resources: resources), namespace: "jenkins-tidb-operator", cloud: "kubernetes-ng") {
         node(POD_LABEL) {
             container('main') {
                 def WORKSPACE = pwd()
@@ -287,6 +287,8 @@ try {
             ],
         ]
         podTemplate(
+            cloud: "kubernetes-ng",
+            namespace: "jenkins-tidb-operator",
             label: buildPodLabel,
             yaml: buildPodYAML(resources: resources, any: true),
             // We allow this pod to remain active for a while, later jobs can
@@ -386,7 +388,9 @@ try {
         if (GIT_REF ==~ /^(master|)$/ || GIT_REF ==~ /^(release-.*)$/
             || GIT_REF ==~ /^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/) {
             // Upload assets if the git ref is the master branch or version tag
-            podTemplate(yaml: buildPodYAML(resources: [requests: [cpu: "1", memory: "2Gi"]])) {
+            podTemplate(yaml: buildPodYAML(resources: [requests: [cpu: "1", memory: "2Gi"]]),
+              cloud: "kubernetes-ng", namespace: "tidb-operator",
+            ) {
                 node(POD_LABEL) {
                     container("main") {
                         dir("${PROJECT_DIR}") {
