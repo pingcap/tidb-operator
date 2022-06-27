9499aba88723edca220cc952c6497c0d644442e6
Shiori <sihao.yu@pingcap.com>
2022-06-27 10:58:37 +0800

    fix lint

--- cmd/backup-manager/app/clean/clean.go
+++ cmd/backup-manager/app/clean/clean.go
@@ -60,23 +60,23 @@ func (bo *Options) cleanBRRemoteBackupData(ctx context.Context, backup *v1alpha1
 			return err
 		}
 
-		klog.Infof("For backup %s, try to delete %s objects", bo, len(objs))
+		klog.Infof("For backup %s, try to delete %d objects", bo, len(objs))
 
 		// batch delete objects
 		result := s.BatchDeleteObjects(ctx, objs, opt.BatchDeleteOption)
 
 		if len(result.Deleted) != 0 {
-			klog.Infof("For backup %s, delete %s objects successfully", bo, len(result.Deleted))
+			klog.Infof("For backup %s, delete %d objects successfully", bo, len(result.Deleted))
 			klog.V(4).Infof("For backup %s, all objects deleted: %s", bo, strings.Join(result.Deleted, ","))
 		}
 		if len(result.Deleted) < len(objs) {
 			if len(result.Errors) != 0 {
-				klog.Errorf("For backup %s, delete %s objects failed", bo, len(result.Errors))
+				klog.Errorf("For backup %s, delete %d objects failed", bo, len(result.Errors))
 				for _, oerr := range result.Errors {
 					klog.V(4).Infof("For backup %s, delete object %s failed: %s", bo, oerr.Key, oerr.Err)
 				}
 			} else {
-				klog.Errorf("For backup %s, deleted objects are less than expected but failed is 0, deleted: %s, expected: %s", bo, len(result.Deleted), len(objs))
+				klog.Errorf("For backup %s, deleted objects are less than expected but failed is 0, deleted: %d, expected: %d", bo, len(result.Deleted), len(objs))
 			}
 
 			return fmt.Errorf("objects remain to delete")
