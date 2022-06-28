4340904411582a177bce7ac1b521e35602322f3d
xu21yingan <100554983+xu21yingan@users.noreply.github.com>
2022-06-28 10:02:39 +0800

    Bump operator to v1.3.5 (#4569)

--- deploy/aws/variables.tf
+++ deploy/aws/variables.tf
@@ -19,7 +19,7 @@ variable "eks_version" {
 
 variable "operator_version" {
   description = "TiDB operator version"
-  default     = "v1.3.4"
+  default     = "v1.3.5"
 }
 
 variable "operator_values" {
