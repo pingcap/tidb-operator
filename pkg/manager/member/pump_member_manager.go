56f5ade6125d282777a04323ce24bdb9ec38b2ec
Mike <842725815@qq.com>
2022-06-30 23:39:59 +0800

    Update pkg/manager/member/pump_member_manager.go
    Co-authored-by: DanielZhangQD <36026334+DanielZhangQD@users.noreply.github.com>

--- pkg/manager/member/pump_member_manager.go
+++ pkg/manager/member/pump_member_manager.go
@@ -465,7 +465,7 @@ func getNewPumpStatefulSet(tc *v1alpha1.TidbCluster, cm *corev1.ConfigMap) (*app
 	podSpec := spec.BuildPodSpec()
 	podSpec.Containers, err = MergePatchContainers(containers, tc.Spec.Pump.AdditionalContainers)
 	if err != nil {
-		return nil, fmt.Errorf("pump[%s/%s] failed to merge containers spec , error: %v", objMeta.Namespace, objMeta.Name, err)
+		return nil, fmt.Errorf("failed to merge containers spec for Pump of [%s/%s], error: %v", objMeta.Namespace, objMeta.Name, err)
 	}
 
 	podSpec.Volumes = volumes
