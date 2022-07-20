c0de2400ad0e20757522a046318d1034fb5f4b24
Shiori <sihao.yu@pingcap.com>
2022-07-20 10:04:26 +0800

    WIP

--- pkg/manager/suspender/suspender.go
+++ pkg/manager/suspender/suspender.go
@@ -13,33 +13,98 @@
 
 package suspender
 
-import "github.com/pingcap/tidb-operator/pkg/apis/pingcap/v1alpha1"
+import (
+	"context"
+	"fmt"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	errutil "k8s.io/apimachinery/pkg/util/errors"
+
+	"github.com/pingcap/tidb-operator/pkg/apis/pingcap/v1alpha1"
+	"github.com/pingcap/tidb-operator/pkg/controller"
+)
 
 type Suspender interface {
-	Suspend(*v1alpha1.TidbCluster) error
+	Suspend(v1alpha1.MemberType, *v1alpha1.TidbCluster) error
 }
 
 type suspender struct {
-	comp v1alpha1.MemberType
+	deps *controller.Dependencies
 }
 
-func (s *suspender) Suspend(tc *v1alpha1.TidbCluster) (bool, error) {
+func (s *suspender) Suspend(typ v1alpha1.MemberType, tc *v1alpha1.TidbCluster) error {
+	spec := tc.ComponentSpec(typ)
+	status := tc.ComponentStatus(typ)
 
-	status := v1alpha1.Compo
-	supsended := tc.ComponentIsSuspended(s.comp)
+	suspended := tc.ComponentIsSuspended(typ)
+	needed := s.needsSuspend(spec, status)
 
-	if !s.suspendable(tc) {
-		if supsended {
-			// transform phase firstly
+	if !needed {
+		if suspended {
+			err := s.resume()
+			if err != nil {
+				return err
+			}
 
-			return supsended, nil
+			status.SetPhase(v1alpha1.NormalPhase)
+		}
+		return nil
+	}
+
+	if !suspended {
+		status.SetPhase(v1alpha1.SuspendedPhase)
+		return nil
+	}
+
+	return s.suspend(tc, status, *spec.SuspendAction())
+}
+
+func (s *suspender) suspend(cluster metav1.Object, status v1alpha1.ComponentStatus, action v1alpha1.SuspendAction) error {
+	ns := cluster.GetNamespace()
+	name := cluster.GetName()
+	component := status.MemberType()
+
+	errs := []error{}
+	if action.SuspendStatefuleSet {
+		// FIXME: use a common function
+		stsName := fmt.Sprintf("%s-%s", name, component)
+		err := s.deps.KubeClientset.AppsV1().StatefulSets(ns).Delete(context.TODO(), stsName, metav1.DeleteOptions{})
+		if err != nil {
+			errs = append(errs, err)
 		}
 	}
 
-	return 
+	return errutil.NewAggregate(errs)
 }
 
-func (s *suspender) suspendable(tc *v1alpha1.TidbCluster) bool {
+func (s *suspender) resume() error {
+	return nil
+}
+
+func (s *suspender) suspendSts(cluster metav1.Object, status v1alpha1.ComponentStatus) error {
+	ns := cluster.GetNamespace()
+	name := cluster.GetName()
+
+	stsName := fmt.Sprintf("%s-%s", name, status.MemberType())
+	return s.deps.KubeClientset.AppsV1().StatefulSets(ns).Delete(stsName, &metav1.DeleteOptions{})
+}
+
+func (s *suspender) needsSuspend(spec v1alpha1.ComponentAccessor, status v1alpha1.ComponentStatus) bool {
 	// check if need to suspend
-	
+	action := spec.SuspendAction()
+	if action == nil {
+		return false
+	}
+
+	if !action.SuspendStatefuleSet {
+		return false
+	}
+
+	// check if can suspend
+	phase := status.GetPhase()
+	if phase != v1alpha1.NormalPhase && phase != v1alpha1.SuspendedPhase {
+		return false
+	}
+
+	return true
 }
