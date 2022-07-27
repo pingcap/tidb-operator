7c62bf2920e790fc42c110af6fa6aec3d528c608
hoyhbx <hoyhbx@gmail.com>
2022-07-27 17:44:35 -0500

    avoid silent rejection to invalid podManagementPolicy

--- pkg/apis/pingcap/v1alpha1/tidbcluster_component.go
+++ pkg/apis/pingcap/v1alpha1/tidbcluster_component.go
@@ -18,6 +18,7 @@ import (
 	apps "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/klog/v2"
 )
 
 const (
@@ -118,7 +119,10 @@ func (a *componentAccessorImpl) PodManagementPolicy() apps.PodManagementPolicyTy
 	// unified podManagementPolicy check to avoid check everywhere
 	if policy == apps.OrderedReadyPodManagement {
 		return apps.OrderedReadyPodManagement
+	} else if policy == apps.ParallelPodManagement {
+		return apps.ParallelPodManagement
 	}
+	klog.Warningf("PodManagementPolicy \"%s\" is invalid. Fall back to use \"Parallel\" instead\n", policy)
 	return apps.ParallelPodManagement
 }
 
