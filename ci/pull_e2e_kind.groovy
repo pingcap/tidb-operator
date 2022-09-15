5f5769ad582365be3916214e761d6a714cdce867
Shiori <sihao.yu@pingcap.com>
2022-09-15 18:38:48 +0800

    pull ci image from harbor

--- ci/pull_e2e_kind.groovy
+++ ci/pull_e2e_kind.groovy
@@ -37,7 +37,7 @@ metadata:
 spec:
   containers:
   - name: main
-    image: gcr.io/k8s-testimages/kubekins-e2e:v20210808-1eaeec7-master
+    image: hub-new.pingcap.net/tidb-operator/kubekins-e2e:v20210808-1eaeec7-master
     command:
     - runner.sh
     # Clean containers on TERM signal in root process to avoid cgroup leaking.
