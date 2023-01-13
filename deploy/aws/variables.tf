8f23de2fd7b0574a8647dfef5a99b752c38288e3
Xuecheng Zhang <csuzhangxc@gmail.com>
2023-01-13 13:55:42 +0800

    bump operator to v1.4.1 (#4839)

--- deploy/aws/variables.tf
+++ deploy/aws/variables.tf
@@ -19,7 +19,7 @@ variable "eks_version" {
 
 variable "operator_version" {
   description = "TiDB operator version"
-  default     = "v1.4.0"
+  default     = "v1.4.1"
 }
 
 variable "operator_values" {
