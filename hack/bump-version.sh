0083c6a0d176bc5d623b5d79f3d36d820817ab82
Shiori <sihao.yu@pingcap.com>
2023-02-03 11:19:25 +0800

    bump version to v1.4.2 (#4852)

--- hack/bump-version.sh
+++ hack/bump-version.sh
@@ -22,8 +22,8 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 # parameters
-OPERATOR_OLD="v1\.4\.0"
-OPERATOR_NEW="v1\.4\.1"
+OPERATOR_OLD="v1\.4\.1"
+OPERATOR_NEW="v1\.4\.2"
 TIDB_OLD="v6\.1\.0"
 TIDB_NEW="v6\.5\.0"
 DM_OLD="v6.1.0"
