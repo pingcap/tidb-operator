57c106049b6831e1392b6faabac36f069e136afb
Shiori <sihao.yu@pingcap.com>
2023-02-08 17:29:48 +0800

    optimize

--- ci/pull_e2e_kind.groovy
+++ ci/pull_e2e_kind.groovy
@@ -164,7 +164,7 @@ def build(String name, String code, Map resources = e2ePodResources) {
                         unstash 'tidb-operator'
                         stage("Debug Info") {
                             println "debug host: 172.16.5.21"
-                            println "debug command: kubectl -n jenkins-tidb exec -ti ${NODE_NAME} bash"
+                            println "debug command: kubectl -n jenkins-tidb-operator exec -ti ${NODE_NAME} bash"
                             sh """
                             echo "====== shell env ======"
                             echo "pwd: \$(pwd)"
