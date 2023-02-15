6a65e389c1f19f9ac1ad2bad1c062a7d969a5086
Chao Zheng <charleszheng44@gmail.com>
2023-02-15 11:29:07 -0800

    Implement the feature of pod serial number control for TiCDC using asts

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -114,6 +114,9 @@ func GetPodOrdinals(tc *v1alpha1.TidbCluster, memberType v1alpha1.MemberType) (s
 	} else if memberType == v1alpha1.TiFlashMemberType {
 		ann = label.AnnTiFlashDeleteSlots
 		replicas = tc.Spec.TiFlash.Replicas
+	} else if memberType == v1alpha1.TiCDCMemberType {
+		ann = label.AnnTiCDCDeleteSlots
+		replicas = tc.Spec.TiCDC.Replicas
 	} else {
 		return nil, fmt.Errorf("unknown member type %v", memberType)
 	}
