3e2917852fc4a0188e0a3519978ae98c684a109d
Shiori <sihao.yu@pingcap.com>
2023-02-17 11:26:09 +0800

    bump version to 1.4.3

--- deploy/aws/variables.tf
+++ deploy/aws/variables.tf
@@ -19,7 +19,7 @@ variable "eks_version" {
 
 variable "operator_version" {
   description = "TiDB operator version"
-  default     = "v1.4.2"
+  default     = "v1.4.3"
 }
 
 variable "operator_values" {
