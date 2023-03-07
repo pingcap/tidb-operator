d6ae0a0ce1faf9207ebfe4ec0cfea32562be21aa
Hao Luo <luohao135680@gmail.com>
2023-03-07 09:16:29 -0800

    Add PD graceful restart

--- pkg/apis/pingcap/v1alpha1/types.go
+++ pkg/apis/pingcap/v1alpha1/types.go
@@ -1378,6 +1378,8 @@ const (
 	EvictLeaderAnnKey = "tidb.pingcap.com/evict-leader"
 	// EvictLeaderAnnKeyForResize is the annotation key to evict leader user by pvc resizer.
 	EvictLeaderAnnKeyForResize = "tidb.pingcap.com/evict-leader-for-resize"
+	// PDLeaderTransferAnnKey is the annotation key to transfer PD leader used by user.
+	PDLeaderTransferAnnKey = "tidb.pingcap.com/pd-transfer-leader"
 )
 
 // The `Value` of annotation controls the behavior when the leader count drops to zero, the valid value is one of:
@@ -1387,6 +1389,15 @@ const (
 const (
 	EvictLeaderValueNone      = "none"
 	EvictLeaderValueDeletePod = "delete-pod"
+)
+
+// The `Value` of PD leader transfer annotation controls the behavior when the leader is transferred to another member, the valid value is one of:
+//
+// - `none`: doing nothing.
+// - `delete-pod`: delete pod.
+const (
+	TransferLeaderValueNone      = "none"
+	TransferLeaderValueDeletePod = "delete-pod"
 )
 
 type EvictLeaderStatus struct {
