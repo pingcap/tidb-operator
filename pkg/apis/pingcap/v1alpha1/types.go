bf0b14ea2c04aa7dcf1ea6aa053a71b67c00495c
WizardXiao <89761062+WizardXiao@users.noreply.github.com>
2023-03-09 05:46:41 +0800

    br: refine BackoffRetryPolicy time format (#4925)
    * refine BackoffRetryPolicy time format
    
    * fix some ut

--- pkg/apis/pingcap/v1alpha1/types.go
+++ pkg/apis/pingcap/v1alpha1/types.go
@@ -2004,15 +2004,17 @@ type BRConfig struct {
 // 1. the number of retries can not exceed MaxRetryTimes
 // 2. the time from discovery failure can not exceed RetryTimeout
 type BackoffRetryPolicy struct {
-	// MinRetryDuration is the seconds of min retry duration, the retry duration will be MinRetryDuration << (retry num -1)
-	// +kubebuilder:default=300
-	MinRetryDuration int `json:"minRetryDuration,omitempty"`
+	// MinRetryDuration is the min retry duration, the retry duration will be MinRetryDuration << (retry num -1)
+	// format reference, https://golang.org/pkg/time/#ParseDuration
+	// +kubebuilder:default="300s"
+	MinRetryDuration string `json:"minRetryDuration,omitempty"`
 	// MaxRetryTimes is the max retry times
 	// +kubebuilder:default=2
 	MaxRetryTimes int `json:"maxRetryTimes,omitempty"`
-	// RetryTimeout is the minutes of retry timeout
-	// +kubebuilder:default=30
-	RetryTimeout int `json:"retryTimeout,omitempty"`
+	// RetryTimeout is the retry timeout
+	// format reference, https://golang.org/pkg/time/#ParseDuration
+	// +kubebuilder:default="30m"
+	RetryTimeout string `json:"retryTimeout,omitempty"`
 }
 
 // BackoffRetryRecord is the record of backoff retry
