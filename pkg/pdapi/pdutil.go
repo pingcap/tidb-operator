eb5e045b87c8276401084b55f5b4993addc49700
artem_danilov <artem.danilov@airbnb.com>
2023-05-04 12:53:52 -0700

    change default value to not check tikv store during upgrade
    Signed-off-by: artem_danilov <artem.danilov@airbnb.com>

--- pkg/pdapi/pdutil.go
+++ pkg/pdapi/pdutil.go
@@ -19,10 +19,10 @@ import (
 	"github.com/pingcap/tidb-operator/pkg/apis/pingcap/v1alpha1"
 )
 
-// IsClusterStable checks if cluster is stable by quering PD and checking status of all stores
+// IsTiKVStable checks if cluster is stable by quering PD and checking status of all stores
 // we check PD directly instead of checking the same information already available in the CR status to get the most up to data
 // stores status in PD could be up to 20 seconds stale https://docs.pingcap.com/tidb/stable/tidb-scheduling#information-collection
-func IsClusterStable(pdClient PDClient) string {
+func IsTiKVStable(pdClient PDClient) string {
 	storesInfo, err := pdClient.GetStores()
 	if err != nil {
 		return fmt.Sprintf("can't access PD: %s", err)
