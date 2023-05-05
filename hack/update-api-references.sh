f8287c8ef3a0f6e1b26e26466fa331637aecf085
csuzhangxc <csuzhangxc@gmail.com>
2023-05-05 08:09:44 +0000

    update pkg name

--- hack/update-api-references.sh
+++ hack/update-api-references.sh
@@ -38,12 +38,12 @@ pushd ${API_DIR} >/dev/null
 popd >/dev/null
 
 echo "Generating API references docs for BR federation ..."
-API_DIR="${ROOT}/pkg/apis/pingcap/br-federation-v1alpha1"
+API_DIR="${ROOT}/pkg/apis/pingcap/br_federation_v1alpha1"
 
 pushd ${API_DIR} >/dev/null
     GOROOT=$(go env GOROOT) ${DOCS_BIN} \
         -config "$DOCS_PATH/config.json" \
         -template-dir "$DOCS_PATH/template" \
-        -api-dir "github.com/pingcap/tidb-operator/pkg/apis/pingcap/br-federation-v1alpha1" \
+        -api-dir "github.com/pingcap/tidb-operator/pkg/apis/pingcap/br_federation_v1alpha1" \
         -out-file "$DOCS_PATH/br-federation-docs.md"
 popd >/dev/null
