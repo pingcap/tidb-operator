1dda29410b609611b28bc18f71a52269e9917490
WangLe1321 <wangle1321@163.com>
2023-05-15 10:50:56 +0800

    br: implement federation volume backup controller
    Signed-off-by: WangLe1321 <wangle1321@163.com>

--- pkg/apis/federation/pingcap/v1alpha1/openapi_generated.go
+++ pkg/apis/federation/pingcap/v1alpha1/openapi_generated.go
@@ -27,15 +27,67 @@ import (
 
 func GetOpenAPIDefinitions(ref common.ReferenceCallback) map[string]common.OpenAPIDefinition {
 	return map[string]common.OpenAPIDefinition{
-		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeBackup":             schema_apis_federation_pingcap_v1alpha1_VolumeBackup(ref),
-		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeBackupList":         schema_apis_federation_pingcap_v1alpha1_VolumeBackupList(ref),
-		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeBackupSchedule":     schema_apis_federation_pingcap_v1alpha1_VolumeBackupSchedule(ref),
-		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeBackupScheduleList": schema_apis_federation_pingcap_v1alpha1_VolumeBackupScheduleList(ref),
-		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeBackupScheduleSpec": schema_apis_federation_pingcap_v1alpha1_VolumeBackupScheduleSpec(ref),
-		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeBackupSpec":         schema_apis_federation_pingcap_v1alpha1_VolumeBackupSpec(ref),
-		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeRestore":            schema_apis_federation_pingcap_v1alpha1_VolumeRestore(ref),
-		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeRestoreList":        schema_apis_federation_pingcap_v1alpha1_VolumeRestoreList(ref),
-		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeRestoreSpec":        schema_apis_federation_pingcap_v1alpha1_VolumeRestoreSpec(ref),
+		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.BRConfig":                  schema_apis_federation_pingcap_v1alpha1_BRConfig(ref),
+		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeBackup":              schema_apis_federation_pingcap_v1alpha1_VolumeBackup(ref),
+		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeBackupList":          schema_apis_federation_pingcap_v1alpha1_VolumeBackupList(ref),
+		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeBackupMemberCluster": schema_apis_federation_pingcap_v1alpha1_VolumeBackupMemberCluster(ref),
+		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeBackupMemberSpec":    schema_apis_federation_pingcap_v1alpha1_VolumeBackupMemberSpec(ref),
+		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeBackupSchedule":      schema_apis_federation_pingcap_v1alpha1_VolumeBackupSchedule(ref),
+		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeBackupScheduleList":  schema_apis_federation_pingcap_v1alpha1_VolumeBackupScheduleList(ref),
+		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeBackupScheduleSpec":  schema_apis_federation_pingcap_v1alpha1_VolumeBackupScheduleSpec(ref),
+		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeBackupSpec":          schema_apis_federation_pingcap_v1alpha1_VolumeBackupSpec(ref),
+		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeRestore":             schema_apis_federation_pingcap_v1alpha1_VolumeRestore(ref),
+		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeRestoreList":         schema_apis_federation_pingcap_v1alpha1_VolumeRestoreList(ref),
+		"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeRestoreSpec":         schema_apis_federation_pingcap_v1alpha1_VolumeRestoreSpec(ref),
+	}
+}
+
+func schema_apis_federation_pingcap_v1alpha1_BRConfig(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "BRConfig contains config for BR",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"concurrency": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Concurrency is the size of thread pool on each node that execute the backup task",
+							Type:        []string{"integer"},
+							Format:      "int64",
+						},
+					},
+					"checkRequirements": {
+						SchemaProps: spec.SchemaProps{
+							Description: "CheckRequirements specifies whether to check requirements",
+							Type:        []string{"boolean"},
+							Format:      "",
+						},
+					},
+					"sendCredToTikv": {
+						SchemaProps: spec.SchemaProps{
+							Description: "SendCredToTikv specifies whether to send credentials to TiKV",
+							Type:        []string{"boolean"},
+							Format:      "",
+						},
+					},
+					"options": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Options means options for backup data to remote storage with BR. These options has highest priority.",
+							Type:        []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Default: "",
+										Type:    []string{"string"},
+										Format:  "",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
 	}
 }
 
@@ -121,6 +173,156 @@ func schema_apis_federation_pingcap_v1alpha1_VolumeBackupList(ref common.Referen
 		},
 		Dependencies: []string{
 			"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeBackup", "k8s.io/apimachinery/pkg/apis/meta/v1.ListMeta"},
+	}
+}
+
+func schema_apis_federation_pingcap_v1alpha1_VolumeBackupMemberCluster(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "VolumeBackupMemberCluster contains the TiDB cluster which need to execute volume backup",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"k8sClusterName": {
+						SchemaProps: spec.SchemaProps{
+							Description: "K8sClusterName is the name of the k8s cluster where the tc locates",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"tcName": {
+						SchemaProps: spec.SchemaProps{
+							Description: "TCName is the name of the TiDBCluster CR which need to execute volume backup",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"tcNamespace": {
+						SchemaProps: spec.SchemaProps{
+							Description: "TCNamespace is the namespace of the TiDBCluster CR",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func schema_apis_federation_pingcap_v1alpha1_VolumeBackupMemberSpec(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "VolumeBackupMemberSpec contains the backup specification for one tidb cluster",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"resources": {
+						SchemaProps: spec.SchemaProps{
+							Default: map[string]interface{}{},
+							Ref:     ref("k8s.io/api/core/v1.ResourceRequirements"),
+						},
+					},
+					"env": {
+						SchemaProps: spec.SchemaProps{
+							Description: "List of environment variables to set in the container, like v1.Container.Env. Note that the following builtin env vars will be overwritten by values set here - S3_PROVIDER - S3_ENDPOINT - AWS_REGION - AWS_ACL - AWS_STORAGE_CLASS - AWS_DEFAULT_REGION - AWS_ACCESS_KEY_ID - AWS_SECRET_ACCESS_KEY - GCS_PROJECT_ID - GCS_OBJECT_ACL - GCS_BUCKET_ACL - GCS_LOCATION - GCS_STORAGE_CLASS - GCS_SERVICE_ACCOUNT_JSON_KEY - BR_LOG_TO_TERM",
+							Type:        []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Default: map[string]interface{}{},
+										Ref:     ref("k8s.io/api/core/v1.EnvVar"),
+									},
+								},
+							},
+						},
+					},
+					"br": {
+						SchemaProps: spec.SchemaProps{
+							Description: "BRConfig is the configs for BR",
+							Ref:         ref("github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.BRConfig"),
+						},
+					},
+					"s3": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("github.com/pingcap/tidb-operator/pkg/apis/pingcap/v1alpha1.S3StorageProvider"),
+						},
+					},
+					"gcs": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("github.com/pingcap/tidb-operator/pkg/apis/pingcap/v1alpha1.GcsStorageProvider"),
+						},
+					},
+					"azblob": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("github.com/pingcap/tidb-operator/pkg/apis/pingcap/v1alpha1.AzblobStorageProvider"),
+						},
+					},
+					"local": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("github.com/pingcap/tidb-operator/pkg/apis/pingcap/v1alpha1.LocalStorageProvider"),
+						},
+					},
+					"tolerations": {
+						SchemaProps: spec.SchemaProps{
+							Type: []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Default: map[string]interface{}{},
+										Ref:     ref("k8s.io/api/core/v1.Toleration"),
+									},
+								},
+							},
+						},
+					},
+					"toolImage": {
+						SchemaProps: spec.SchemaProps{
+							Description: "ToolImage specifies the tool image used in `Backup`, which supports BR and Dumpling images. For examples `spec.toolImage: pingcap/br:v4.0.8` or `spec.toolImage: pingcap/dumpling:v4.0.8` For BR image, if it does not contain tag, Pod will use image 'ToolImage:${TiKV_Version}'.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"imagePullSecrets": {
+						SchemaProps: spec.SchemaProps{
+							Description: "ImagePullSecrets is an optional list of references to secrets in the same namespace to use for pulling any of the images.",
+							Type:        []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Default: map[string]interface{}{},
+										Ref:     ref("k8s.io/api/core/v1.LocalObjectReference"),
+									},
+								},
+							},
+						},
+					},
+					"serviceAccount": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Specify service account of backup",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"cleanPolicy": {
+						SchemaProps: spec.SchemaProps{
+							Description: "CleanPolicy denotes whether to clean backup data when the object is deleted from the cluster, if not set, the backup data will be retained",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"priorityClassName": {
+						SchemaProps: spec.SchemaProps{
+							Description: "PriorityClassName of Backup Job Pods",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+				},
+			},
+		},
+		Dependencies: []string{
+			"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.BRConfig", "github.com/pingcap/tidb-operator/pkg/apis/pingcap/v1alpha1.AzblobStorageProvider", "github.com/pingcap/tidb-operator/pkg/apis/pingcap/v1alpha1.GcsStorageProvider", "github.com/pingcap/tidb-operator/pkg/apis/pingcap/v1alpha1.LocalStorageProvider", "github.com/pingcap/tidb-operator/pkg/apis/pingcap/v1alpha1.S3StorageProvider", "k8s.io/api/core/v1.EnvVar", "k8s.io/api/core/v1.LocalObjectReference", "k8s.io/api/core/v1.ResourceRequirements", "k8s.io/api/core/v1.Toleration"},
 	}
 }
 
@@ -226,8 +428,31 @@ func schema_apis_federation_pingcap_v1alpha1_VolumeBackupSpec(ref common.Referen
 			SchemaProps: spec.SchemaProps{
 				Description: "VolumeBackupSpec describes the attributes that a user creates on a volume backup.",
 				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"clusters": {
+						SchemaProps: spec.SchemaProps{
+							Type: []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Default: map[string]interface{}{},
+										Ref:     ref("github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeBackupMemberCluster"),
+									},
+								},
+							},
+						},
+					},
+					"template": {
+						SchemaProps: spec.SchemaProps{
+							Default: map[string]interface{}{},
+							Ref:     ref("github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeBackupMemberSpec"),
+						},
+					},
+				},
 			},
 		},
+		Dependencies: []string{
+			"github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeBackupMemberCluster", "github.com/pingcap/tidb-operator/pkg/apis/federation/pingcap/v1alpha1.VolumeBackupMemberSpec"},
 	}
 }
 
