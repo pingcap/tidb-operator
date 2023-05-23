caa4a67752190e5bacdb5c33185d2a8d4fb1db57
BornChanger <dawn_catcher@126.com>
2023-05-23 19:20:11 +0800

    *: clean up redundant code
    Signed-off-by: BornChanger <dawn_catcher@126.com>

--- cmd/backup-manager/app/util/backup_size.go
+++ cmd/backup-manager/app/util/backup_size.go
@@ -17,14 +17,12 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"sort"
 	"strings"
 	"sync/atomic"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ebs"
-	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/pingcap/errors"
 	"github.com/pingcap/tidb-operator/cmd/backup-manager/app/constants"
 	"github.com/pingcap/tidb-operator/pkg/apis/pingcap/v1alpha1"
@@ -44,15 +42,8 @@ import (
 // interface CalcVolSnapBackupSize called by backup and backup clean.
 
 const (
-	// This value can be between 5 and 1,000; if MaxResults is given a value larger than 1,000, only 1,000 results are returned.
-	DescribeSnapMaxReturnResult = 1000
-
 	// This value can be between 100 and 1,0000, and charge ~0.6$/1 million request
 	ListSnapMaxReturnResult = 10000
-
-	// This value can be between 100 and 1,0000, and charge ~0.6$/1 million request
-	ListBlocksMaxReturnResult = 10000
-
 	// This value can be between 1 and 50 due to aws service quota
 	EbsApiConcurrency = 40
 )
@@ -139,69 +130,6 @@ func getSnapshotsFromBackupmeta(ctx context.Context, provider v1alpha1.StoragePr
 	return volumeIDMap, nil
 }
 
-// getBackupVolSnapshots get a volume-snapshots map contains map[volumeId]{snapshot1, snapshot2, snapshot3}
-func getBackupVolSnapshots(volumes map[string]string) (map[string][]*ec2.Snapshot, error) {
-	volWithTheirSnapshots := make(map[string][]*ec2.Snapshot)
-
-	// read all snapshots from aws
-	ec2Session, err := util.NewEC2Session(util.CloudAPIConcurrency)
-	if err != nil {
-		klog.Errorf("new a ec2 session failure.")
-		return nil, err
-	}
-
-	// init search filter.Values
-	// init volWithTheirSnapshots
-	volValues := make([]*string, 0)
-	for volumeId := range volumes {
-		volValues = append(volValues, aws.String(volumeId))
-		if volWithTheirSnapshots[volumeId] == nil {
-			volWithTheirSnapshots[volumeId] = make([]*ec2.Snapshot, 0)
-		}
-	}
-
-	filters := []*ec2.Filter{{Name: aws.String("volume-id"), Values: volValues}}
-	// describe snapshot is heavy operator, try to call only once
-	// api has limit with max 1000 snapshots
-	// search with filter volume id the backupmeta contains
-	var nextToken *string
-	for {
-		resp, err := ec2Session.EC2.DescribeSnapshots(&ec2.DescribeSnapshotsInput{
-			OwnerIds:   aws.StringSlice([]string{"self"}),
-			MaxResults: aws.Int64(DescribeSnapMaxReturnResult),
-			Filters:    filters,
-			NextToken:  nextToken,
-		})
-
-		if err != nil {
-			return nil, err
-		}
-
-		for i, s := range resp.Snapshots {
-			if *s.State == ec2.SnapshotStateCompleted {
-				if volWithTheirSnapshots[*s.VolumeId] == nil {
-					klog.Errorf("search with filter[volume-id] received unexpected result, volumeId:%s, snapshotId:%s", *s.VolumeId, *s.SnapshotId)
-					break
-				}
-				klog.Infof("the snapshot#%d %s created for volume %s", i, *s.SnapshotId, *s.VolumeId)
-				volWithTheirSnapshots[*s.VolumeId] = append(volWithTheirSnapshots[*s.VolumeId], s)
-			} else { // skip ongoing snapshots
-				klog.Infof("the snapshot#%d %s creating... skip it", i, *s.SnapshotId)
-				continue
-			}
-		}
-
-		// check if there's more to retrieve
-		if resp.NextToken == nil {
-			break
-		}
-		klog.Infof("the total number of snapshot is %d", len(resp.Snapshots))
-		nextToken = resp.NextToken
-	}
-
-	return volWithTheirSnapshots, nil
-}
-
 // calcBackupSize get a volume-snapshots backup size
 func calcBackupSize(ctx context.Context, volumes map[string]string) (uint64, error) {
 	var backupSize uint64
@@ -267,74 +195,5 @@ func calculateSnapshotSize(snapshotId string) (uint64, uint64, error) {
 		nextToken = resp.NextToken
 	}
 	klog.Infof("full backup snapshot size %d bytes, num of ListSnapshotBlocks request %d", snapshotSize, numApiReq)
-	return snapshotSize, numApiReq, nil
-}
-
-func getPrevSnapshotId(snapshotId string, volSnapshots []*ec2.Snapshot) (string, error) {
-	var prevSnapshotId string
-
-	sort.Slice(volSnapshots, func(i, j int) bool {
-		return volSnapshots[i].StartTime.Before(*volSnapshots[j].StartTime)
-	})
-
-	for i, snapshot := range volSnapshots {
-		if snapshotId == *snapshot.SnapshotId {
-			// the first snapshot for the volume
-			if i == 0 {
-				return "", nil
-			}
-			prevSnapshotId = *volSnapshots[i-1].SnapshotId
-			klog.Infof("the prevSnapshot index is %d, ID is %s", i, *snapshot.SnapshotId)
-			break
-		}
-	}
-	if len(prevSnapshotId) == 0 {
-		return "", fmt.Errorf("Could not find the prevousely snapshot id, current snapshotId: %s.", snapshotId)
-	}
-	return prevSnapshotId, nil
-}
-
-// changedBlocksSize calculates changed blocks total size in bytes between two snapshots with common ancestry.
-func changedBlocksSize(preSnapshotId string, snapshotId string) (uint64, uint64, error) {
-	var numBlocks int
-	var snapshotSize uint64
-	var numApiReq uint64
-
-	klog.Infof("the calc snapshot size for %s, base on prev snapshot %s", snapshotId, preSnapshotId)
-	ebsSession, err := util.NewEBSSession(util.CloudAPIConcurrency)
-	if err != nil {
-		klog.Errorf("new a ebs session failure.")
-		return 0, numApiReq, err
-	}
-
-	var nextToken *string
-
-	for {
-		resp, err := ebsSession.EBS.ListChangedBlocks(&ebs.ListChangedBlocksInput{
-			FirstSnapshotId:  aws.String(preSnapshotId),
-			MaxResults:       aws.Int64(ListBlocksMaxReturnResult),
-			SecondSnapshotId: aws.String(snapshotId),
-			NextToken:        nextToken,
-		})
-		numApiReq += 1
-		if err != nil {
-			return 0, numApiReq, err
-		}
-		numBlocks += len(resp.ChangedBlocks)
-
-		// retrieve only changed block and blocks only existed in current snapshot (new add blocks)
-		for _, block := range resp.ChangedBlocks {
-			if block.SecondBlockToken != nil && resp.BlockSize != nil {
-				snapshotSize += uint64(*resp.BlockSize)
-			}
-		}
-
-		// check if there is more to retrieve
-		if resp.NextToken == nil {
-			break
-		}
-		nextToken = resp.NextToken
-	}
-	klog.Infof("the total size of snapshot %d, num of api ListChangedBlocks request %d, snapshot id %s", snapshotSize, numApiReq, snapshotId)
 	return snapshotSize, numApiReq, nil
 }
