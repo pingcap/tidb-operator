37e12925b8e1d86b4450af913a852aa1ab6fa274
WangLe1321 <wangle1321@163.com>
2023-06-21 17:50:53 +0800

    br: fix warmup template
    Signed-off-by: WangLe1321 <wangle1321@163.com>

--- pkg/controller/restore/restore_controller.go
+++ pkg/controller/restore/restore_controller.go
@@ -195,7 +195,7 @@ func (c *Controller) updateRestore(cur interface{}) {
 	}
 
 	if v1alpha1.IsRestoreVolumeComplete(newRestore) {
-		tc, err := c.getTC(newRestore)
+		/*tc, err := c.getTC(newRestore)
 		if err != nil {
 			klog.Errorf("Fail to get tidbcluster for restore %s/%s, %v", ns, name, err)
 			return
@@ -204,7 +204,7 @@ func (c *Controller) updateRestore(cur interface{}) {
 		if _, ok := tc.Annotations[label.AnnTiKVVolumesReadyKey]; ok && v1alpha1.IsRestoreWarmUpStarted(newRestore) {
 			klog.V(4).Infof("restore %s/%s is already VolumeComplete, skipping.", ns, name)
 			return
-		}
+		}*/
 
 		c.enqueueRestore(newRestore)
 		return
