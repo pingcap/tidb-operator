fd50ea56fbb7290cd307aee3f75552b18345b335
xhe <xw897002528@gmail.com>
2023-07-11 17:02:13 +0800

    fix: add missing tiproxy label (#5164)

--- pkg/apis/pingcap/v1alpha1/component_spec.go
+++ pkg/apis/pingcap/v1alpha1/component_spec.go
@@ -471,6 +471,8 @@ func getComponentLabelValue(c MemberType) string {
 		return label.TiKVLabelVal
 	case TiFlashMemberType:
 		return label.TiFlashLabelVal
+	case TiProxyMemberType:
+		return label.TiProxyLabelVal
 	case TiCDCMemberType:
 		return label.TiCDCLabelVal
 	case PumpMemberType:
