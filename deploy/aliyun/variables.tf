88af89a18b7d1d5e9cf333d1cc35eb7925e4fd11
xhe <xw897002528@gmail.com>
2023-08-17 13:04:04 +0800

    bump to v1.6.0-alpha.6 (#5251)
    Signed-off-by: xhe <xw897002528@gmail.com>

--- deploy/aliyun/variables.tf
+++ deploy/aliyun/variables.tf
@@ -10,7 +10,7 @@ variable "bastion_cpu_core_count" {
 
 variable "operator_version" {
   type    = string
-  default = "v1.6.0-alpha.5"
+  default = "v1.6.0-alpha.6"
 }
 
 variable "operator_helm_values" {
--- deploy/aws/variables.tf
+++ deploy/aws/variables.tf
@@ -19,7 +19,7 @@ variable "eks_version" {
 
 variable "operator_version" {
   description = "TiDB operator version"
-  default     = "v1.6.0-alpha.5"
+  default     = "v1.6.0-alpha.6"
 }
 
 variable "operator_values" {
