b571bef3ffe225c1a74e81b01283d245a0841f62
hillium <yujuncen@pingcap.com>
2023-10-11 17:24:47 +0800

    fix build
    Signed-off-by: hillium <yujuncen@pingcap.com>

--- cmd/http-service/pbgen/api/service.pb.go
+++ cmd/http-service/pbgen/api/service.pb.go
@@ -15,8 +15,6 @@ import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	structpb "google.golang.org/protobuf/types/known/structpb"
-	reflect "reflect"
-	sync "sync"
 )
 
 const (
