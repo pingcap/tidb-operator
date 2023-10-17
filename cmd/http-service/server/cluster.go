7acd9b1af546e7f7d5a233c563ec9e211c1646cd
csuzhangxc <csuzhangxc@gmail.com>
2023-10-17 12:01:53 +0000

    basic status

--- cmd/http-service/server/cluster.go
+++ cmd/http-service/server/cluster.go
@@ -18,6 +18,8 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/pingcap/log"
 	"go.uber.org/zap"
@@ -32,7 +34,22 @@ import (
 	"github.com/pingcap/tidb-operator/pkg/apis/pingcap/v1alpha1"
 )
 
+type ClusterStatus string
+
 const (
+	// the cluster is still being created
+	ClusterStatusCreating ClusterStatus = "creating"
+	// all components are running
+	ClusterStatusRunning ClusterStatus = "running"
+	// some components are deleting
+	ClusterStatusDeleting ClusterStatus = "deleting"
+	// some components are scaling
+	ClusterStatusScaling ClusterStatus = "scaling"
+	// some components are upgrading
+	ClusterStatusUpgrading ClusterStatus = "upgrading"
+	// some components are unavailable
+	ClusterStatusUnavailable ClusterStatus = "unavailable"
+
 	helperImage = "busybox:1.36"
 
 	// try `kanshiori/mysqlclient-arm64` for ARM64
@@ -470,5 +487,144 @@ func convertMonitorComponetsResources(req *api.CreateClusterReq) (promRes, grafa
 }
 
 func (s *ClusterServer) GetCluster(ctx context.Context, req *api.GetClusterReq) (*api.GetClusterResp, error) {
-	return nil, nil
+	k8sID := getKubernetesID(ctx)
+	opCli := s.KubeClient.GetOperatorClient(k8sID)
+	kubeCli := s.KubeClient.GetKubeClient(k8sID)
+	logger := log.L().With(zap.String("request", "GetCluster"), zap.String("k8sID", k8sID), zap.String("clusterID", req.ClusterId))
+	if opCli == nil || kubeCli == nil {
+		logger.Error("K8s client not found")
+		message := fmt.Sprintf("no %s is specified in the request header or the kubeconfig context not exists", HeaderKeyKubernetesID)
+		setResponseStatusCodes(ctx, http.StatusBadRequest)
+		return &api.GetClusterResp{Success: false, Message: &message}, nil
+	}
+
+	tc, err := opCli.PingcapV1alpha1().TidbClusters(req.ClusterId).Get(ctx, tidbClusterName, metav1.GetOptions{})
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			logger.Error("TidbCluster not found", zap.Error(err))
+			setResponseStatusCodes(ctx, http.StatusNotFound)
+			message := fmt.Sprintf("TidbCluster %s not found", req.ClusterId)
+			return &api.GetClusterResp{Success: false, Message: &message}, nil
+		} else {
+			logger.Error("Get TidbCluster failed", zap.Error(err))
+			message := fmt.Sprintf("get TidbCluster failed: %s", err.Error())
+			setResponseStatusCodes(ctx, http.StatusInternalServerError)
+			return &api.GetClusterResp{Success: false, Message: &message}, nil
+		}
+	}
+
+	info := convertToClusterInfo(logger, tc)
+	return &api.GetClusterResp{Success: true, Data: info}, nil
+}
+
+func convertToClusterInfo(logger *zap.Logger, tc *v1alpha1.TidbCluster) *api.ClusterInfo {
+	info := &api.ClusterInfo{
+		ClusterId: tc.Namespace,
+		Version:   tc.Spec.Version,
+		Status:    string(convertClusterStatus(tc)),
+		Pd: &api.PDStatus{
+			Phase:    string(tc.Status.PD.Phase),
+			Replicas: uint32(tc.Spec.PD.Replicas),
+			Members:  make([]*api.PDMember, 0, len(tc.Status.PD.Members)),
+			// TODO(csuzhangxc): resource
+			// TODO(csuzhangxc): config
+		},
+		Tikv: &api.TiKVStatus{
+			Phase:    string(tc.Status.TiKV.Phase),
+			Replicas: uint32(tc.Spec.TiKV.Replicas),
+			Members:  make([]*api.TiKVMember, 0, len(tc.Status.TiKV.Stores)),
+		},
+		Tidb: &api.TiDBStatus{
+			Phase:    string(tc.Status.TiDB.Phase),
+			Replicas: uint32(tc.Spec.TiDB.Replicas),
+			Members:  make([]*api.TiDBMember, 0, len(tc.Status.TiDB.Members)),
+		},
+	}
+
+	// TODO(csuzhangxc): start time
+	namePrefix := fmt.Sprintf("%s-", tc.Name)
+	for name, member := range tc.Status.PD.Members {
+		id, err := strconv.ParseUint(member.ID, 10, 64)
+		if err != nil {
+			logger.Error("Parse PD member ID failed", zap.String("id", member.ID), zap.Error(err))
+		}
+		info.Pd.Members = append(info.Pd.Members, &api.PDMember{
+			Name:   strings.TrimPrefix(name, namePrefix),
+			Id:     id,
+			Health: member.Health,
+		})
+	}
+
+	for _, member := range tc.Status.TiKV.Stores {
+		id, err := strconv.ParseUint(member.ID, 10, 64)
+		if err != nil {
+			logger.Error("Parse TiKV store ID failed", zap.String("id", member.ID), zap.Error(err))
+		}
+		info.Tikv.Members = append(info.Tikv.Members, &api.TiKVMember{
+			Name:  strings.TrimPrefix(member.PodName, namePrefix),
+			Id:    id,
+			State: string(member.State),
+		})
+	}
+
+	tidbHealthCount := 0
+	for name, member := range tc.Status.TiDB.Members {
+		info.Tidb.Members = append(info.Tidb.Members, &api.TiDBMember{
+			Name:   strings.TrimPrefix(name, namePrefix),
+			Health: member.Health,
+		})
+		if member.Health {
+			tidbHealthCount++
+		}
+	}
+
+	if !tc.PDIsAvailable() || !tc.TiKVIsAvailable() || tidbHealthCount == 0 {
+		info.Status = string(ClusterStatusUnavailable)
+	}
+
+	if tc.Spec.TiFlash != nil && tc.Spec.TiFlash.Replicas > 0 {
+		info.Tiflash = &api.TiFlashStatus{
+			Phase:    string(tc.Status.TiFlash.Phase),
+			Replicas: uint32(tc.Spec.TiFlash.Replicas),
+			Members:  make([]*api.TiFlashMember, 0, len(tc.Status.TiFlash.Stores)),
+		}
+		tiflashReadyCount := 0
+		for _, member := range tc.Status.TiFlash.Stores {
+			id, err := strconv.ParseUint(member.ID, 10, 64)
+			if err != nil {
+				logger.Error("Parse TiFlash store ID failed", zap.String("id", member.ID), zap.Error(err))
+			}
+			info.Tiflash.Members = append(info.Tiflash.Members, &api.TiFlashMember{
+				Name:  strings.TrimPrefix(member.PodName, namePrefix),
+				Id:    id,
+				State: string(member.State),
+			})
+			if member.State == v1alpha1.TiKVStateUp {
+				tiflashReadyCount++
+			}
+		}
+		if tiflashReadyCount == 0 {
+			info.Status = string(ClusterStatusUnavailable)
+		}
+	}
+
+	return info
+}
+
+func convertClusterStatus(tc *v1alpha1.TidbCluster) ClusterStatus {
+	if tc.DeletionTimestamp != nil {
+		return ClusterStatusDeleting
+	}
+
+	// TODO(csuzhangxc): creating
+
+	if tc.Status.PD.Phase == v1alpha1.UpgradePhase || tc.Status.TiKV.Phase == v1alpha1.UpgradePhase || tc.Status.TiDB.Phase == v1alpha1.UpgradePhase {
+		return ClusterStatusUpgrading
+	}
+
+	if tc.Status.PD.Phase == v1alpha1.ScalePhase || tc.Status.TiKV.Phase == v1alpha1.ScalePhase || tc.Status.TiDB.Phase == v1alpha1.ScalePhase {
+		return ClusterStatusScaling
+	}
+
+	return ClusterStatusRunning
 }
