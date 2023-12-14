ac686c295207b884722a3a4a3a69a5c00e267ac6
BornChanger <dawn_catcher@126.com>
2023-12-14 19:33:14 +0800

    ebs br: make sure backup can be resumed even after being paused for a long time
    Signed-off-by: BornChanger <dawn_catcher@126.com>

--- pkg/fedvolumebackup/backupschedule/backup_schedule_manager_test.go
+++ pkg/fedvolumebackup/backupschedule/backup_schedule_manager_test.go
@@ -170,10 +170,10 @@ func TestGetLastScheduledTime(t *testing.T) {
 	}
 
 	// test too many miss
-	bs.Status.LastBackupTime.Time = now.AddDate(-1000, 0, 0)
+	bs.Status.LastBackupTime.Time = now.AddDate(0, -12, 0)
 	getTime, err = getLastScheduledTime(bs, time.Now)
 	g.Expect(err).Should(BeNil())
-	g.Expect(getTime).Should(BeNil())
+	g.Expect(getTime).ShouldNot(BeNil())
 }
 
 func TestBuildBackup(t *testing.T) {
