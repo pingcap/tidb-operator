44111ca30fa8835e2ad88aeaf69008ec62f3131e
WangLe1321 <wangle1321@163.com>
2023-12-19 16:56:46 +0800

    br: fix warmup tmp file creating error

--- images/ebs-warmup/warmup-steps.sh
+++ images/ebs-warmup/warmup-steps.sh
@@ -36,6 +36,9 @@ EOF
 
 # The trap command is to make sure the sidecars are terminated when the jobs are finished
 cleanup() {
+    if [ ! -d "/tmp/pod" ]; then
+        mkdir -p /tmp/pod
+    fi
     touch /tmp/pod/main-terminated
 }
 
