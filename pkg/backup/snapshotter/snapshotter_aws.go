674561796baa8c5e617f41402ab6d632e9cf4516
BornChanger <dawn_catcher@126.com>
2024-01-10 19:18:57 +0800

    *: clean up redundant code
    Signed-off-by: BornChanger <dawn_catcher@126.com>

--- pkg/backup/snapshotter/snapshotter_aws.go
+++ pkg/backup/snapshotter/snapshotter_aws.go
@@ -30,7 +30,6 @@ const (
 	CloudAPIConcurrency = 8
 	PVCTagKey           = "CSIVolumeName"
 	PodTagKey           = "kubernetes.io/created-for/pvc/name"
-	PVAvailableStatus   = "Available"
 )
 
 // AWSSnapshotter is the snapshotter for creating snapshots from volumes (during a backup)
@@ -103,10 +102,6 @@ func (s *AWSSnapshotter) AddVolumeTags(pvs []*corev1.PersistentVolume) error {
 	resourcesTags := make(map[string]util.TagMap)
 
 	for _, pv := range pvs {
-		// Only tagging to available volumes
-		if pv.Status.Phase != PVAvailableStatus {
-			continue
-		}
 		podName := pv.GetAnnotations()[label.AnnPodNameKey]
 		pvcName := pv.GetName()
 		volId := pv.Spec.CSI.VolumeHandle
