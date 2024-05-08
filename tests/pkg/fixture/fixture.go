c0ce5a6b5514a1331da2fa1f78f1bffa3c3ea327
csuzhangxc <csuzhangxc@gmail.com>
2024-05-08 09:14:33 +0000

    update image and version for PD ms

--- tests/pkg/fixture/fixture.go
+++ tests/pkg/fixture/fixture.go
@@ -798,29 +798,17 @@ func AddPDMSForTidbCluster(tc *v1alpha1.TidbCluster) *v1alpha1.TidbCluster {
 	if tc.Spec.PDMS != nil {
 		return tc
 	}
-	pdmsImage := "hub.pingcap.net/devbuild/pd"
-	version := "v8.0.0-4193"
-	// TODO: remove pd version when released pdms
-	tc.Spec.PD.BaseImage = "hub.pingcap.net/devbuild/pd"
-	tc.Spec.PD.Version = &version
+	pdmsImage := "pingcap/pd"
 	tc.Spec.PDMS = []*v1alpha1.PDMSSpec{
 		{
 			Name: tsoService,
-			// TODO: replace pdms image when released pdms
 			BaseImage: &pdmsImage,
-			ComponentSpec: v1alpha1.ComponentSpec{
-				Version: &version,
-			},
 			Replicas:             2,
 			ResourceRequirements: WithStorage(BurstableSmall, "10Gi"),
 		},
 		{
 			Name: schedulingService,
-			// TODO: replace pdms image when released pdms
 			BaseImage: &pdmsImage,
-			ComponentSpec: v1alpha1.ComponentSpec{
-				Version: &version,
-			},
 			Replicas:             1,
 			ResourceRequirements: WithStorage(BurstableSmall, "10Gi"),
 		},
