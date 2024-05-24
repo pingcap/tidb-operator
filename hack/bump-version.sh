50afd40e0870a0e11fd9e8292788b0c7abfb463e
Xuecheng Zhang <csuzhangxc@gmail.com>
2024-05-25 00:10:23 +0800

    bump TiDB Operator to 1.6.0 and TiDB to 8.1.0 (#5641)

--- hack/bump-version.sh
+++ hack/bump-version.sh
@@ -22,12 +22,12 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 # parameters
-OPERATOR_OLD="v1\.6\.0-alpha.10"
-OPERATOR_NEW="v1\.6\.0-beta.1"
-TIDB_OLD="v7\.1\.1"
-TIDB_NEW="v7\.5\.1"
-DM_OLD="v7.1.1"
-DM_NEW="v7.5.1"
+OPERATOR_OLD="v1.6.0-beta.1"
+OPERATOR_NEW="v1.6.0"
+TIDB_OLD="v7.5.1"
+TIDB_NEW="v8.1.0"
+DM_OLD="v7.5.1"
+DM_NEW="v8.1.0"
 
 find ./deploy -name "*\.tf"| xargs $SED_BIN -i "s/$OPERATOR_OLD/$OPERATOR_NEW/g"
 find ./charts -name "*\.yaml"| xargs $SED_BIN -i "s/$OPERATOR_OLD/$OPERATOR_NEW/g"
