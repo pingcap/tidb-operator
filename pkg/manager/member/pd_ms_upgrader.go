15de8144f7afe4566af0da64463279b45683e81d
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-08-14 11:14:46 +0800

    pdms: Choose a suitable pdms to transfer primary when upgrade (#5643) (#5709)
    Signed-off-by: husharp <ihusharp@gmail.com>
    Co-authored-by: husharp <ihusharp@gmail.com>

--- pkg/manager/member/pd_ms_upgrader.go
+++ pkg/manager/member/pd_ms_upgrader.go
@@ -22,6 +22,7 @@ import (
 	"github.com/pingcap/tidb-operator/pkg/controller"
 	mngerutils "github.com/pingcap/tidb-operator/pkg/manager/utils"
 	"github.com/pingcap/tidb-operator/pkg/third_party/k8s"
+	"github.com/pingcap/tidb-operator/pkg/util/cmpver"
 	apps "k8s.io/api/apps/v1"
 	"k8s.io/klog/v2"
 )
@@ -120,11 +121,94 @@ func (u *pdMSUpgrader) gracefulUpgrade(tc *v1alpha1.TidbCluster, oldSet *apps.St
 			}
 			continue
 		}
-		mngerutils.SetUpgradePartition(newSet, i)
-		return nil
+
+		return u.upgradePDMSPod(tc, i, newSet, curService)
 	}
 	return nil
 }
+
+func (u *pdMSUpgrader) upgradePDMSPod(tc *v1alpha1.TidbCluster, ordinal int32, newSet *apps.StatefulSet, curService string) error {
+	// Only support after `8.3.0` to keep compatibility.
+	if check, err := pdMSSupportMicroServicesWithName.Check(tc.PDMSVersion(curService)); check && err == nil {
+		ns := tc.GetNamespace()
+		tcName := tc.GetName()
+		upgradePDMSName := PDMSName(tcName, ordinal, tc.Namespace, tc.Spec.ClusterDomain, tc.Spec.AcrossK8s, curService)
+		upgradePodName := PDMSPodName(tcName, ordinal, curService)
+
+		pdClient := controller.GetPDClient(u.deps.PDControl, tc)
+		primary, err := pdClient.GetMSPrimary(curService)
+		if err != nil {
+			return err
+		}
+
+		klog.Infof("TidbCluster: [%s/%s]' pdms upgrader: check primary: %s, upgradePDMSName: %s, upgradePodName: %s", ns, tcName,
+			primary, upgradePDMSName, upgradePodName)
+		// If current pdms is primary, transfer primary to other pdms pod
+		if strings.Contains(primary, upgradePodName) || strings.Contains(primary, upgradePDMSName) {
+			targetName := ""
+
+			if tc.PDMSStsActualReplicas(curService) > 1 {
+				targetName = choosePDMSToTransferFromMembers(tc, newSet, ordinal)
+			}
+
+			if targetName != "" {
+				klog.Infof("TidbCluster: [%s/%s]' pdms upgrader: transfer pdms primary to: %s", ns, tcName, targetName)
+				err := controller.GetPDMSClient(u.deps.PDControl, tc, curService).TransferPrimary(targetName)
+				if err != nil {
+					klog.Errorf("TidbCluster: [%s/%s]' pdms upgrader: failed to transfer pdms primary to: %s, %v", ns, tcName, targetName, err)
+					return err
+				}
+				klog.Infof("TidbCluster: [%s/%s]' pdms upgrader: transfer pdms primary to: %s successfully", ns, tcName, targetName)
+			} else {
+				klog.Warningf("TidbCluster: [%s/%s]' pdms upgrader: skip to transfer pdms primary, because can not find a suitable pd", ns, tcName)
+			}
+		}
+	}
+
+	mngerutils.SetUpgradePartition(newSet, ordinal)
+	return nil
+}
+
+// choosePDMSToTransferFromMembers choose a pdms to transfer primary from members
+//
+// Assume that current primary ordinal is x, and range is [0, n]
+//  1. Find the max suitable ordinal in (x, n], because they have been upgraded
+//  2. If no suitable ordinal, find the min suitable ordinal in [0, x) to reduce the count of transfer
+func choosePDMSToTransferFromMembers(tc *v1alpha1.TidbCluster, newSet *apps.StatefulSet, ordinal int32) string {
+	ns := tc.GetNamespace()
+	tcName := tc.GetName()
+	klog.Infof("Tidbcluster: [%s/%s]' pdms upgrader: start to choose pdms to transfer primary from members", ns, tcName)
+	ordinals := helper.GetPodOrdinals(*newSet.Spec.Replicas, newSet)
+
+	// set ordinal to max ordinal if ordinal isn't exist
+	if !ordinals.Has(ordinal) {
+		ordinal = helper.GetMaxPodOrdinal(*newSet.Spec.Replicas, newSet)
+	}
+
+	targetName := ""
+	list := ordinals.List()
+	if len(list) == 0 {
+		return ""
+	}
+
+	// just using pods index for now. TODO: add healthy checker for pdms.
+	// find the maximum ordinal which is larger than ordinal
+	if len(list) > int(ordinal)+1 {
+		targetName = PDMSPodName(tcName, list[len(list)-1], controller.PDMSTrimName(newSet.Name))
+	}
+
+	if targetName == "" && ordinal != 0 {
+		// find the minimum ordinal which is less than ordinal
+		targetName = PDMSPodName(tcName, list[0], controller.PDMSTrimName(newSet.Name))
+	}
+
+	klog.Infof("Tidbcluster: [%s/%s]' pdms upgrader: choose pdms to transfer primary from members, targetName: %s", ns, tcName, targetName)
+	return targetName
+}
+
+// PDMSSupportMicroServicesWithName returns true if the given version of PDMS supports microservices with name.
+// related https://github.com/tikv/pd/pull/8157.
+var pdMSSupportMicroServicesWithName, _ = cmpver.NewConstraint(cmpver.GreaterOrEqual, "v8.3.0")
 
 type fakePDMSUpgrader struct{}
 
