b1053ac9a85484b73d345a07c3b8f7df8f580ca3
Xuecheng Zhang <csuzhangxc@gmail.com>
2024-09-04 11:48:28 +0800

    bump TiDB Operator to v1.5.4, TiDB to v7.5.3 (#5732)

--- deploy/aliyun/variables.tf
+++ deploy/aliyun/variables.tf
@@ -10,7 +10,7 @@ variable "bastion_cpu_core_count" {
 
 variable "operator_version" {
   type    = string
-  default = "v1.5.3"
+  default = "v1.5.4"
 }
 
 variable "operator_helm_values" {
@@ -36,7 +36,7 @@ variable "cluster_name" {
 
 variable "tidb_version" {
   description = "TiDB cluster version"
-  default     = "v7.5.1"
+  default     = "v7.5.3"
 }
 variable "tidb_cluster_chart_version" {
   description = "tidb-cluster chart version"
