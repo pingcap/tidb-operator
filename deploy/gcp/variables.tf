ce7a8ef4b2ff295ac86e00623939b7faec9593c3
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-12-20 09:42:14 +0800

    bump TiDB Operator to 1.6.1 and TiDB to 8.5.0 (#5990)
    Co-authored-by: csuzhangxc <csuzhangxc@gmail.com>

--- deploy/gcp/variables.tf
+++ deploy/gcp/variables.tf
@@ -24,11 +24,11 @@ variable "node_locations" {
 
 variable "tidb_version" {
   description = "TiDB version"
-  default     = "v8.1.0"
+  default     = "v8.5.0"
 }
 
 variable "tidb_operator_version" {
-  default = "v1.6.0"
+  default = "v1.6.1"
 }
 
 variable "tidb_operator_chart_version" {
