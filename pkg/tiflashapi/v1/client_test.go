a3f836c0093da77e5382fd8949f674fbcb31e582
csuzhangxc <csuzhangxc@gmail.com>
2024-12-25 17:53:10 +0800

    ut for TiFlash API

---
+++ pkg/tiflashapi/v1/client_test.go
@@ -0,0 +1,40 @@
+// Copyright 2024 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tiflashapi
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTiFlash_GetStoreStatus(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "/tiflash/store-status", r.URL.Path)
+		assert.Equal(t, http.MethodGet, r.Method)
+		w.Write([]byte("Running"))
+	}))
+	defer server.Close()
+
+	client := NewTiFlashClient(server.URL, 5*time.Second, nil, false)
+	status, err := client.GetStoreStatus(context.Background())
+	require.NoError(t, err)
+	assert.Equal(t, Running, status)
+}
