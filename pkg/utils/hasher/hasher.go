300a551ebee158ec5a4a03686e4b2a6a2d5d9d06
Hoshea <fgksgf@gmail.com>
2025-01-10 14:09:20 +0800

    Fix: generate tiflash config hash by combining tiflash config and proxy config (#6027)

--- pkg/utils/hasher/hasher.go
+++ pkg/utils/hasher/hasher.go
@@ -26,16 +26,21 @@ import (
 	hashutil "github.com/pingcap/tidb-operator/third_party/kubernetes/pkg/util/hash"
 )
 
-// GenerateHash takes a TOML string as input, unmarshals it into a map,
-// and generates a hash of the resulting configuration. The hash is then
-// encoded into a safe string format and returned.
+// GenerateHash takes multiple TOML strings as input, unmarshals each TOML string into a map,
+// then append the map to an array, generates a hash of the resulting configuration.
+// The hash is then encoded into a safe string format and returned.
 // If the order of keys in the TOML string is different, the hash will be the same.
-func GenerateHash(tomlStr v1alpha1.ConfigFile) (string, error) {
-	var config map[string]any
-	if err := toml.NewDecoder(bytes.NewReader([]byte(tomlStr))).Decode(&config); err != nil {
-		return "", fmt.Errorf("failed to unmarshal toml string %s: %w", tomlStr, err)
+// If the order of input TOML strings is different, the hash will be different.
+func GenerateHash(tomlStrings ...v1alpha1.ConfigFile) (string, error) {
+	configs := make([]map[string]any, 0, len(tomlStrings))
+	for _, tomlStr := range tomlStrings {
+		var config map[string]any
+		if err := toml.NewDecoder(bytes.NewReader([]byte(tomlStr))).Decode(&config); err != nil {
+			return "", fmt.Errorf("failed to unmarshal toml string %s: %w", tomlStr, err)
+		}
+		configs = append(configs, config)
 	}
 	hasher := fnv.New32a()
-	hashutil.DeepHashObject(hasher, config)
+	hashutil.DeepHashObject(hasher, configs)
 	return rand.SafeEncodeString(fmt.Sprint(hasher.Sum32())), nil
 }
