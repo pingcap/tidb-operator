bcad14421fe27b8be10ee41d9c2abd19444494bd
liubo02 <liubo02@pingcap.com>
2025-04-28 13:44:32 +0800

    feat(tso): add tso api
    Signed-off-by: liubo02 <liubo02@pingcap.com>

--- tests/validation/validation.go
+++ tests/validation/validation.go
@@ -14,10 +14,52 @@
 
 package validation
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+
+	"github.com/tidbcloud/tidb-operator/tests/validation/third_party/apiextensions-apiserver/pkg/test"
+)
+
 var InstanceCRDs = []string{
 	"crd/core.pingcap.com_pds.yaml",
 	"crd/core.pingcap.com_tikvs.yaml",
 	"crd/core.pingcap.com_tidbs.yaml",
 	"crd/core.pingcap.com_tiflashes.yaml",
 	"crd/core.pingcap.com_ticdcs.yaml",
+	"crd/core.pingcap.com_tsoes.yaml",
+}
+
+type Case struct {
+	desc         string
+	current, old any
+	wantErrs     []string
+}
+
+func Validate(t *testing.T, cases []Case) {
+	for _, crd := range InstanceCRDs {
+		validators := test.FieldValidators(t,
+			test.MustLoadManifest[apiextensionsv1.CustomResourceDefinition](t, crd))
+
+		path := "openAPIV3Schema.properties.spec"
+		validator, found := validators["v1alpha1"][path]
+		require.True(t, found, "failed to find validator for %s", path)
+
+		for i := range cases {
+			c := &cases[i]
+
+			t.Run(c.desc, func(tt *testing.T) {
+				tt.Parallel()
+				errs := validator(c.current, c.old)
+				require.Equal(tt, len(c.wantErrs), len(errs), "%s: %v", c.desc, errs)
+				for i, err := range errs {
+					assert.EqualError(tt, err, c.wantErrs[i], c.desc)
+				}
+			})
+		}
+
+	}
 }
--- tests/validation/validation_test.go
+++ tests/validation/validation_test.go
@@ -16,23 +16,13 @@ package validation
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-
-	"github.com/tidbcloud/tidb-operator/tests/validation/third_party/apiextensions-apiserver/pkg/test"
 )
 
 // Test for
 // - +kubebuilder:validation:XValidation:rule="self == oldSelf",message="topology is immutable"
 // - +kubebuilder:validation:XValidation:rule="(!has(oldSelf.topology) && !has(self.topology)) || (has(oldSelf.topology) && has(self.topology))",fieldPath=".topology",message="topology can only be set when created"
 func TestTopologyImmutable(t *testing.T) {
-	cases := []struct {
-		desc         string
-		current, old any
-		wantErrs     []string
-	}{
+	cases := []Case{
 		{
 			desc:    "topology is always unset",
 			old:     map[string]any{},
@@ -92,26 +82,6 @@ func TestTopologyImmutable(t *testing.T) {
 			},
 		},
 	}
-	for _, crd := range InstanceCRDs {
-		validators := test.FieldValidators(t,
-			test.MustLoadManifest[apiextensionsv1.CustomResourceDefinition](t, crd))
 
-		path := "openAPIV3Schema.properties.spec"
-		validator, found := validators["v1alpha1"][path]
-		require.True(t, found, "failed to find validator for %s", path)
-
-		for i := range cases {
-			c := &cases[i]
-
-			t.Run(c.desc, func(tt *testing.T) {
-				tt.Parallel()
-				errs := validator(c.current, c.old)
-				require.Equal(tt, len(c.wantErrs), len(errs), "%s: %v", c.desc, errs)
-				for i, err := range errs {
-					assert.EqualError(tt, err, c.wantErrs[i], c.desc)
-				}
-			})
-		}
-
-	}
+	Validate(t, cases)
 }
