09025113d4b1c6860affa183ed825feb78317877
Cody (Xuecheng) Zhang <csuzhangxc@gmail.com>
2025-06-29 09:52:02 +0800

    bump TiDB Operator to 1.6.2 and TiDB to 8.5.2 (#6271)

--- deploy/aws/variables.tf
+++ deploy/aws/variables.tf
@@ -19,7 +19,7 @@ variable "eks_version" {
 
 variable "operator_version" {
   description = "TiDB operator version"
-  default     = "v1.6.1"
+  default     = "v1.6.2"
 }
 
 variable "operator_values" {
@@ -80,7 +80,7 @@ variable "bastion_instance_type" {
 
 # For aws tutorials compatiablity
 variable "default_cluster_version" {
-  default = "v8.5.0"
+  default = "v8.5.2"
 }
 
 variable "default_cluster_pd_count" {
