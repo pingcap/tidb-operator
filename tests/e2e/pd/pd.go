43e36219972ba37e18f1292c73f6f3c77e22a5bc
fgksgf <fgksgf@gmail.com>
2025-07-03 10:50:56 +0800

    add e2e

--- tests/e2e/pd/pd.go
+++ tests/e2e/pd/pd.go
@@ -19,14 +19,18 @@ import (
 	"time"
 
 	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/pingcap/tidb-operator/api/v2/core/v1alpha1"
+	metav1alpha1 "github.com/pingcap/tidb-operator/api/v2/meta/v1alpha1"
 	"github.com/pingcap/tidb-operator/pkg/runtime"
 	"github.com/pingcap/tidb-operator/tests/e2e/data"
 	"github.com/pingcap/tidb-operator/tests/e2e/framework"
 	"github.com/pingcap/tidb-operator/tests/e2e/label"
+	"github.com/pingcap/tidb-operator/tests/e2e/utils/cert"
 	"github.com/pingcap/tidb-operator/tests/e2e/utils/waiter"
 )
 
@@ -167,4 +171,69 @@ var _ = ginkgo.Describe("PD", label.PD, func() {
 			f.WaitForPDGroupReadyAndNotSuspended(ctx, pdg)
 		})
 	})
+
+	ginkgo.DescribeTableSubtree("UsePDReadyAPI Feature", label.P1,
+		func(tls bool) {
+			// Setup cluster with UsePDReadyAPI feature gate and optionally TLS
+			if tls {
+				f.SetupCluster(
+					data.WithClusterTLS(),
+					data.WithFeatureGates(metav1alpha1.UsePDReadyAPI),
+				)
+			} else {
+				f.SetupCluster(data.WithFeatureGates(metav1alpha1.UsePDReadyAPI))
+			}
+
+			ginkgo.It("support create PD with UsePDReadyAPI feature gate enabled", func(ctx context.Context) {
+				if tls {
+					ginkgo.By("Installing TLS certificates")
+					ns := f.Namespace.Name
+					clusterName := f.Cluster.Name
+					f.Must(cert.InstallTiDBIssuer(ctx, f.Client, ns, clusterName))
+					f.Must(cert.InstallTiDBComponentsCertificates(ctx, f.Client, ns, clusterName, "pdg", "kvg", "dbg", "fg", "cg"))
+				}
+
+				ginkgo.By("Creating PD with UsePDReadyAPI feature gate enabled")
+				pdg := f.MustCreatePD(ctx, data.WithReplicas[*runtime.PDGroup](3))
+
+				ginkgo.By("Waiting for PD group to be ready with new readiness probe")
+				f.WaitForPDGroupReady(ctx, pdg)
+
+				ginkgo.By("Verifying PD pods have correct readiness probe configuration")
+				// Get PD pods to verify readiness probe configuration
+				pods := &corev1.PodList{}
+				f.Must(f.Client.List(ctx, pods, client.InNamespace(f.Namespace.Name), client.MatchingLabels(map[string]string{
+					v1alpha1.LabelKeyManagedBy: v1alpha1.LabelValManagedByOperator,
+					v1alpha1.LabelKeyComponent: v1alpha1.LabelValComponentPD,
+				})))
+
+				gomega.Expect(len(pods.Items)).To(gomega.Equal(3), "Should have 3 PD pods")
+				for _, pod := range pods.Items {
+					gomega.Expect(pod.Spec.Containers).To(gomega.HaveLen(1), "PD pod should have 1 container")
+					probe := pod.Spec.Containers[0].ReadinessProbe
+					gomega.Expect(probe).ToNot(gomega.BeNil(), "PD container should have readiness probe")
+					gomega.Expect(probe.Exec).ToNot(gomega.BeNil(), "Readiness probe should use Exec action")
+					gomega.Expect(probe.Exec.Command).To(gomega.ContainElement("curl"), "Should use curl command")
+					gomega.Expect(probe.Exec.Command).To(gomega.ContainElement(gomega.ContainSubstring("/pd/api/v2/ready")), "Should probe /pd/api/v2/ready endpoint")
+
+					if tls {
+						gomega.Expect(probe.Exec.Command).To(gomega.ContainElement(gomega.ContainSubstring("https://127.0.0.1:")), "Should use HTTPS in TLS mode")
+						gomega.Expect(probe.Exec.Command).To(gomega.ContainElement("--cacert"), "Should include CA cert for TLS")
+						gomega.Expect(probe.Exec.Command).To(gomega.ContainElement("--cert"), "Should include client cert for TLS")
+						gomega.Expect(probe.Exec.Command).To(gomega.ContainElement("--key"), "Should include client key for TLS")
+					} else {
+						gomega.Expect(probe.Exec.Command).To(gomega.ContainElement(gomega.ContainSubstring("http://127.0.0.1:")), "Should use HTTP in non-TLS mode")
+					}
+				}
+			})
+		},
+		func(tls bool) string {
+			if tls {
+				return "TLS"
+			}
+			return "NO TLS"
+		},
+		ginkgo.Entry(nil, false),
+		ginkgo.Entry(nil, label.FeatureTLS, true),
+	)
 })
