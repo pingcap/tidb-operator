930d2b2941e7fb46182c312e28fe2abc54e59fc5
Jianjun Liao <jianjun.liao@outlook.com>
2025-07-25 11:38:16 +0800

    resolve conflicts
    Signed-off-by: Jianjun Liao <jianjun.liao@outlook.com>

--- pkg/apis/pingcap/v1alpha1/zz_generated.deepcopy.go
+++ pkg/apis/pingcap/v1alpha1/zz_generated.deepcopy.go
@@ -4157,11 +4157,6 @@ func (in *PreparedPlanCache) DeepCopyInto(out *PreparedPlanCache) {
 		*out = new(float64)
 		**out = **in
 	}
-	if in.TiKVConfigUpdateStrategy != nil {
-		in, out := &in.TiKVConfigUpdateStrategy, &out.TiKVConfigUpdateStrategy
-		*out = new(ConfigUpdateStrategy)
-		**out = **in
-	}
 	return
 }
 
