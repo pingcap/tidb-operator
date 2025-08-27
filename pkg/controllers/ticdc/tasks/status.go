8d2792fc4d6cc3e6df31221087df83ec66659c47
liubo02 <liubo02@pingcap.com>
2025-08-27 20:37:56 +0800

    fix lint and ut
    Signed-off-by: liubo02 <liubo02@pingcap.com>

--- pkg/controllers/ticdc/tasks/status.go
+++ pkg/controllers/ticdc/tasks/status.go
@@ -50,10 +50,16 @@ func TaskStatus(state *ReconcileContext, c client.Client) task.Task {
 		needUpdate = compare.SetIfNotEmptyAndChanged(&ticdc.Status.ID, state.MemberID) || needUpdate
 		needUpdate = compare.SetIfChanged(&ticdc.Status.IsOwner, state.IsOwner) || needUpdate
 		needUpdate = compare.SetIfNotEmptyAndChanged(&ticdc.Status.ObservedGeneration, ticdc.Generation) || needUpdate
-		needUpdate = compare.SetIfNotEmptyAndChanged(&ticdc.Status.UpdateRevision, ticdc.Labels[v1alpha1.LabelKeyInstanceRevisionHash]) || needUpdate
+		needUpdate = compare.SetIfNotEmptyAndChanged(
+			&ticdc.Status.UpdateRevision,
+			ticdc.Labels[v1alpha1.LabelKeyInstanceRevisionHash],
+		) || needUpdate
 
 		if ready {
-			needUpdate = compare.SetIfNotEmptyAndChanged(&ticdc.Status.CurrentRevision, pod.Labels[v1alpha1.LabelKeyInstanceRevisionHash]) || needUpdate
+			needUpdate = compare.SetIfNotEmptyAndChanged(
+				&ticdc.Status.CurrentRevision,
+				pod.Labels[v1alpha1.LabelKeyInstanceRevisionHash],
+			) || needUpdate
 		}
 
 		if needUpdate {
