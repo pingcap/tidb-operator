5850fe2b55e35dcf41b9c68c9cad51652c6cee81
Bo Liu <liubo02@pingcap.com>
2025-09-08 20:14:51 +0800

    fix(group): break if feature gates field is not synced (#6437)

--- pkg/controllers/replicationworkergroup/builder.go
+++ pkg/controllers/replicationworkergroup/builder.go
@@ -31,7 +31,7 @@ func (r *Reconciler) NewRunner(state *tasks.ReconcileContext, reporter task.Task
 		common.TaskContextCluster[scope.ReplicationWorkerGroup](state, r.Client),
 		// if it's paused just return
 		task.IfBreak(common.CondClusterIsPaused(state)),
-		common.TaskCheckFeatureGates[scope.ReplicationWorkerGroup](state, r.Client),
+		task.IfBreak(common.CondFeatureGatesIsNotSynced[scope.ReplicationWorkerGroup](state)),
 
 		// get all schedulers
 		common.TaskContextSlice[scope.ReplicationWorkerGroup](state, r.Client),
