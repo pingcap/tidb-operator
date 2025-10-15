4ad5f1144bda8729d22e13b00831894b3d08ce03
Bo Liu <liubo02@pingcap.com>
2025-10-15 18:55:58 +0800

    fix(log): sleep x to allow all logs are tailed to stdout (#6498)

--- pkg/manager/member/tikv_member_manager.go
+++ pkg/manager/member/tikv_member_manager.go
@@ -563,12 +563,16 @@ func getNewTiKVSetForTidbCluster(tc *v1alpha1.TidbCluster, cm *corev1.ConfigMap)
 			},
 		}
 		if logTailer.UseSidecar {
+			var sleepCmd string
+			if logTailer.SleepTimeSeconds != nil {
+				sleepCmd = fmt.Sprintf("sleep %d; ", *logTailer.SleepTimeSeconds)
+			}
 			c.RestartPolicy = ptr.To(corev1.ContainerRestartPolicyAlways)
 			// NOTE: tail cannot hanle sig TERM when it's PID is 1
 			c.Command = []string{
 				"sh",
 				"-c",
-				fmt.Sprintf(`trap "exit 0" TERM; touch %s; tail -n0 -F %s & wait $!`, rocksDBLogFilePath, rocksDBLogFilePath),
+				fmt.Sprintf(`trap "%sexit 0" TERM; touch %s; tail -n0 -F %s & wait $!`, sleepCmd, rocksDBLogFilePath, rocksDBLogFilePath),
 			}
 			initContainers = append(initContainers, c)
 		} else {
@@ -625,12 +629,16 @@ func getNewTiKVSetForTidbCluster(tc *v1alpha1.TidbCluster, cm *corev1.ConfigMap)
 			},
 		}
 		if logTailer.UseSidecar {
+			var sleepCmd string
+			if logTailer.SleepTimeSeconds != nil {
+				sleepCmd = fmt.Sprintf("sleep %d; ", *logTailer.SleepTimeSeconds)
+			}
 			c.RestartPolicy = ptr.To(corev1.ContainerRestartPolicyAlways)
 			// NOTE: tail cannot hanle sig TERM when it's PID is 1
 			c.Command = []string{
 				"sh",
 				"-c",
-				fmt.Sprintf(`trap "exit 0" TERM; touch %s; tail -n0 -F %s & wait $!`, raftLogFilePath, raftLogFilePath),
+				fmt.Sprintf(`trap "%sexit 0" TERM; touch %s; tail -n0 -F %s & wait $!`, sleepCmd, raftLogFilePath, raftLogFilePath),
 			}
 			initContainers = append(initContainers, c)
 		} else {
