e4b626282e45901af0bf6b4b264d3826a7be568c
ris <79858083+RidRisR@users.noreply.github.com>
2025-10-16 19:55:50 +0800

    bump TiDB Operator to 1.6.4 (#6501)

--- deploy/aws/variables.tf
+++ deploy/aws/variables.tf
@@ -19,7 +19,7 @@ variable "eks_version" {
 
 variable "operator_version" {
   description = "TiDB operator version"
-  default     = "v1.6.3"
+  default     = "v1.6.4"
 }
 
 variable "operator_values" {
