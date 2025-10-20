15a92c4dbd3ce50d7c915565ca57d1c4c03a1693
liubo02 <liubo02@pingcap.com>
2025-10-20 23:36:49 +0800

    fix lint
    Signed-off-by: liubo02 <liubo02@pingcap.com>

--- pkg/client/opt.go
+++ pkg/client/opt.go
@@ -18,7 +18,10 @@ type ApplyOptions struct {
 	// Immutable defines fields which is immutable
 	// It's only for some fields which cannot be changed but actually maybe changed.
 	// For example,
-	// We change storage class ref in instance CR to modify volumes if feature VolumeAttributesClass is not enabled, but it's immutable in PVC. When we enable VolumeAttributesClass, we have to skip storage class change when apply.
+	// Storage class ref can be changed in instance CR to modify volumes
+	// if feature VolumeAttributesClass is not enabled(just like v1).
+	// However, it's immutable in PVC. When VolumeAttributesClass is enabled,
+	// storage class should not be applied to PVC.
 	// NOTE; now slice/array is not supported
 	Immutable [][]string
 }
