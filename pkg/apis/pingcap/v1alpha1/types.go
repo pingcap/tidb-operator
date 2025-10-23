b348230ee80c35a80ca2783065a2141cb6fb37e3
Ziya Ye <ziya.ye@databricks.com>
2025-10-23 16:07:59 -0700

    Add pingcap's fix pr on mooncake endpoint (#14)
    * azureblob: upgrade gocloud to support endpoint options
    
    * try make AD work with Azure other region
    
    * fix build
    
    ---------
    
    Co-authored-by: Ziya Ye <ziya.ye@your.hostname.com>
    (cherry picked from commit 779c481c3901ec5222f6ee436ef5950b716e23ed)

--- pkg/apis/pingcap/v1alpha1/types.go
+++ pkg/apis/pingcap/v1alpha1/types.go
@@ -2119,6 +2119,8 @@ type AzblobStorageProvider struct {
 	SasToken string `json:"sasToken,omitempty"`
 	// Prefix of the data path.
 	Prefix string `json:"prefix,omitempty"`
+	// Endpoint of azblob compatible storage service
+	Endpoint string `json:"endpoint,omitempty"`
 }
 
 // BackupType represents the backup type.
