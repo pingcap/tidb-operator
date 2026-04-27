38939e10266ecf44d355fcd95272fbe41c87da3e
SuperQ <superq@gmail.com>
2026-04-27 22:56:09 +0200

    Implement scale subresource on Pod owners
    In order for PodDisruptionBudgets to work on various groups of Pods
    we need to implement the Scale subresource at the direct owner of
    the Pod.
    
    Otherwise the PDB will return errors like:
    
    ```yaml
    status:
      conditions:
        - lastTransitionTime: "2026-04..."
          message: tikvs.core.pingcap.com does not implement the scale subresource
          reason: SyncFailed
          status: "False"
          type: DisruptionAllowed
    ```
    
    Followup to https://github.com/pingcap/tidb-operator/pull/6213
    
    Signed-off-by: SuperQ <superq@gmail.com>

--- api/core/v1alpha1/tiproxy_types.go
+++ api/core/v1alpha1/tiproxy_types.go
@@ -89,6 +89,7 @@ type TiProxyList struct {
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
+// +kubebuilder:subresource:scale:specpath=.spec.replicas,statuspath=.status.replicas,selectorpath=.status.selector
 // +kubebuilder:resource:categories=instance
 // +kubebuilder:selectablefield:JSONPath=`.spec.cluster.name`
 // +kubebuilder:printcolumn:name="Cluster",type=string,JSONPath=`.spec.cluster.name`
